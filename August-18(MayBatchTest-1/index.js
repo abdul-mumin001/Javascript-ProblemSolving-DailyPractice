bc99f4bb5c3a418801731e151bc6f58aadda84a7
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-08-18 14:51:58 +0530

    jsProblemSolving

--- August-18(MayBatchTest-1/index.js
+++ August-18(MayBatchTest-1/index.js
@@ -31,3 +31,38 @@ function calculateCharge(no_of_days){
     }
 }
 console.log(calculateCharge(10))
+
+
+
+// Q. Abhishek choudhary is the teacher of a higher secondary school. He was assigned a task to record the number of students
+//  in each class. The 1st class has ‘A ’ students and 2nd class has ‘B’ students. He completed noting down all the classes but 
+//  till he reached class xth the school was over. When he was going through the list he noticed that the next class had students
+//   equal to the sum of its last two classes. Can you help him to find out the number of students present in xth class using the 
+//   above details.
+
+
+// constraint: 
+
+// 1<=A,B<=100
+
+
+
+
+function students(a,b){
+    let c=a+b;
+    let d=c+b;
+    let e=d+c;
+    let f=e+d;
+    let g=f+e;
+    let h=g+f;
+    let i=h+g;
+    let j=i+h;
+    return j;
+
+
+}
+console.log(students(20,30))
+
+
+
+
