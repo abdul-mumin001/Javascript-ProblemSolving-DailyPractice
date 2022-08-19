2995b87ae51ac51de7d79f057ce280d493b2cd4d
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-08-19 23:55:53 +0530

    August-19

--- August-18(MayBatchTest-1/August-19/app.js
+++ August-18(MayBatchTest-1/August-19/app.js
@@ -9,3 +9,69 @@ console.log(9.789.toFixed(2))
 // toPrecision=>it basically returns the exact number of digits that you want.
 
 console.log(9.7898.toPrecision(1))
+
+
+
+
+//Prakash sir function class
+
+
+console.log("hello console")
+// console.log("hello")
+
+// function
+
+// Robot
+
+// function
+// Go to market(200 line)
+// Get vegetables(200 lines)
+// Get cash from Atomics(200 lines)
+
+// function goToMarket(){
+//     console.log("Go to Market")
+// }
+// function buyVegetables(a,b){
+//     console.log(a,b);
+//     console.log("Buy vegetables")
+// }
+// function withdrawCash(){
+//     console.log("Withdraw Cash from Atm")
+// }
+// goToMarket();
+// buyVegetables();
+// withdrawCash();
+
+// What is variable- store data that can change the value . 
+
+// let veg1="Carrot";
+// let veg2="Brinjal";
+
+// console.log("Monday")
+// buyVegetables(veg1,veg2)
+// let veg1="tomato"
+// let veg2="coliflower"
+
+// console.log("TUESDAY")
+// buyVegetables(veg1,veg2)
+
+
+
+// count personal savings of everyone then add then together
+
+// let prakash, mumin,joyin,totalsavings;
+
+// function savings(mutualFund,stock,cashInBank, pfFund,bond,crypto){//it will calculate the total savings of a person
+
+//     let total=mutualFund+stock+cashInBank+pfFund+bond+crypto;
+//     return total;
+
+// }
+// prakash=savings(1000,2000,50000,60000,7000,3000);
+// mumin=savings(12000,13000,2000,4000,5000,23000);
+// joyin=savings(2100,2000,1000,300,4000,500000);
+
+// console.log(prakash)
+// totalsavings= prakash+mumin+joyin;
+// console.log(totalsavings,"totalsavings") 
+
