dfb3391196e58ee066efa578a5af720bed40a79e
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-09-05 23:36:42 +0530

    pattern-practice

---
+++ August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5/app.js
@@ -0,0 +1,31 @@
+// *****
+// *****
+// *****
+// *****
+
+
+for(let i=1;i<=4;i++){
+    let stars='';
+    for(let j=1;j<=5;j++){
+      stars=stars+'*'
+    }
+    console.log(stars)
+  }
+
+
+
+
+// *
+// **
+// ***
+// ****
+// ***** 
+
+
+for(let i=1;i<=5;i++){
+    stars='';
+    for(let j=1;j<=i;j++){
+      stars+='*'
+    }
+    console.log(stars)
+  }
