5b843d647ff994fe2cf0bc6038cb2dfe51d32412
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-09-11 21:35:07 +0530

    practice

--- August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5-doc-pattern/11-sep/index.js
+++ August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5-doc-pattern/11-sep/index.js
@@ -1,6 +1,23 @@
-let num=1255
-let a=num%10;
-let b=Math.floor(num/100)
-let c=b%10;
-let ans=c+''+a
-console.log(ans)
+//second and lastdigit
+// let num=1255;
+// let a=num%10;
+
+// let b=Math.floor(num/100)
+
+// let c=b%10;
+
+// let ans=c+''+a
+// console.log(ans)
+
+
+
+
+let num = 1234 , str = '';
+
+while(num != 0){  // true 
+  let digit = num %10; // digit = 1%10 = 1
+  num = Math.floor(num/10); // num = 1/10 = 0
+  console.log(digit); // 4 , 3 , 2 , 1
+  str += digit; //4321 
+}
+console.log(str);
