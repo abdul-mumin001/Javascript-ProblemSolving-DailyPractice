bd854e65bcc9e1bbbaadc642113085df806422f1
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-09-14 01:26:49 +0530

    Pattern

--- August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5-doc-pattern/app.js
+++ August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5-doc-pattern/app.js
@@ -375,3 +375,196 @@ for(let i=0;i<=4;i++){
   }
   console.log(str);
 }
+
+
+
+
+
+
+// 18.Q 
+//E
+//D E
+//C D E
+//B C D E
+//A B C D E
+
+
+n = 5;
+for(let i=n-1;i>=0;i--){
+  let str ="";
+  for(let j=i; j<n; j++){
+    str +=String.fromCharCode(j+65)+" ";    
+  }
+  console.log(str);
+}
+
+// 19.Q 
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+
+
+ n=5;
+for (let i=1; i<=n; i++){
+  let str=""
+for (let j =i;j<=n; j++){
+  str+="*";
+  }
+  for (let k=1;k<=i*2-2;k++){
+  str+=" ";
+  }
+  for (let l=i;l<=n;l++)
+      {
+    str+="*";
+    }
+    console.log(str);
+        }
+        
+  for (let i=1;i<=n;i++){
+  let string="";
+  for (let j=1;j<=i;j++){
+        string+="*";
+      }
+  for (let k=i*2-2;k<n*2-2;k++){
+    string+=" ";
+    }
+  for (let l=1;l<=i;l++)
+    {
+    string+="*";
+  }console.log(string);
+  }
+
+
+
+
+  // 20.Q 
+
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
+
+   n=5;
+for(let i=1;i<=n;i++){
+  let str="";
+  for(let j=1;j<=i;j++){
+    str+="*";
+  } 
+  for (let k=i*2;k<=n*2-1;k++){
+    str+=" ";
+  }
+    for (let l=1;l<=i;l++){
+    str+="*";
+  }
+console.log(str);
+  }
+for(let i=1;i<=n-1;i++){
+  let string=""
+  for (let j=n-1;j>=i;j--){
+  string+="*";
+  }
+  for (let k=1;k<=i*2;k++){
+  string+=" ";
+    }
+  for (let l=n-1;l>= i;l--){
+    string+="*";
+    }
+    console.log(string)
+}
+
+
+
+// 21.Q
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+
+ n=5;
+for(let i=0;i<n;i++){
+  let string="";
+  for(j=0;j<n;j++){
+    if(i === 0 || i === n-1 ) {
+      string += "*";
+    }
+    else {
+      if(j === 0 || j === n-1 ) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+  }console.log(string)
+}
+
+
+
+// 22.Q 
+// 4 4 4 4 4 4 4
+// 4 3 3 3 3 3 4
+// 4 3 2 2 2 3 4
+// 4 3 2 1 2 3 4
+// 4 3 2 2 2 3 4
+// 4 3 3 3 3 3 4
+// 4 4 4 4 4 4 4
+
+
+ n = 7
+let start = 1;
+let end = Math.floor(n/2) + 1;
+for(let i = 1; i <= 7; i++){
+  let str = ""
+  for(let j =1; j <= 7 ;j++){
+    if(i == start + 0 || i == n - 0  || j==start + 0 || j == n - 0 ){
+      str+=" "+ (end - 0);
+    }else if(i == start + 1 || i == n-1 || j == start+1 || j == n -1){
+      str += " "+(end-1)
+    } else if(i == start + 2 || i == n-2 || j == start + 2 || j == n - 2){
+      str += " "+(end -2)
+    } else if( i== start + 3 || i == n-3 || j == start +3 || j == n-3 ){
+      str += " " + (end-3)
+    }
+  }
+  console.log(str)
+ }
+
+
+
+//  23.Q 
+// *   *****
+// *   *
+// *   *
+// *********
+//     *   *
+//     *   *
+// *****   *
+
+ str= "";
+for(i = 1; i <= 7; i++){
+  for(j = 1; j <= 9; j++){
+    if(j == 5 && i >= 1 || i == 4 && j >= 1 || j == 1 && i <= 3 ||
+    i == 1 && j >= 6 || i == 7 && j <= 4 || j == 9 && i >= 5){
+      str += "*";
+    } else {
+      str += " ";
+    }
+  }
+  str += "\n";
+}
+console.log(str)
+
