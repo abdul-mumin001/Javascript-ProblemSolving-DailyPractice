557f9bd0ecbcb881dc3330b03fc8cc3e40a3bd31
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-09-16 20:09:38 +0530

    practiceJs

--- August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5-doc-pattern/11-sep/15-sep/index.js
+++ August-18(MayBatchTest-1/August-19/August-20/Sep-4/Sep-5-doc-pattern/11-sep/15-sep/index.js
@@ -11,3 +11,35 @@ function squareroot(number) {
 }
 
 console.log(squareroot(81))
+
+
+
+
+
+// Q. Try to implement Math.pow(a,b) 
+
+
+
+function pow (a,b){
+    if(a > 0 && b === 0){
+        return 1;
+    }
+    c = a;
+    while(b > 1){
+        c = a*c;
+        b--;
+    }
+    return c;
+}
+
+
+console.log(pow(0,0)+'\n'); 
+console.log(pow(2,6)+'\n'); 
+
+
+function power(a,b){
+  let pow=a**b;
+  return pow;
+}
+console.log(power(2,6))
+console.log(power(1,0))
