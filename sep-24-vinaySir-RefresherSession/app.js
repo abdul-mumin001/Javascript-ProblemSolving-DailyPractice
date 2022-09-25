ba6dae2c6943ba301d0cc5e43c11749f9d93d0a5
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-09-25 17:46:22 +0530

    jsPractice

--- sep-24-vinaySir-RefresherSession/app.js
+++ sep-24-vinaySir-RefresherSession/app.js
@@ -174,5 +174,32 @@ do{
 
 
 
+//  Q . You are given a sorted array and a target. Return the index of target if it exists in the array, 
+//   if target does not exist in array return the index of immediate smallest element to target and if the
+//    smallest does not exist return -1.
 
+
+
+function binarySearch(arr, target){
+  let start=0;
+  let end=arr.length-1;
+  while(start<=end){
+    let mid=Math.floor((start+end)/2);
+
+    if(arr[mid]==target){
+       return mid;
+    }else if(arr[mid]<target){
+       start=mid+1;
+    }else{
+       end=mid-1;
+    }
+      if(target>arr[end] && target<arr[start]){
+        return end;
+   }
+    
+  }
+  return -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5],3))
+console.log(binarySearch([1, 2, 3, 4, 5],7))
 
