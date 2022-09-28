1cf93356a8e08071f7cc1470f7140dec5d5e4239
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-09-29 00:28:20 +0530

    File-Organiser

---
+++ sep-29-File-Organiser-project/app.js
@@ -0,0 +1,21 @@
+// fs :- file system. 
+// fs.readFileSync :- 
+// writeFileSync :- 
+// copyFileSync :- copy a file.
+// unlinkSync :- delete a file
+// mkdirSync :- make a folder/ directory
+// existSync :- this is use to check if a path exist or not
+// rmdirSync :- delete a folder
+// lstatSunc :- use to check it is a file or a folder.
+
+
+// path :- manipulate the path
+// path.join :- it is use to join two path
+// path.extname :- use to get the extension of the file.
+
+// process.cwd() :- cwd :- current working directory.
+// process.argv :- it is use to take input
+
+// let a = process.argv[2];
+// console.log(process.argv);
+
