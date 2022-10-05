1aa27c095c88e704c07c3b8e1057e58998040a7b
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-10-05 22:56:48 +0530

    practice

---
+++ oct-5/index.js
@@ -0,0 +1,19 @@
+// # First and Last
+// You are given a sorted array which contains duplicate elements and a target. Return the first and last index of target if it exists in the array, else return -1
+
+// ### Input Format
+//   An array `arr` and an integer `target
+
+// ### Output Format
+// If target exists return the first and last index of the target as an array. Else return -1
+
+// ### Sample Input: 
+// ```
+
+// [1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5]
+// 3
+// ```
+
+// ### Sample Output:
+// ```
+// [3, 6]
