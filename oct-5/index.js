75e59e168201af306f66890ddfd7df3fb7488e8d
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-10-06 23:51:12 +0530

    practice

--- oct-5/index.js
+++ oct-5/index.js
@@ -64,3 +64,28 @@ function search(arr, target){
   }
   console.log(search([1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5],3));
   
+
+
+
+
+  // logical operator 
+
+  var x = 220;
+var y = "Hello";
+var z = undefined;
+        
+// x | | y    // Returns 220 since the first value is truthy
+        
+// x | | z   // Returns 220 since the first value is truthy
+        
+// x && y    // Returns "Hello" since both the values are truthy
+        
+// y && z   // Returns undefined since the second value is falsy
+        
+if( x && y ){ 
+  console.log("Code runs" ); // This block runs because x && y returns "Hello" (Truthy)
+}   
+        
+if( x || z ){
+  console.log("Code runs");  // This block runs because x || y returns 220(Truthy)
+}
