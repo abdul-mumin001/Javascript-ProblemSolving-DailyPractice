2f40ff660bd7d0832cadf72f678b004d7bb067b9
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-10-12 00:21:17 +0530

    linked-list

---
+++ oct-11-linked-list/app.js
@@ -0,0 +1,27 @@
+// const list={
+//     value:1,
+//     next:{
+//         value:2,
+//         next:{
+//             value:3,
+//             next:{
+//                 value:5,
+//                 next:null,
+//             },
+//         },
+//     },
+// };
+
+
+// let current=list;
+// console.log(list)
+// while(current !== null){
+//     console.log(current)
+//     current.value +=1;
+//     current=current.next;
+// }
+// console.log(list)
+
+
+
+
