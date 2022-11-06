b1a8c0a1bc45101c1f44a301c5f8e7f715eb415b
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-06 10:28:36 +0530

    two-sum

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -197,3 +197,38 @@ var isAnagram = function(s, t) {
 
 
 
+
+// Q.Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+
+var twoSum = function(nums, target) {
+      let newObj = {};
+    
+      for(let i = 0; i < nums.length; i++) {
+        const n = nums[i];
+        if(newObj[target - n] !== undefined) {
+          return [newObj[target - n], i];
+        }
+        newObj[n] = i;
+      }
+      return [];
+
+    };
+    
+
