05a3d2c457573274525a8caafaeac701504443cc
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-07 20:33:23 +0530

    pairs-with-an-absolute-diff-k

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -306,3 +306,60 @@ function createStr(arr){
  }
  console.log(createStr(['Hello', 'World', 123]))
  console.log(createStr([1, 2, "My", 56, "name", "is", 5, "Aircampus"]))
+
+
+
+
+//  Q.Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+//  The value of |x| is defined as:
+ 
+//  x if x >= 0.
+//  -x if x < 0.
+  
+ 
+//  Example 1:
+ 
+//  Input: nums = [1,2,2,1], k = 1
+//  Output: 4
+//  Explanation: The pairs with an absolute difference of 1 are:
+//  - [1,2,2,1]
+//  - [1,2,2,1]
+//  - [1,2,2,1]
+//  - [1,2,2,1]
+//  Example 2:
+ 
+//  Input: nums = [1,3], k = 3
+//  Output: 0
+//  Explanation: There are no pairs with an absolute difference of 3.
+//  Example 3:
+ 
+//  Input: nums = [3,2,1,5,4], k = 2
+//  Output: 3
+//  Explanation: The pairs with an absolute difference of 2 are:
+//  - [3,2,1,5,4]
+//  - [3,2,1,5,4]
+//  - [3,2,1,5,4]
+
+
+
+
+var countKDifference = function(nums, k) {
+    var obj={};
+    var ans=0;
+
+    for(var i=0;i<nums.length;i++){
+        if(obj[nums[i] + k] != undefined){
+            ans+=obj[nums[i]+k];
+        }
+        if(obj[nums[i] - k] != undefined){
+            ans+=obj[nums[i]-k];
+        }
+        if(obj[nums[i]]==undefined){
+            obj[nums[i]]=1;
+        }else{
+            obj[nums[i]]++;
+        }
+    }
+    return ans;
+};
