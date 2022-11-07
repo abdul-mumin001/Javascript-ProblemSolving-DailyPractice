4c47a78411baf6a0311cd102ac886ad3463cf090
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-07 20:15:19 +0530

    create-string

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -257,3 +257,52 @@ var twoSum = function (nums, target) {
         storage[target - num] = i;
     }
 };
+
+
+
+// Q. Given a array containing both strings and numbers, remove all the numbers and concat all the strings. Return the resulting string
+
+// ### Input Format
+// An array
+
+// ### Output Format
+// Return a string
+
+// ### Sample Input:
+// ```
+// ['Hello', 'World', 123]
+// ```
+
+// ### Sample Output:
+// ```
+// Hello World
+// ```
+
+// ### Sample Input:
+// ```
+// [1, 2, "My", 56, "name", "is", 5, "Aircampus"]
+// ```
+
+// ### Sample Output:
+// ```
+// My name is Aircampus
+
+
+
+function createStr(arr){
+    var str='';
+   for(var i=0;i<arr.length;i++){
+     if(typeof(arr[i])=='string'){
+       if(str==''){
+         str+=arr[i];
+         // console.log(str)
+       }else{
+         str+=' '+arr[i];
+         // console.log(str)
+       }
+     }
+   }
+   return str;
+ }
+ console.log(createStr(['Hello', 'World', 123]))
+ console.log(createStr([1, 2, "My", 56, "name", "is", 5, "Aircampus"]))
