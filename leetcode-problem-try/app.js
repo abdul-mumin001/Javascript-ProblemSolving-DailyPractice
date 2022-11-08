d499e38b946f62d62c6cbfc4ec50c2d4a5872fe4
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-08 19:04:57 +0530

    First Unique Character in a String

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -378,3 +378,36 @@ for (let elj of nums)
 
 return count / 2
 };
+
+
+
+// Q. Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 0
+// Example 2:
+
+// Input: s = "loveleetcode"
+// Output: 2
+// Example 3:
+
+// Input: s = "aabb"
+// Output: -1
+
+
+
+
+var firstUniqChar = function(s) {
+    for(i=0; i<s.length; i++){
+        if(s.indexOf(s[i])===s.lastIndexOf(s[i])){
+         return i
+        }
+    }
+
+    return -1
+
+};
