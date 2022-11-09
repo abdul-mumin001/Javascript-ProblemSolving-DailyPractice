0c573c496202eee2998f4c5898b1856bcd6f92d6
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-09 22:07:09 +0530

    dom-practice

--- DOM-kamal-sir-11-nov/app.js
+++ DOM-kamal-sir-11-nov/app.js
@@ -29,3 +29,26 @@ Object.assign(firstPara.style, newstyles)
 
 const b=document.querySelector("body")
 b.style.background="aqua"
+
+
+
+// innerText  & textContent 
+const innerText=firstPara.innerText
+const ul=document.getElementById("uno")
+
+// inner gives all the text from all the child elements 
+console.log(ul.innerText)
+
+
+// innerHTML
+
+const innerHTML=ul.innerHTML
+//  innerHTML gives all the 'Html' from  all the childs 
+console.log(innerHTML)
+
+
+const outerHTML=ul.outerHTML
+
+console.log(outerHTML)
+firstPara.innerHTML=ul.outerHTML
+
