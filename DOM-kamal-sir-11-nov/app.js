7d1952deb35a58907a764de8027028ade76320e2
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-09 21:16:08 +0530

    dom-beg

---
+++ DOM-kamal-sir-11-nov/app.js
@@ -0,0 +1,21 @@
+// const elements=document.getElementsByClassName("para")
+// const element=document.getElementById("some-para")
+
+// console.log(elements);
+// console.log(elements);
+
+// return only first element 
+// const el =document.querySelector("#id1, #id2, #id3")
+// console.log(el);
+// return  all elements 
+// const els =document.querySelectorAll("#id1, #id2, #id3")
+
+// console.log(els);
+
+
+
+// modify css by js 
+
+const firstPara=document.getElementById("some-para")
+
+firstPara.style.fontSize='50px'
