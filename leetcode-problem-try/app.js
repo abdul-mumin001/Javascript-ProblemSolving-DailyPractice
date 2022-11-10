4413075324850e3d32fbed67f2cd9039f5c79928
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-10 23:41:08 +0530

    object-item

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -443,4 +443,49 @@ var firstUniqChar = function(s) {
 
 
 
+// Q.## Given an object items and their price, and a number. Return all the items whose price is greater than the given number.
+
+// ### Input Format
+// An object ```obj``` and a number  ```n```
+
+// ### Output Format
+// Return a object.
+
+// ### Sample Input:
+// ```
+// { "a": 3000, "b": 200, "c": 1050 }
+// 1000
+// ```
+
+// ### Sample Output:
+// ```
+// { "a": 3000, "c": 1050 }
+// ```
+
+// ### Sample Input:
+// ```
+// { "Deluxe Burger": 35, "Icecream Shake": 4, "Fries": 5 }
+// 40
+// ```
+
+// ### Sample Output:
+// ```
+// {}
+
+
+
+function expensive(obj, n){
+    let objec={};
+      for(let i in obj){
+        if(obj[i]>n){
+         objec[i]=obj[i];
+        }
+      }
+      return objec;
+    }
+    console.log(expensive({ "a": 3000, "b": 200, "c": 1050 },
+    1000))
+
+
+
 
