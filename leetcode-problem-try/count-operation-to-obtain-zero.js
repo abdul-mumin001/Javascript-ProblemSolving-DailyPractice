1302e8e7ab13893f6ef3e01ab3d852543243ed53
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-10 23:49:58 +0530

    count operation to obtain zero

--- leetcode-problem-try/count-operation-to-obtain-zero.js
+++ leetcode-problem-try/count-operation-to-obtain-zero.js
@@ -68,3 +68,16 @@ var countOperations = function(num1, num2) {
     return r;
 };
 
+
+var countOperations = function(num1, num2) {
+    var ans=0;
+    while(num1 != 0 && num2 !=0){
+       if(num1>num2){
+           num1=num1-num2;
+       } else {
+           num2=num2-num1;
+       }
+       ans++
+   }
+    return ans;
+};
