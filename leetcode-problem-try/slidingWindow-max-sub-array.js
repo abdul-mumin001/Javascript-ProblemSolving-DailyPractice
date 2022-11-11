ef7fd83d296f0f65a0af1f4adfac96f19e9c283b
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-11 23:37:38 +0530

    sliding-window-max-subarray-of-size-k

---
+++ leetcode-problem-try/slidingWindow-max-sub-array.js
@@ -0,0 +1,18 @@
+// Q.Given an array of positive integer find the maximum subarray of size k 
+ 
+
+var arr=[-3,2,3,3,3,6,8,15];
+ var k=4
+
+ var sum=0;
+ var currMax=0;
+
+ for(let i=0;i<k;i++){
+    sum=sum+arr[i];
+ }
+ currMax=sum;
+ for(let i=k;i<arr.length;i++){
+    sum=(sum+arr[i])-arr[i-k];
+    currMax=Math.max(currMax,sum);
+ }
+ console.log(currMax);
