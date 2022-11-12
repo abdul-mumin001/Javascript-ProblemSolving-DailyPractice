4208e94ef0bf6bae2c14bc571d728ec79dc4521f
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-12 23:37:51 +0530

    last-name

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -525,3 +525,43 @@ function expensive(obj, n){
         return  last_name;
       }
       console.log(lastName('Air Campus'))
+
+
+
+    //   Q. Given a array containing both strings and numbers, remove all the numbers and concat all the strings. Return the resulting string
+    //   Input Format
+    //   An array
+      
+    //   Output Format
+    //   Return a string
+      
+    //   Sample Input:
+    //   ['Hello', 'World', 123]
+
+    //   Sample Output:
+    //   Hello World
+
+    //   Sample Input:
+    //   [1, 2, "My", 56, "name", "is", 5, "Aircampus"]
+
+    //   Sample Output:
+    //   My name is Aircampus
+
+
+    function createStr(arr){
+        var str='';
+       for(var i=0;i<arr.length;i++){
+         if(typeof(arr[i])=='string'){
+           if(str==''){
+             str+=arr[i];
+           
+           }else{
+             str+=' '+arr[i];
+             
+           }
+         }
+       }
+       return str;
+     }
+     console.log(createStr(['Hello', 'World', 123]))
+     console.log(createStr([1, 2, "My", 56, "name", "is", 5, "Aircampus"]))
