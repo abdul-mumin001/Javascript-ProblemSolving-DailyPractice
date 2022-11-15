496cd3f35bd4d0f6e159ec0f074224f444abcf2b
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-11-15 23:09:19 +0530

    array-obj

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -794,3 +794,25 @@ function keyValue(obj){
    return [arrKey , arrValue]
   }
   console.log(keyValue({ a: 1, b: 2, c: 3 }));
+
+
+
+
+// Q.Given an object, return the object as an array where each array element represent the corresponding key value.
+// Input Format
+// An object obj
+
+// Output Format
+// Return an array where each element represent key value
+
+// Sample Input:
+// { a: 1, b: 2 }
+// Sample Output:
+// [["a", 1], ["b", 2]]
+
+
+
+function array(obj){
+    return Object.entries(obj)
+  }
+  console.log(array({ a: 1, b: 2 }));
