2bb9d09130afc017fdbaf1d5efa9ef3a37ebdcf4
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-02 23:56:23 +0530

    call-by-reference

--- DSA-by-Ritik-sir/Problem-solving/call-by-reference.js
+++ DSA-by-Ritik-sir/Problem-solving/call-by-reference.js
@@ -1,20 +1,49 @@
  // Call By Reference
  
-function fun(ob, a) {
-    ob.a = 100;
-    a[0] = 10;
-    console.log(ob, a);
-}
+// function fun(ob, a) {
+//     ob.a = 100;
+//     a[0] = 10;
+//     console.log(ob, a);
+// }
  
-let obj = {
-    a: 1
+// let obj = {
+//     a: 1
+// }
+
+// let arr = [1, 2];
+ 
+// console.log("Before Call", obj, arr);
+ 
+// fun(obj, arr);
+ 
+// console.log("After call", obj, arr);
+
+
+
+
+//javascript pass by reference
+function callByReference(varObj) {
+
+    console.log("Inside Call by Reference Method");
+
+    varObj.a = 100;
+    console.log(varObj);
+
 }
 
-let arr = [1, 2];
- 
-console.log("Before Call", obj, arr);
- 
-fun(obj, arr);
- 
-console.log("After call", obj, arr);
+let varObj = {
+    a: 1
+};
+
+console.log("Before Call by Reference Method");
+
+console.log(varObj);
+
+callByReference(varObj)
+
+console.log("After Call by Reference Method");
+
+console.log(varObj);
+
+
 
