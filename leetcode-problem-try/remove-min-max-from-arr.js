c868830dec9a96b0a4539b48687eeec3a1e421cf
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-03 23:25:51 +0530

    Removing Minimum and Maximum From Array

--- leetcode-problem-try/remove-min-max-from-arr.js
+++ leetcode-problem-try/remove-min-max-from-arr.js
@@ -19,8 +19,8 @@
 // The maximum element in the array is nums[1], which is 10.
 // We can remove both the minimum and maximum by removing 2 elements from the front and 3 elements from the back.
 // This results in 2 + 3 = 5 deletions, which is the minimum number possible.
-// Example 2:
 
+// Example 2:
 // Input: nums = [0,-4,19,1,8,-2,-3,5]
 // Output: 3
 // Explanation: 
@@ -29,6 +29,12 @@
 // We can remove both the minimum and maximum by removing 3 elements from the front.
 // This results in only 3 deletions, which is the minimum number possible.
 
+// Example 3:
+// Input: nums = [101]
+// Output: 1
+// Explanation:  
+// There is only one element in the array, which makes it both the minimum and maximum element.
+// We can remove it with 1 deletion.
 
 var minimumDeletions = function(nums) {
     let result;
