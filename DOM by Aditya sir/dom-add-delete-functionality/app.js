059679b4500d1cc910802f86fdc143e5c8ca79ba
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-08 11:16:48 +0530

    dom-add-delete

--- DOM by Aditya sir/dom-add-delete-functionality/app.js
+++ DOM by Aditya sir/dom-add-delete-functionality/app.js
@@ -19,5 +19,15 @@ function addTodo() {
   arr.push(input);
 }
 
-
+function deleteTodo(e) {
+    if (e.target.classList.contains("delete")) {
+      e.target.parentNode.remove();
+  
+      const input = e.target.previousElementSibling.innerText;
+      const index = arr.indexOf(input);
+      arr.splice(index, 1);
+    }
+  }
+  
+  showData.addEventListener("click", deleteTodo);
 addBtn.addEventListener("click", addTodo);
