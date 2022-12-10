e8c6e8225114ae7a36c9ddb9e311809f39859eca
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-10 15:18:28 +0530

    Power of Two

--- leetcode-problem-try/Power-of-Two.js
+++ leetcode-problem-try/Power-of-Two.js
@@ -15,13 +15,17 @@
 // Input: n = 3
 // Output: false
 
+var isPowerOfTwo = function (n) {
+  if (n == 0) return false;
+  while (n % 2 == 0) {
+    n = n / 2;
+  }
+  return n == 1;
+};
 
+var isPowerOfTwo = function (n) {
+  if (n <= 0) return false;
+  else if ((n & (n - 1)) == 0) return true;
 
-
-var isPowerOfTwo = function(n) {
-    // if(n==0) return false;
-    // while(n%2 == 0){
-    //     n=n/2    
-    // }
-    // return n==1
+  return false;
 };
