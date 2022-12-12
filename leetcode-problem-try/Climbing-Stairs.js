0980d4a5b70943c50cbbb3d6727ff2d61c2398ad
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-12 23:33:08 +0530

    Climbing-Stairs

---
+++ leetcode-problem-try/Climbing-Stairs.js
@@ -0,0 +1,40 @@
+// Q. Climbing Stairs
+// You are climbing a staircase. It takes n steps to reach the top.
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+// Example 2:
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+
+
+var climbStairs = function(n) {
+    if(n==1 || n==0){
+        return 1
+    }
+    let first=1;
+    let sec=2;
+    let third;
+    for(let i=3;i<=n;i++){
+        third=first+sec;
+        first=sec;
+        sec=third;
+    }
+    return sec;
+};
+console.log(climbStairs(2))
+console.log(climbStairs(3))
+console.log(climbStairs(4))
+console.log(climbStairs(5))
+console.log(climbStairs(6))
