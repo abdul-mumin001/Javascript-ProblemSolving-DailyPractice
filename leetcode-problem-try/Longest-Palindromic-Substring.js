d8125b6194aa83a7982acd86fdf8142e78417032
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-13 22:34:41 +0530

    Longest Palindromic Substring

---
+++ leetcode-problem-try/Longest-Palindromic-Substring.js
@@ -0,0 +1,40 @@
+// Q. Longest Palindromic Substring
+// Given a string s, return the longest 
+// palindromic
+// substring
+//  in s.
+
+// Example 1:
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+// Input: s = "cbbd"
+// Output: "bb"
+
+
+
+var longestPalindrome = function(s) {
+    if (s.length < 1) return "";
+    let maxSubStart = 0;
+    let maxSubLength = 0;
+    for (let i = 0; i < s.length; i++) {
+      const lengthCenteredAtChar = expandAroundCenter(s, i, i);
+      const lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1);
+      const longestSubAtChar = Math.max(lengthCenteredAtChar, lengthCenteredAtSpace)
+      if (longestSubAtChar > maxSubLength) {
+        maxSubLength = longestSubAtChar;
+        maxSubStart = i - Math.floor((maxSubLength - 1) / 2);
+      }
+    }
+    return s.substr(maxSubStart, maxSubLength);
+  }
+  
+  function expandAroundCenter(s, left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+      
+  };
