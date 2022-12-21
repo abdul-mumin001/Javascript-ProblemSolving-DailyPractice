91ceefc3eec40cd2f38c629297fd3e7b10314501
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-12-21 23:28:59 +0530

    Max Chunks To Make Sorted

---
+++ leetcode-problem-try/Max-Chunks-To-Make-Sorted.js
@@ -0,0 +1,32 @@
+// Q. Max Chunks To Make Sorted
+// You are given an integer array arr of length n that represents a permutation of the integers in the range [0, n - 1].
+// We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
+// Return the largest number of chunks we can make to sort the array.
+
+//  Example 1:
+// Input: arr = [4,3,2,1,0]
+// Output: 1
+// Explanation:
+// Splitting into two or more chunks will not return the required result.
+// For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+// Example 2:
+// Input: arr = [1,0,2,3,4]
+// Output: 4
+// Explanation:
+// We can split into two chunks, such as [1, 0], [2, 3, 4].
+// However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible.
+
+
+
+var maxChunksToSorted = function(arr) {
+    let count = 0;
+    let curr_max = -1;
+    
+    for(let i = 0; i < arr.length; ++i){
+            if(curr_max < arr[i])
+                curr_max = arr[i];
+            if(curr_max == i)
+                ++count;
+        }
+        return count;    
+};
