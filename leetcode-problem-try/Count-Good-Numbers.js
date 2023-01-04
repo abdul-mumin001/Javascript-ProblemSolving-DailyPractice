1bfd74ece9331ba9ac0610baa653ba09f06cc925
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-04 22:02:49 +0530

    Count-Good-Numbers

--- leetcode-problem-try/Count-Good-Numbers.js
+++ leetcode-problem-try/Count-Good-Numbers.js
@@ -39,3 +39,26 @@ function moduloPower(base, exp, mod) {
 console.log(countGoodNumbers(4))
 console.log(countGoodNumbers(3))
 console.log(countGoodNumbers(50))
+
+
+
+
+var countGoodNos = function(n) {
+    const mod = 1000000007n
+    const count_5 = Math.ceil(n / 2)
+    const count_4 = n - count_5
+    const pow_5 = moduloPower(5, count_5, mod)
+    const pow_4 = moduloPower(4, count_4, mod)
+
+    const ans = (pow_5 * pow_4) % mod
+    return Number(ans)
+};
+
+function moduloPower(base, exp, mod) {
+    if (exp === 0) return 1n
+    let a = moduloPower(base, Math.floor(exp / 2), mod)
+    a *= a
+    if (exp % 2 === 1) a *= BigInt(base)
+    return a % mod
+}
+
