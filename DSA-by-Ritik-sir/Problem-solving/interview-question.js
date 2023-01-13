3badea3cd44ea323757bb97d169f87b643894144
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-13 11:45:02 +0530

    practice-interview-question

--- DSA-by-Ritik-sir/Problem-solving/interview-question.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-question.js
@@ -40,3 +40,78 @@
 
 
 //7
+// let data=10 - -10;
+// console.log(data);
+
+
+
+//8
+// const set = new Set([1,1,2,3,4]);
+// console.log(set);
+
+
+//9
+// let data = {name:"mumin"};
+// console.log(delete data.name)
+// console.log(data)
+
+
+
+//10
+// let data={ name:"mumin"}
+// console.log(delete data)
+
+
+//11
+// const data=["mumin","abdul","aziz"];
+// const [x,y,z]=data;
+// console.log(x,z,y);
+
+
+
+// 12
+//how to get age without .operator.
+
+// const data={
+//     name:"mumin",
+//     age:23,
+//     skill:"js"
+// };
+// const {age}=data;
+// console.log(age)
+// //by using .operator
+// console.log(data.age)
+
+
+
+
+//13  how to merge both the obj.
+// let data={name:"mumin",age:23,skill:"js"}
+// let info={city:"Silchar",mail:"abdulmuminbar@gmail.com"};
+// let da={...data,...info};
+// console.log(da);
+
+
+
+
+//14 if it is same key in both obj then always prefer next value.
+// let data={name:"mumin",age:23,skill:"js"}
+// let info={city:"Silchar",skill:"Node"};
+// data={...data,...info};
+// console.log(data)
+
+
+
+
+//15
+// const ans=false || {} || null;
+// console.log(ans);
+
+
+
+//16
+// console.log(Promise.resolve(5))
+
+
+//17
+
