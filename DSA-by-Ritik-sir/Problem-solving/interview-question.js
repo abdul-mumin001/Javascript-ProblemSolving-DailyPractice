7790a88cbcffc14b9e5e86b593490870879d3d4f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-13 10:26:33 +0530

    practice-interview-question

--- DSA-by-Ritik-sir/Problem-solving/interview-question.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-question.js
@@ -1,2 +1,42 @@
-console.log(3+2+'7')
+//1
+// console.log(3+2+'7')
 
+
+//2
+// let a=[];
+// let b=[];
+// console.log(a==b);
+// console.log(a===b);
+
+
+
+
+//3
+// let a=[];
+// let b=a;
+// console.log(a==b);
+// console.log(a===b);
+
+
+
+//4
+// let a=[20];
+// let b=[20];
+// console.log(a[0]==b[0]);
+// console.log(a[0]===b[0]);
+
+
+
+//5
+// let z=[1,2,3,4,5];
+// let a={name:"anil"};
+// console.log(...z);
+
+
+
+//6
+// console.log(typeof NaN)
+
+
+
+//7
