5f6862198a52c268a0fa679f869308489218c599
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-21 20:51:58 +0530

    interview revision

--- DSA-by-Ritik-sir/Problem-solving/interview-question.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-question.js
@@ -589,3 +589,21 @@
 
 
 
+
+
+
+// re-interview practice
+// ----------------------------
+
+// window and this keyword
+
+var a=10;
+ 
+function b(){
+    var x=2;
+}
+
+console.log(a)
+console.log(window.a)
+console.log(this.a)
+console.log(this===window);
