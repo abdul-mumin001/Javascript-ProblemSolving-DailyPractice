55ef64ca439fbd33f98502bf61c8a088a0c27de5
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-22 10:17:08 +0530

    interview revision

--- DSA-by-Ritik-sir/Problem-solving/interview-question.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-question.js
@@ -1,15 +1,11 @@
 //1
 // console.log(3+2+'7')
 
-
 //2
 // let a=[];
 // let b=[];
 // console.log(a==b);
 // console.log(a===b);
-
-
-
 
 //3
 // let a=[];
@@ -17,57 +13,41 @@
 // console.log(a==b);
 // console.log(a===b);
 
-
-
 //4
 // let a=[20];
 // let b=[20];
 // console.log(a[0]==b[0]);
 // console.log(a[0]===b[0]);
 
-
-
 //5
 // let z=[1,2,3,4,5];
 // let a={name:"anil"};
 // console.log(...z);
 
-
-
 //6
 // console.log(typeof NaN)
-
-
 
 //7
 // let data=10 - -10;
 // console.log(data);
 
-
-
 //8
 // const set = new Set([1,1,2,3,4]);
 // console.log(set);
-
 
 //9
 // let data = {name:"mumin"};
 // console.log(delete data.name)
 // console.log(data)
 
-
-
 //10
 // let data={ name:"mumin"}
 // console.log(delete data)
-
 
 //11
 // const data=["mumin","abdul","aziz"];
 // const [x,y,z]=data;
 // console.log(x,z,y);
-
-
 
 // 12
 //how to get age without .operator.
@@ -82,17 +62,11 @@
 // //by using .operator
 // console.log(data.age)
 
-
-
-
 //13  how to merge both the obj.
 // let data={name:"mumin",age:23,skill:"js"}
 // let info={city:"Silchar",mail:"abdulmuminbar@gmail.com"};
 // let da={...data,...info};
 // console.log(da);
-
-
-
 
 //14 if it is same key in both obj then always prefer next value.
 // let data={name:"mumin",age:23,skill:"js"}
@@ -100,18 +74,12 @@
 // data={...data,...info};
 // console.log(data)
 
-
-
-
 //15
 // const ans=false || {} || null;
 // console.log(ans);
 
-
-
 //16
 // console.log(Promise.resolve(5))
-
 
 //17
 // let name="abdul";
@@ -121,22 +89,16 @@
 // }
 // getName();
 
-
-
-
 // 18
 // function sumValues(x,y,z){
 // return x+y+z;
 // }
 // console.log(sumValues(...[1,2,3]))
 
-
 //19
 // const name="step by step code";
 // console.log(!typeof name==='object')
 // console.log(!typeof name==="string")
-
-
 
 //20
 // let person={
@@ -146,34 +108,25 @@
 // person.name="abdul"
 // console.log(person)
 
-
-
-
 // 21 remove first element
 // let data=[5,6,7,8,9];
 // data.shift()
 // console.log(data)
-
-
 
 //22 add from the first
 // let data=[1,2,3,4]
 // data.unshift(1)
 // console.log(data)
 
-
 // 23 remove from the last
 // let data=[1,2,3,4,5];
 // data.pop()
 // console.log(data)
 
-
 // 24 add in the end
 // let data=[1,2,3,4,5];
 // data.push(6)
 // console.log(data)
-
-
 
 // 25 check any value is odd or even
 // let a=32;
@@ -183,18 +136,12 @@
 //     console.log('odd')
 // }
 
-
-
 // 26 convert data to boolean false value
 // let data="string"
 // console.log(typeof !data)
 
-
-
-
 // 27 differnce bet map and forEach
 //  map function hamesha kuch na kuch return karega but forEach kuch bhi return nhi karega
-
 
 // 28
 // let data=["abdul","mumin","barbhuiya"];
@@ -208,8 +155,6 @@
 // console.log(data.slice(0,2))
 // console.log(data);
 
-
-
 // 29
 // let a=2;
 // setTimeout(()=>{
@@ -217,20 +162,14 @@
 // },0);
 // a=100;
 
-
-
 // 30
 // console.log(a)
 // var a;
 
-
-
 // 31
 // console.log([[[[]]]])
 
-
-// 32 how to find os  name 
-
+// 32 how to find os  name
 
 // 33
 // function furit(){
@@ -241,7 +180,6 @@
 // }
 // furit()
 
-
 // 34
 // for(var i=0;i<3;i++){
 //     console.log(i)
@@ -250,9 +188,6 @@
 //     },1)
 // }
 
-
-
-
 //35
 // for(let i=0;i<3;i++){
 //         // console.log(i)
@@ -260,15 +195,10 @@
 //             console.log(i)
 //         },1)
 //     }
-    
-
-
 
 // 36
 // console.log(+true)
 // console.log(typeof +true);
-    
-
 
 //37
 // let data="size";
@@ -280,8 +210,6 @@
 // console.log(bird.size);
 // console.log(bird.data);
 
-
-
 // 38
 // let a={name:"abdul"}
 // let b;
@@ -289,15 +217,11 @@
 // a.name="mumin"
 // console.log(b.name);
 
-
-
-
 // // 39
 // var a;
 // let a=20;
 // console.log(a)
 // // error like Identifier 'a' has already been declared
-
 
 // //40
 // let a=3;
@@ -306,17 +230,11 @@
 // console.log(a==b);
 // console.log(a===b);
 
-
-
 // 41
 // let a;
 // let nam={};
 // console.log(a)
 // console.log(nam);
-
-
-
-
 
 // 42
 // function sum(a,b){
@@ -324,17 +242,11 @@
 // }
 // console.log(sum(1,'2'))
 
-
-
-
 // 43
 // let i=0;
 // console.log(i++)
 // console.log(++i);
 // console.log(i);
-
-
-
 
 // 44
 // const sum=eval(`10*10+5`)
@@ -344,13 +256,11 @@
 // const obj={a:"one",b:"two",a:"repeat"}
 // console.log(obj)
 
-
 // 46
 // for(let i=1;i<5;i++){
 //     if(i===3) continue;
 //     console.log(i)
 // }
-
 
 // 47
 // const foo = ()=> console.log('First');
@@ -359,8 +269,6 @@
 // bar();
 // foo();
 // baz();
-
-
 
 // 48
 // const person ={name:"abdul"};
@@ -372,12 +280,8 @@
 // console.log(sayHi.call(person,23));
 // console.log(sayHi.bind(person,21)());
 
-
-
-// 49 convert string to array 
+// 49 convert string to array
 // console.log([..."mumin"]);
-
-
 
 // 50
 // const firstPromise= new Promise((resolve,reject)=>{
@@ -389,8 +293,6 @@
 
 // Promise.race([firstPromise,secondPromise]).then(resolve=>console.log(resolve));
 
-
-
 // // 51
 // const person={
 //     name:"mumin",
@@ -400,17 +302,11 @@
 //     console.log(key)
 // }
 
-
-
-
 // 52
 // console.log(Object.values(100))
 
-
 // 53
 // console.log(3 + 4 + +'5')
-
-
 
 // 54
 // let data=[1,2,3].map(num=>{
@@ -419,12 +315,9 @@
 // })
 // console.log(data)
 
-
 // 55
 // console.log(!true -true)
 // console.log(true + +'10')
-
-
 
 // //56
 // // Coercion
@@ -435,66 +328,56 @@
 //  // Closures
 //  // Currying
 //  // Promises
- 
- 
+
 //  //Promises
- 
+
 //  new Promise((resolve, reject) => {
 //    // Promise Syntax
-//  }) 
- 
- 
+//  })
+
 // //  States of Promise
 //  pending
 //  fullfilled
 //  rejected
-  
- 
- 
+
 //  Handlers
 //  .then
 //  .catch
-  
- 
- 
+
 // //  .catch handler does not catch errors occured in asynchronous functions like setTimeout
 
- 
 //  const promise =  new Promise((resolve, reject) => {
 //     setTimeout(() => {
 //        reject("Error")
 //     })
 //  })
- 
-//  console.log(promise); 
- 
- 
+
+//  console.log(promise);
+
 //   promise.then((result) => console.log(result))
-//         .catch((err) => console.log(err)) 
- 
- 
+//         .catch((err) => console.log(err))
+
 //   async function handlePromise(){
- 
+
 //     try {
 //        const result = await promise;
 //        console.log(result);
 //     } catch(err){
 //        console.log(err)
 //     }
- 
+
 //  }
- 
+
 //  handlePromise().catch(err => console.log(err));
-  
- 
+
 //  //Currying
- 
+
 //  // Transfomation of the function of multiple arguments into a function of single arguments in sequence
- 
+
 //   function sum(a, b){
 //     return a + b;
 //  }
- 
+
 //  function curryFunc(func){
 //     return function foo(a){
 //        return function bar(b){
@@ -502,95 +385,81 @@
 //        }
 //     }
 //  }
- 
+
 //  curryFunc(sum)(2)(5)
- 
+
 //  const addTwo = curryFunc(sum)(2);
 //  const addFive = curryFunc(sum)(5);
- 
+
 //  console.log(addTwo(10));
-//  console.log(addFive(10)); 
- 
+//  console.log(addFive(10));
+
 //  // Lexical Environment
- 
- 
+
 //  //closuer
 //  //functions bundled together with its lexical environment;
- 
+
 //  function foo(){
 //     let a = 2;
 //     return function bar(){
 //        console.log(a);
 //     }
 //  }
- 
+
 // //  const b = foo();
-//  b(); 
- 
- 
+//  b();
+
 //  //NaN
 // //   let a = "Hello"
 //  console.log(NaN);
-  
- 
- 
+
 //  //This keyword
- 
+
 //  const obj = {
 //     a: "12",
-//     hello(){ 
+//     hello(){
 //        console.log(this);
 //     }
 //  }
- 
- 
+
 //  const obj2 = {
 //     b: "aircampus"
 //  };
-  
+
 //  obj.hello.call(obj2); // List of parameters is sent
 //  obj.hello.apply(obj2); // Array of parameters is sent
 //  const newHello = obj.hello.bind(obj2);
 //  newHello();
- 
- 
+
 //  //Coercion
- 
+
 //  let a =  "5";
 //  let b = 12;
- 
+
 //  // Implicit Coercion
 //  let c = a + b;
 //  console.log(typeof c) //String
- 
+
 //  // Explicit Coering
 //  let d = Number(a) + b;
 //  console.log(typeof d) //Number
-  
- 
- 
+
 //  //Boolena and Logical Coercion
 // //  var x = 220;
 // //  var y = "Hello";
 // //  var z = undefined;
-         
+
 //  // x | | y    // Returns 220 since the first value is truthy
-         
+
 //  // x | | z   // Returns 220 since the first value is truthy
-         
+
 //  // x && y    // Returns "Hello" since both the values are truthy
-         
+
 //  // y && z   // Returns undefined since the second value is falsy
-         
- 
-// //  if( x || y ){ 
+
+// //  if( x || y ){
 // //    console.log("Code runs" ); // This block runs because x && y returns "Hello" (Truthy)
 // //  }
-
-
-
-
-
 
 // re-interview practice
 // ----------------------------
@@ -598,7 +467,7 @@
 // window and this keyword
 
 // var a=10;
- 
+
 // function b(){
 //     var x=2;
 // }
@@ -608,18 +477,13 @@
 // console.log(this.a)
 // console.log(this===window);
 
+// Higher  Order Function
 
-
-
-// Higher  Order Function 
-
-// A function which takes another function as an argument or  return a function is known as Higher order function. 
-
-
+// A function which takes another function as an argument or  return a function is known as Higher order function.
 
 // eg:
 
-let arr=[1,2,3,4,5,6,7,8,9]
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // function FilterOdd (){
 //     let FilterArr=[];
 //     for(let i=0;i<arr.length;i++){
@@ -629,7 +493,7 @@ let arr=[1,2,3,4,5,6,7,8,9]
 //     }
 //     return FilterArr;
 // }
-   
+
 // console.log(FilterOdd(arr))
 
 // function FilterEven (){
@@ -643,8 +507,6 @@ let arr=[1,2,3,4,5,6,7,8,9]
 // }
 // console.log(FilterEven(arr))
 
-
-
 // function FilterFunction(arr,callBack){
 //     let FilterArr=[];
 //     for(let i=0;i<arr.length;i++){
@@ -652,7 +514,6 @@ let arr=[1,2,3,4,5,6,7,8,9]
 //     }
 //     return FilterArr;
 // }
-
 
 // function isOdd(x){
 //     return x%2 != 0;
@@ -663,3 +524,72 @@ let arr=[1,2,3,4,5,6,7,8,9]
 
 // console.log(FilterFunction(arr,isOdd));
 // console.log(FilterFunction(arr,isEven));
+
+//callBack function
+
+//A callBack is a function which passed as an argument to another function.The technique allows a function to call another function.
+
+// function sayHello (){
+//     console.log("Hello Mumin")
+// }
+// setTimeout(sayHello,1000);
+// console.log("Hey!!")
+
+
+
+
+
+// clouser
+
+// A clouser is the combination of function bundled together with refference to its surrounding state (the lexical environment). 
+// In other words clouser gives you an access of outer function scope from an inner function scope.
+
+
+// lexical environmemt eg:
+
+// function init(){
+//     var name="Mumin"
+//     function displayName(){
+//         console.log(name)
+//     }
+//     displayName()
+// }
+// init()
+
+
+
+// // clouser
+
+// function makeFunc(){
+//     var name='Mumin';
+//     function displayName(){
+//         console.log(name)
+//     }
+//     return displayName;
+// }
+// const myFunc=makeFunc();
+// myFunc();
+
+
+
+
+
+//IIFE
+// An IIFE (immediately Invoked Function Expression)  is  javascript function that runs as soon as it is defined.
+// The name IIFE is promted by Ben Alman in his blog. 
+
+// eg:
+// (()=>{
+// console.log('hello');
+// })()
+
+// using the IIFE result 
+// const result=(()=>{
+//      const name="Aziz";
+//      return name;
+// })();
+// console.log(result);
+
+
+
+
