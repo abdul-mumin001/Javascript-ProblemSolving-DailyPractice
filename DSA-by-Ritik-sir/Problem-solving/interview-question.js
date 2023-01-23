858c7d75689c3af3715ee6e54124f8d64e32158a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-23 22:14:46 +0530

    interview revision

--- DSA-by-Ritik-sir/Problem-solving/interview-question.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-question.js
@@ -666,4 +666,66 @@
 
 
 
+// use of map
+
+// const arr=[1,2,3]
+
+// function double(x){
+//     return x*2;
+// }
+
+// const output=arr.map(double);
+// console.log(output);
+
+
+
+// binary 
+
+// function binary(x){
+//    return x.toString(2);
+// }
+//  const output=arr.map(binary);
+//  console.log(output);
+
+
+
+
+// filter function
+
+// const arr=[1,2,3]
+
+// function isOdd(x){
+//     return x%2 !=0;
+// }
+
+// const output=arr.filter(isOdd)
+// console.log((output));
+
+
+
+// reduce function 
+
+// function findSum(){
+//     let sum=0;
+//     for(let i=0;i<arr.length;i++){
+//         sum=sum+arr[i]
+//     }
+//     return sum;
+// }
+// console.log(findSum(arr));
+
+
+
+// const output=arr.reduce(function(acc,curr){
+//     acc=acc+curr;
+//     return acc;
+// },0)
+// console.log(output);
+
+
+
+
+
+
+
 
