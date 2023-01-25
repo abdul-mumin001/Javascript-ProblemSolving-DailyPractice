489349eb6321125bb0a9b1f2452623d5dfaf44a1
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-25 22:59:17 +0530

    Search in Rotated Sorted Array

--- leetcode-problem-try/Search-in-Rotated-Sorted-Array.js
+++ leetcode-problem-try/Search-in-Rotated-Sorted-Array.js
@@ -32,3 +32,7 @@ var search = function(nums, target) {
        return -1
     };
    
+    console.log(search([4,5,6,7,0,1,2],3));
+    console.log(search([4,5,6,7,0,1,2],0));
+    console.log(search([1],0));
+    
