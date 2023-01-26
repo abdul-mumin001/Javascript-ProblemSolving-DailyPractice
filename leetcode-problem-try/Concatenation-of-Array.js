1a8c2091994955a71d0c4abea14f6ff470ee28c8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-26 23:31:22 +0530

    Concatenation of Array

--- leetcode-problem-try/Concatenation-of-Array.js
+++ leetcode-problem-try/Concatenation-of-Array.js
@@ -40,3 +40,6 @@ console.log(getConcatenation([1,3,2,1]));
 var getConcatenate = function(nums) {
     return nums.concat(nums)
 };
+
+console.log(getConcatenate([1,2,1]));
+console.log(getConcatenate([1,3,2,1]));
