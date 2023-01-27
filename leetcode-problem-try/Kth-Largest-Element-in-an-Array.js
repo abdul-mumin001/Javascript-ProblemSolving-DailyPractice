47423ee2ed0d972f139fa1ce44689ad8fa92735b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-27 22:56:47 +0530

    Kth Largest Element in an Array

--- leetcode-problem-try/Kth-Largest-Element-in-an-Array.js
+++ leetcode-problem-try/Kth-Largest-Element-in-an-Array.js
@@ -14,8 +14,59 @@
 
 
 
-var findKthLargest = function(nums, k) {
-    return nums.sort(function(a,b) {return b-a})[k-1]
-};
+// var findKthLargest = function(nums, k) {
+//     return nums.sort(function(a,b) {return b-a})[k-1]
+// };
     
-console.log(findKthLargest([3,2,1,5,6,4],2));
+// console.log(findKthLargest([3,2,1,5,6,4],2));
+
+
+// 2nd method with all downwords code
+
+ var findKthLargest = function(nums, k) {
+    
+     
+    quickSort(nums, 0, nums.length, k)
+    return nums[k - 1]
+
+};
+function quickSort(nums, start, end, k) {
+  if (end <= start) return
+  
+  let mid = Math.floor((start + end) / 2)
+  let left = start, right = end
+  
+  while(left < right) {
+    while (left < mid) {
+      if (nums[left] < nums[mid]) {
+        swap(nums, left, mid)
+        mid = left
+      } else {
+        left++
+      }
+    }
+    
+    while(right > mid) {
+      if (nums[right] > nums[mid]) {
+        swap(nums, right, mid)
+        mid = right
+      } else {
+        right--
+      }
+    }
+  }
+  
+  if (mid === k - 1) return
+  if (mid < k - 1) {
+    quickSort(nums, mid, end, k)
+  } else {
+    quickSort(nums, start, mid, k)
+  }
+  
+}
+
+function swap(nums, i, j) {
+  const tmp = nums[i]
+  nums[i] = nums[j]
+  nums[j] = tmp
+}
