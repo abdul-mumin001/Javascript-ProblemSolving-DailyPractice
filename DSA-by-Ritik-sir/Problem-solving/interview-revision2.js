dbe0a9412db1478b4c485f1ae88fb6eb8b8f6e7f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-29 22:08:21 +0530

    Javascript revision"

---
+++ DSA-by-Ritik-sir/Problem-solving/interview-revision2.js
@@ -0,0 +1,35 @@
+// Types of function
+
+// function statement
+
+//func defination
+function sayHello(para){
+    console.log('hello',para);
+
+    // return 'mumin'
+}
+// func invocation
+sayHello('abdul')
+// console.log("```````````");
+
+// let rVal=sayHello()
+// console.log('rval',rVal);
+
+
+// function are first class citizens:
+//function are treated as a variable
+
+// function expression
+let fnContainer = function fn(){
+    console.log("function expression");
+}
+fnContainer()
+
+// Anonymous function: function which doesn't need name
+let fnCont = function (){
+    console.log("Anonymous function");
+}
+fnCont()
+
+
+
