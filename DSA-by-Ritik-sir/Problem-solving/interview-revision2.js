c9230d00bb1629f755195940a5182ce92323341b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-01-30 23:24:27 +0530

    Javascript revision"

--- DSA-by-Ritik-sir/Problem-solving/interview-revision2.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-revision2.js
@@ -40,3 +40,34 @@
 // })()
 
 
+
+// First class citizen 
+// function are treated as a variable
+
+// 1.assignment -> variable can be passed as a parameter
+// 2.function can be passed a parameter -> hof,callback
+// variable can be return from a function
+// 3. function can be return from a function -> clouser
+
+
+// function sayHello(para){
+//     console.log('hello',para);
+//     return "mumin"
+// }
+// function smaller(){
+//     console.log('smaller');
+// }
+// sayHello([1,2,3])
+// let rVal=sayHello(smaller)
+// console.log(rVal);
+
+
+function outer(){
+    console.log("outer function");
+    return function inner(){
+        console.log("inner function");
+    }
+}
+let rVal=outer();
+console.log("rval",rVal);
+rVal()
