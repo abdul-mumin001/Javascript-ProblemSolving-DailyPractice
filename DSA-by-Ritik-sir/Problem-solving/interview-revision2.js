b913740d1570835ae499a1513176c1a549e7e291
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-02-03 00:23:53 +0530

    Javascript revision async await in js

--- DSA-by-Ritik-sir/Problem-solving/interview-revision2.js
+++ DSA-by-Ritik-sir/Problem-solving/interview-revision2.js
@@ -176,3 +176,32 @@ var promise = new Promise(function(resolve, reject) {
 		  console.log('Some error has occurred'); 
 	  });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//   Async/Await Function in JavaScript
+
+	// Async: It simply allows us to write promises-based code as if it was synchronous and it checks that we are not breaking 
+	// the execution thread. It operates asynchronously via the event loop. Async functions will always return a value. 
+	// It makes sure that a promise is returned and if it is not returned then JavaScript automatically wraps it in a promise 
+	// which is resolved with its value.
+
+
+	const getData = async() => {
+		var data = "Hello World";
+		return data;
+		}
+		  
+		getData().then(data => console.log(data));
+
