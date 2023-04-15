252a3c9e614c94d82c2b3ba422adeb0fa15898ec
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-15 23:44:03 +0530

    count operation to obtain zero

--- leetcode-problem-try/count-operation-to-obtain-zero.js
+++ leetcode-problem-try/count-operation-to-obtain-zero.js
@@ -1,5 +1,4 @@
 // Q.You are given two non-negative integers num1 and num2.
-
 // In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.
 
 // For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
@@ -29,7 +28,7 @@
 
 
 var countOperations = function(num1, num2) {
-    
+
     let count = 0
         while(num1 !== 0 && num2 !== 0){
             if(num1 > num2) num1 -= num2;
