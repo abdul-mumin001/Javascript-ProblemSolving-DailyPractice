be0e1093337b2a236b6fe1f8792f6cf224bba69c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-15 23:42:53 +0530

    interview preparation

--- leetcode-problem-try/count-operation-to-obtain-zero.js
+++ leetcode-problem-try/count-operation-to-obtain-zero.js
@@ -29,17 +29,6 @@
 
 
 var countOperations = function(num1, num2) {
-    //     let ans=0;
-    //    while(num1 !=0 && num2!=0){
-    //         if(num1>=num2){
-    //         num1=num1-num2;
-    //     }else{
-    //        num2=num2-num1;
-    //     }
-    //     ans++
-    //    }
-    //    return ans;
-    
     
     let count = 0
         while(num1 !== 0 && num2 !== 0){
