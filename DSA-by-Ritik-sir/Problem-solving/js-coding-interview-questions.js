28cd9e0d99ae0d273379079bb1eab0f4ae901a09
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-16 23:02:45 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -12,7 +12,21 @@ function modifyArray(arr, callback) {
     callback();
   }
   var arr = [1, 2, 3, 4, 5];
-  
+
   modifyArray(arr, function() {
     console.log("array has been modified", arr);
   });
+
+
+
+//   Q2: Given a string, reverse each word in the sentence
+// Ans-
+
+var string = "Welcome to this Javascript Guide!";
+// Output becomes !ediuG tpircsavaJ siht ot emocleW
+var reverseEntireSentence = reverseBySeparator(string, "");
+// Output becomes emocleW ot siht tpircsavaJ !ediuG
+var reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
+function reverseBySeparator(string, separator) {
+  return string.split(separator).reverse().join(separator);
+}
