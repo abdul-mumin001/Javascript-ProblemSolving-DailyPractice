c2a501d165c06d79c1fc7ae49f9de1e1bf4993cd
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-18 23:20:41 +0530

    problem solving

--- leetcode-problem-try/app.js
+++ leetcode-problem-try/app.js
@@ -381,6 +381,10 @@ return count / 2
 
 
 
+
+
+
+
 // Q. Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
  
@@ -439,6 +443,9 @@ var firstUniqChar = function(s) {
         }
         return -1
 };
+
+
+
 
 
 
