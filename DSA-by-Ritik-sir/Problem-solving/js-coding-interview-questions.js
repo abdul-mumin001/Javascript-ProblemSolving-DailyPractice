8c1600e486a76174d53162889e1f01db3a55d94c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-20 22:38:56 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -73,3 +73,31 @@ function isInt(num) {
 console.log(isInt(4));  // true
 console.log(isInt(12.2));  // false
 console.log(isInt(0.3));   // false
+
+
+
+// Q6: Implement enqueue and dequeue using only two stacks. 
+// Ans-
+
+
+var inputStack = [];  // First stack
+var outputStack = [];  // Second stack
+
+// For enqueue, just push the item into the first stack
+function enqueue(stackInput, item) {
+  return stackInput.push(item);
+}
+
+function dequeue(stackInput, stackOutput) {
+  // Reverse the stack such that the first element of the output stack is the
+  // last element of the input stack. After that, pop the top of the output to
+  // get the first element that was ever pushed into the input stack
+  if (stackOutput.length <= 0) {
+    while(stackInput.length > 0) {
+      var elementToOutput = stackInput.pop();
+      stackOutput.push(elementToOutput);
+    }
+  }
+
+  return stackOutput.pop();
+}
