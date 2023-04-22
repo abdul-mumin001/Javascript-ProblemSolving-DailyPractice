451dcbea180f9af6e8897e60e9cd78fd2ba248e3
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-22 23:21:43 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -149,6 +149,10 @@ function mul (x) {
 
 
 // Ans -
+// You can create a closure to keep the value passed to the function createBase even after the inner function is returned. The inner 
+// function that is being returned is created within an outer function, making it a closure, and it has access to the variables within
+//  the outer function, in this case the variable baseNumber.
+
 
 function createBase(baseNumber) {
   return function(N) {
