a0428e17b36bdcdc24dd70a9fb690e552bfcd557
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-24 00:39:40 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -164,3 +164,18 @@ function createBase(baseNumber) {
 var addSix = createBase(6);
 console.log(addSix(10));
 console.log(addSix(21));
+
+
+
+// Q10: FizzBuzz Challenge 
+// Problem
+// Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at
+// multiples of 3 and 5.
+
+
+for (let i = 1; i <= 100; i++) {
+  let f = i % 3 == 0,
+    b = i % 5 == 0;
+    
+  console.log(f ? (b ? 'FizzBuzz' : 'Fizz') : b ? 'Buzz' : i);
+}
