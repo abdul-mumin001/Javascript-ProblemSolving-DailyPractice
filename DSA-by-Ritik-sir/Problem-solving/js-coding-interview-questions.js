d743ffe9aca7169445bc18f155cae55f7a8b002c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-26 23:21:04 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -210,3 +210,29 @@ function isAnagram(first, second) {
 
   return a === b;
 }
+
+
+
+
+
+// Q12: How would you use a closure to create a private counter? 
+
+function counter() {
+  var _counter = 0;
+  // return an object with several functions that allow you
+  // to modify the private _counter variable
+  return {
+    add: function(increment) { 
+      _counter += increment; 
+    },
+    retrieve: function() { return 'The counter is currently at: ' + _counter; }
+  }
+}
+// usage of our counter function
+var c = counter();
+c.add(5); 
+c.add(9); 
+
+// now we can access the private variable in the following way
+console.log(c.retrieve()); 
+// The counter is currently at: 14
