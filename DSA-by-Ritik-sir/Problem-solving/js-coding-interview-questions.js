057b11cd54084818ec4ceca7d00b2668c333cdea
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-27 23:01:35 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -251,15 +251,15 @@ console.log(c.retrieve());   //output: The counter is currently at: 14
 
 // The specific list of "falsy" values in JavaScript is as follows:
 
- ""   //(empty string)
- 0, -0, NaN   //(invalid number)
- null, undefined
- false
+//  ""   //(empty string)
+//  0, -0, NaN   //(invalid number)
+//  null, undefined
+//  false
 // Any value that's not on this "falsy" list is "truthy." Here are some examples of those:
 
-"hello"
-42
-true
-[ ], [ 1, "2", 3 ]    //(arrays)
-{ }, { a: 42 }   //(objects)
-function foo() {..}   //(functions)
+// "hello"
+// 42
+// true
+// [ ], [ 1, "2", 3 ]    //(arrays)
+// { }, { a: 42 }   //(objects)
+// function foo() {..}   //(functions)
