d495d22142dd27e5d9ae20589c35b8a4d7375a4c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-04-29 22:14:23 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -305,3 +305,14 @@ console.log(k);  // output 1function
 })();
 
 console.log(b);
+// Ans-
+// The code above will output 5 even though it seems as if the variable was declared within a function and can't be accessed outside 
+// of it. This is because
+
+var a = b = 5;
+// is interpreted the following way:
+
+var a = b;
+b = 5;
+// But b is not declared anywhere in the function with var so it is set equal to 5 in the global scope.
+
