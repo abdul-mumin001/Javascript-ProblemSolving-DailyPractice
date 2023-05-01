bad648f985a8196973db1277c3defb8097e6878e
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-01 22:37:23 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
+++ DSA-by-Ritik-sir/Problem-solving/js-coding-interview-questions.js
@@ -325,6 +325,9 @@ b = 5;
 // multiply(5)(6);
 
 // Ans-
+// You can create a closure to keep the value of a even after the inner function is returned. The inner function that is being returned
+// is created within an outer function, making it a closure, and it has access to the variables within the outer function, in this case 
+// the variable a.
 
 function multiply(a) {
   return function(b) {
