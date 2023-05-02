121c09775033b4f5286690ddb266f819a2ad8f04
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-02 21:16:35 +0530

    problem solving

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -3,22 +3,22 @@
 // JavaScript provides a typeof operator that can examine a value and tell you what type it is:
 
 var a;
-typeof a;				// "undefined"
+console.log(typeof a);				// "undefined"
 
 a = "hello world";
-typeof a;				// "string"
+console.log(typeof a);				// "string"
 
 a = 42;
-typeof a;				// "number"
+console.log(typeof a);				// "number"
 
 a = true;
-typeof a;				// "boolean"
+console.log(typeof a);				// "boolean"
 
 a = null;
-typeof a;				// "object" -- weird, bug
+console.log(typeof a);				// "object" -- weird, bug
 
 a = undefined;
-typeof a;				// "undefined"
+console.log(typeof a);			// "undefined"
 
 a = { b: "c" };
-typeof a;				// "object"
+console.log(typeof a);				// "object"
