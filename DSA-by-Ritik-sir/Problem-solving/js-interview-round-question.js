08831474629a9be141a5bea8821fcc68ef8c1872
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-05 22:26:34 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -59,3 +59,13 @@ arr[2];			//output: true
 arr.length;		//output: 3
 
 typeof arr;		//output: "object"
+
+
+
+// Q4:   What is Scope in JavaScript? 
+// Ans-
+
+// In JavaScript, each function gets its own scope. Scope is basically a collection of variables as well as the rules for how those 
+// variables are accessed by name. Only code inside that function can access that function's scoped variables.
+//          A variable name has to be unique within the same scope. A scope can be nested inside another scope. If one scope is nested 
+// inside another, code inside the innermost scope can access variables from either scope.
