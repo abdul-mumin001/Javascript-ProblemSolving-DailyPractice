5cc591e63d699d480ca0b8072e7c5bcef868a251
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-07 23:10:11 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -105,4 +105,22 @@ a === b;		// false
 // If the guess was too high, set max to be one smaller than the guess.
 // Go back to step two.
 
+// Implementation
 
+var binarySearch = function(array, value) {
+    var guess,
+        min = 0,
+        max = array.length - 1;	
+
+    while(min <= max){
+        guess = Math.floor((min + max) /2);
+	if(array[guess] === value)
+	    return guess;
+	else if(array[guess] < value)
+	    min = guess + 1;
+	else
+	    max = guess - 1;	
+     }
+	
+     return -1;
+}
