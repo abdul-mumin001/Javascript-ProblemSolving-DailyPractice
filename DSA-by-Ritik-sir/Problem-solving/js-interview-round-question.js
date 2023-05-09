e595b71fdb2076bbb40566472a4e370f217b6774
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-09 23:41:00 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -175,3 +175,8 @@ function linearSearch(array, toFind){
 
 
 
+// Q9: What is let keyword in JavaScript?  
+
+// Ans-
+// In addition to creating declarations for variables at the function level, ES6 lets you declare variables to belong to individual 
+// blocks (pairs of { .. }), using the let keyword.
