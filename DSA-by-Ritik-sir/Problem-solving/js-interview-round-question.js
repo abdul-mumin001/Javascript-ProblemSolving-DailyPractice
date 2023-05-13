b7c9f35eeff0c484129cd1dd05dc6bd6da9c01e6
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-13 23:46:41 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -286,3 +286,28 @@ function linearSearch(array, toFind){
 // early.
 // Most of the time, I would prefer the .forEach method, but it really depends on what you are trying to do. for loops allow more
 // flexibility, such as prematurely terminate the loop using break or incrementing the iterator more than once per loop.
+
+
+
+
+
+// Q17:   What are some of the advantages/disadvantages of writing JavaScript code in a language that compiles to JavaScript?
+// Ans-
+// Some examples of languages that compile to JavaScript include CoffeeScript, Elm, ClojureScript, PureScript, and TypeScript.
+// Advantages:
+// Fixes some of the longstanding problems in JavaScript and discourages JavaScript anti-patterns.
+// Enables you to write shorter code, by providing some syntactic sugar on top of JavaScript, which I think ES5 lacks, but ES2015 is 
+// awesome.
+// Static types are awesome (in the case of TypeScript) for large projects that need to be maintained over time.
+
+// Disadvantages:
+// Require a build/compile process as browsers only run JavaScript and your code will need to be compiled into JavaScript before being 
+// served to browsers.
+// Debugging can be a pain if your source maps do not map nicely to your pre-compiled source.
+// Most developers are not familiar with these languages and will need to learn it. There's a ramp up cost involved for your team if 
+// you use it for your projects.
+// Smaller community (depends on the language), which means resources, tutorials, libraries, and tooling would be harder to find.
+// IDE/editor support might be lacking.
+// These languages will always be behind the latest JavaScript standard.
+// Developers should be cognizant of what their code is being compiled to — because that is what would actually be running, and that is 
+// what matters in the end.
