45d5e4ec29e904df42a4878cfd04a7c48b6b5ec2
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-14 23:17:52 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -331,21 +331,21 @@ function linearSearch(array, toFind){
 
 
 
-// Q19:   What does use strict do?
+// Q19:What does use strict do?
 
 // Ans-
 // The use strict literal is entered at the top of a JavaScript program or at the top of a function and it helps you write safer JavaScript
 //  code by throwing an error if a global variable is created by mistake. For example, the following program will throw an error:
 
-function doSomething(val) {
-  "use strict"; 
-  x = val + 10;
-}`
+// function doSomething(val) {
+//   "use strict"; 
+//   x = val + 10;
+// }`
 
 // It will throw an error because x was not defined and it is being set to some value in the global scope, which isn't allowed with use 
 // strict The small change below fixes the error being thrown:
 
-function doSomething(val) {
-  "use strict"; 
-  var x = val + 10;
-}
+// function doSomething(val) {
+//   "use strict"; 
+//   var x = val + 10;
+// }
