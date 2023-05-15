b0e7704c2ff22477acb5e7461b16a478803e051b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-15 23:07:17 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -349,3 +349,13 @@ function linearSearch(array, toFind){
 //   "use strict"; 
 //   var x = val + 10;
 // }
+
+
+
+
+
+// Q20:   Why is it, in general, a good idea to leave the global scope of a website as-is and never touch it?
+
+// Ans-
+// Every script has access to the global scope, and if everyone uses the global namespace to define their variables, collisions will likely
+// occur. Use the module pattern (IIFEs) to encapsulate your variables within a local namespace.
