1cd6bff0169edee45a06012646d4ca341414a257
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-17 23:07:36 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -396,3 +396,41 @@ function linearSearch(array, toFind){
 // all the images, scripts, links and sub-frames have finished loading.
 // The DOM event DOMContentLoaded will fire after the DOM for the page has been constructed, but do not wait for other resources to finish
 //  loading. This is preferred in certain cases when you do not need the full page to be loaded before initializing.
+
+
+
+
+// Q24:   What is Callback Hell and what is the main cause of it?
+
+// Ans-
+// Asynchronous JavaScript, or JavaScript that uses callbacks, is hard to get right intuitively. A lot of code ends up looking like this:
+
+fs.readdir(source, function (err, files) {
+  if (err) {
+    console.log('Error finding files: ' + err)
+  } else {
+    files.forEach(function (filename, fileIndex) {
+      console.log(filename)
+      gm(source + filename).size(function (err, values) {
+        if (err) {
+          console.log('Error identifying file size: ' + err)
+        } else {
+          console.log(filename + ' : ' + values)
+          aspect = (values.width / values.height)
+          widths.forEach(function (width, widthIndex) {
+            height = Math.round(width / aspect)
+            console.log('resizing ' + filename + 'to ' + height + 'x' + height)
+            this.resize(width, height).write(dest + 'w' + width + '_' + filename, function(err) {
+              if (err) console.log('Error writing file: ' + err)
+            })
+          }.bind(this))
+        }
+      })
+    })
+  }
+})
+
+// See the pyramid shape and all the }) at the end? This is affectionately known as callback hell.
+// The cause of callback hell is when people try to write JavaScript in a way where execution happens visually from top to bottom. Lots of
+//  people make this mistake! In other languages like C, Ruby or Python there is the expectation that whatever happens on line 1 will 
+//  finish before the code on line 2 starts running and so on down the file.
