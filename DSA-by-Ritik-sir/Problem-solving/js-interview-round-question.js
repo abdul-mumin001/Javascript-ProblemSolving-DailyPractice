a700ab17371e3c6a92165a88722922720f36bffd
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-21 22:51:17 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -556,3 +556,38 @@ function groupBy(ar){
 }
 
 console.log(groupBy(people));
+
+
+
+
+
+
+// 30. Program to check a string with balanced brackets.
+
+const isValid=(str)=>{
+  let s=str.replace(/\s+/g, '');
+  if(s.length % 2 !==0) return false;
+
+
+  const stack=[];
+  const map=new Map([
+    ['(',')'],
+    ['[',']'],
+    ['{','}']
+  ]);
+
+
+  for(let i=0; i<s.length; i+=1){
+    if(map.has(s[i])){
+      stack.push(map.get(s[i]))
+    }else if(s[i] !== stack.pop()){
+      return false
+    }
+  }
+  return stack.length === 0;
+}
+
+let str1="({} [] ({}))";
+let str2="}}";
+
+console.log(isValid(str1));
