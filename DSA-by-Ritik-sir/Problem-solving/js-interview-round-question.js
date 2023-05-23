1ed775195cec31d4b653489a38eef32ba51527bd
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-23 23:41:00 +0530

    js question practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -624,3 +624,31 @@ return sum;
 }
 
 console.log(twoSum(array,9));
+
+
+
+
+
+
+
+
+// 32. Find the missing number from unsorted array with O(n) complexity
+
+// Algorithm
+// Create a variable sum = 1 which will store the missing number and a counter variable c = 2.
+// Traverse the array from start to end.
+// Update the value of sum as sum = sum — array[i] + c and update c as c++.
+// Print the missing number as a sum.
+
+let arr1=[2,7,8,5,1,4,3,6];
+function missing(a){
+  let l=a.length;
+  let sum=1;
+  for(let i=2;i<=(1+1);i++){
+    sum=sum+i;
+    sum=sum-a[i-2];
+
+  }
+  return sum;
+}
+console.log(missing(arr1));
