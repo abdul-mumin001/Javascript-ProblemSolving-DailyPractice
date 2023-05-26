d33a6650c0c3852934c5034f2baada38548a23e1
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-26 22:55:10 +0530

    js coding practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -697,6 +697,9 @@ console.log(Missing(ar2));
 let arr3 = [12,34,21,14,67,35,64,25];
 arr3.sort((a,b)=>a-b);
 
-const nthLargest= arr3[arr3.length-n];
+// const nthLargest= arr3[arr3.length-n];
+
+const nthLargest= arr3[arr3.length-2]; //if n=2 
+
 
 console.log(nthLargest);
