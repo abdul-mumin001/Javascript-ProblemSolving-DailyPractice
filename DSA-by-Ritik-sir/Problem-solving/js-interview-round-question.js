5fad9f1656811f9559b67330b437b116d26abf7a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-05-29 23:12:52 +0530

    js coding practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -728,3 +728,22 @@ function unique(arr4){
   
 }
 console.log(unique(arr4));
+
+
+
+
+
+
+
+// 36. Print all duplicate elements of an array
+
+let arr5=[1,1,5,6,7,7,8,9,3,4,4,];
+function removeDuplicates(ar){
+  let result=ar.filter((item,index)=>{
+    return ar.indexOf(item) !== index;
+
+  });
+  return result;
+
+}
+console.log(removeDuplicates(arr5));
