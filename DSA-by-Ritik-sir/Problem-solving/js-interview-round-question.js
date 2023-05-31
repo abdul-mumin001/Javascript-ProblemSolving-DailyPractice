af14ef3617a8db063dafbddf8012ef7c4447c81c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-01 00:00:21 +0530

    js coding practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -787,3 +787,29 @@ let result=friends.reduce((pre,curr)=>{
 },[]);
 
 console.log(result);
+
+
+
+
+//38 program to find the factorial of a number
+
+
+function factorial(x) {
+  // if number is 0
+  if (x === 0) {
+    return 1;
+  }
+
+  // if number is positive
+  else {
+    return x * factorial(x - 1);
+  }
+}
+
+const num = 3;
+
+// calling factorial() if num is non-negative
+if (num > 0) {
+  let result = factorial(num);
+  console.log(`The factorial of ${num} is ${result}`);
+}
