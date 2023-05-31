738cff43b8ecfc872c0ddcc2d1802108aa6f4bef
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-01 00:01:27 +0530

    js coding practice

--- DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
+++ DSA-by-Ritik-sir/Problem-solving/js-interview-round-question.js
@@ -795,12 +795,14 @@ console.log(result);
 
 
 function factorial(x) {
+
   // if number is 0
   if (x === 0) {
     return 1;
   }
 
   // if number is positive
+
   else {
     return x * factorial(x - 1);
   }
@@ -809,6 +811,7 @@ function factorial(x) {
 const num = 3;
 
 // calling factorial() if num is non-negative
+
 if (num > 0) {
   let result = factorial(num);
   console.log(`The factorial of ${num} is ${result}`);
