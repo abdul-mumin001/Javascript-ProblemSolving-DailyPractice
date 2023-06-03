1ca1d48bc7d390f72274a8ec8aba38c4f2c290cd
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-03 22:41:48 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -1,7 +1,6 @@
 // 1. Print the multiplication table with 7
 
-for(let i = 1; i <= 10; i++)
-{
+for(let i = 1; i <= 10; i++){
     let row = "7 * " + i + " = " + 7 * i;
     console.log(row);
 }
@@ -11,14 +10,12 @@ for(let i = 1; i <= 10; i++)
 
 // 2. Print all the multiplication tables with numbers from 1 to 10
 
-for(let i = 1; i <= 10; i++)
-{
+for(let i = 1; i <= 10; i++){
     printTable(i);
     console.log("");
 }
 
-function printTable(n)
-{
+function printTable(n){
     for(let i = 1; i <= 10; i++)
     {
         let row = n + " * " + i + " = " + n * i;
