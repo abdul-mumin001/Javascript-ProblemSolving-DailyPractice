d8000e32635fd247b549455531038b38d4d428a5
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-04 22:54:01 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -22,3 +22,19 @@ function printTable(n){
         console.log(row);
     }
 }
+
+
+
+
+
+
+// 3. Calculate the sum of numbers from 1 to 10
+
+let sum = 0;
+
+for(let i = 1; i <= 10; i++)
+{
+    sum += i;
+}
+
+console.log(sum);
