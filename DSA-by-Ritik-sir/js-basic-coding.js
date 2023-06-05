2d27db3bc60d2b6835b96bc690af838cd984727d
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-05 22:44:32 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -51,3 +51,19 @@ for(let i = 1; i <= 10; i++){
 }
 
 console.log(prod);
+
+
+
+
+
+
+// 5. Calculate the sum of odd numbers greater than 10 and less than 30
+
+let s = 0;
+
+for(let i = 11; i <= 30; i += 2)
+{
+    s += i;
+}
+
+console.log(s);
