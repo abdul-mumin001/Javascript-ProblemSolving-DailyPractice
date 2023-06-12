b3b86387784525b8ef89f6f7d269c01531be6df0
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-12 23:45:55 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -239,14 +239,14 @@ console.log("Max: ", max);
 
 // 12. Print the first 10 Fibonacci numbers without recursion
 
+
 let f0 = 0;
 console.log(f0);
 
 let f1 = 1;
 console.log(f1);
 
-for(let i = 2; i < 10; i++)
-{
+for(let i = 2; i < 10; i++){
     let fi = f1 + f0;
     console.log(fi);
     
