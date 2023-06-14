8dc415a05f9868aca8090affc3f613500bfd77c3
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-14 21:00:50 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -273,3 +273,35 @@ function findFibonacci(n){
 let n = findFibonacci(10);
 
 console.log(n);
+
+
+
+
+// 14. Create a function that will return a Boolean specifying if a number is prime
+
+function isPrime(n)
+{
+    if (n < 2)
+        return false;
+        
+    if (n == 2)
+        return true;
+    
+    let maxDiv = Math.sqrt(n);
+    
+    for(let i = 2; i <= maxDiv; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(2, " is prime? ", isPrime(2));
+console.log(3, " is prime? ", isPrime(3));
+console.log(4, " is prime? ", isPrime(4));
+console.log(5, " is prime? ", isPrime(5));
+console.log(9, " is prime? ", isPrime(9));
