3d4a707d334beff91218713cad89d12a6ce1bfa2
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-17 21:06:30 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -366,3 +366,52 @@ function isPrime(n){
     
     return true;
 }
+
+
+
+
+
+// 17. Create a function that will return in an array the first "nPrimes" prime numbers greater than a particular number "startAt"
+
+console.log(getPrimes(10, 100));
+
+function getPrimes(nPrimes, startAt)
+{
+    let ar = [];
+    
+    let i = startAt;
+    
+    while(ar.length < nPrimes)
+    {
+        if (isPrime(i))
+        {
+            ar.push(i);
+        }
+        
+        i++;
+    }
+    
+    return ar;
+}
+
+// Returns true if a number is prime
+function isPrime(n)
+{
+    if (n < 2)
+        return false;
+        
+    if (n == 2)
+        return true;
+    
+    let maxDiv = Math.sqrt(n);
+    
+    for(let i = 2; i <= maxDiv; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
