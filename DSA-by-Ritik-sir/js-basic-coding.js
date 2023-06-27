6d56f868a18a0c33981d1c5c3fa533bce21c7011
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-27 21:48:54 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -512,3 +512,42 @@ function mergeArrays(arr1, arr2){
    
    return arr;
 }
+
+
+
+
+
+
+
+// 23. Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are 
+// either in the first array or second array but not in both
+
+
+let aar1 = [1, 2, 3, 10, 5, 3, 14];
+let aar2 = [1, 4, 5, 6, 14];
+
+let aar = mergeExclusive(aar1, aar2);
+console.log(aar);
+
+function mergeExclusive(aar1, aar2)
+{
+    let aar = [];
+    
+    for(let el of aar1)
+    {
+        if (!aar2.includes(el))
+        {
+            aar.push(el);
+        }
+    }
+    
+    for(let el of aar2)
+    {
+        if (!aar1.includes(el))
+        {
+            aar.push(el);
+        }
+    }
+    
+    return aar;
+}
