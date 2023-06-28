3ae3298e3b407301035ab829259242f4c232e89e
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-06-28 23:13:48 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -564,14 +564,11 @@ let aarr2 = [-1, 4, 5, 6, 14];
 let aarr = mergeLeft(aarr1, aarr2);
 console.log(aarr);
 
-function mergeLeft(aarr1, aarr2)
-{
+function mergeLeft(aarr1, aarr2){
     let aarr = [];
     
-    for(let el of aarr1)
-    {
-        if (!aarr2.includes(el))
-        {
+    for(let el of aarr1){
+        if (!aarr2.includes(el)){
             aarr.push(el);
         }
     }
