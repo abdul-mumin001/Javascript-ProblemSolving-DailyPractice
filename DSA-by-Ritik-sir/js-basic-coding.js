2f7f59ff5dd8e906caf71174f862bc4726db22f0
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-01 23:45:54 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -640,3 +640,56 @@ function isInArray(n, ar, fromIndex, toIndex){
     }
     return false;
 }
+
+
+
+
+
+
+
+// 26. Calculate the sum of first 100 prime numbers
+
+let n = 10;
+console.log("Sum of first ", n, " primes is ", sumPrimes(10));
+
+function sumPrimes(n)
+{
+    let foundPrimes = 0;
+    let i = 2;
+    let sum = 0;
+    
+    while(foundPrimes < n)
+    {
+        if (isPrime(i))
+        {
+            foundPrimes++;
+            sum += i;
+        }
+        
+        i++;
+    }
+    
+    return sum;
+}
+
+// Returns true if number n is prime
+function isPrime(n)
+{
+    if (n < 2)
+        return false;
+        
+    if (n == 2)
+        return true;
+    
+    let maxDiv = Math.sqrt(n);
+    
+    for(let i = 2; i <= maxDiv; i++)
+    {
+        if (n % i === 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
