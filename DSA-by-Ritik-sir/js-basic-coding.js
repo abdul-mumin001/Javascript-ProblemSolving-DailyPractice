844481fa6cfdd97ad15fc021f361e64d3dd4fab5
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-02 22:07:30 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -684,3 +684,52 @@ function isPrime(n){
     }
     return true;
 }
+
+
+
+
+// 27. Print the distance between the first 100 prime numbers
+
+
+printDistances(100);
+// Print distances between the first n prime numbers
+function printDistances(n)
+{
+    let lastPrime = 2;
+    let i = lastPrime + 1;
+    let foundPrimes = 1;
+    
+    while(foundPrimes < n)
+    {
+        if (isPrime(i))
+        {
+            console.log(i - lastPrime, "\t", i, " - ", lastPrime);
+
+            foundPrimes++;
+            lastPrime = i;
+        }
+        
+        i++;
+    }
+}
+// Returns true if number n is prime
+function isPrime(n)
+{
+    if (n < 2)
+        return false;
+        
+    if (n == 2)
+        return true;
+    
+    let maxDiv = Math.sqrt(n);
+    
+    for(let i = 2; i <= maxDiv; i++)
+    {
+        if (n % i === 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
