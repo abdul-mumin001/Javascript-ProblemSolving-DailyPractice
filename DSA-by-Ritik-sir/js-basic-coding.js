7537bf73f534cdbeebbddda2c2744753600ec834
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-04 23:51:30 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -732,21 +732,71 @@ function isPrime(n){
 // 28. Create a function that will add two positive numbers of indefinite size. The numbers are received as strings and the result should 
 // be also provided as string.
 
+// Method 1
+
+// let n1 = "2909034221912398942349";
+// let n2 = "1290923909029309499";
+// let sum = add(n1, n2);
+
+// console.log(n1, "\n", n2, "\n", sum);
+
+// function add(sNumber1, sNumber2){
+//     let s = "";
+//     let carry = 0;
+    
+//     let maxSize = Math.max(sNumber1.length, sNumber2.length);
+    
+//     for(let i = 0; i < maxSize; i++){
+//         let digit1 = digitFromRight(sNumber1, i);
+//         let digit2 = digitFromRight(sNumber2, i);
+        
+//         let sum = digit1 + digit2 + carry;
+//         let digitSum = sum % 10;
+//         carry = sum >= 10 ? 1 : 0;
+        
+//         s = digitSum.toString() + s;
+//     }
+    
+//     if (carry > 0)
+//         s = carry + s;
+    
+//     return s;
+// }
+
+// function digitFromRight(s, digitNo){
+//     if (digitNo >= s.length)
+//         return 0;
+        
+//     let char = s[ s.length - 1 - digitNo ];
+//     return parseInt(char);
+// }
+
+
+
+// Method 2
+
 let n1 = "2909034221912398942349";
 let n2 = "1290923909029309499";
 let sum = add(n1, n2);
 
-console.log(n1, "\n", n2, "\n", sum);
+console.log(n1);
+console.log(n2);
+console.log(sum);
 
-function add(sNumber1, sNumber2){
+function add(sNumber1, sNumber2)
+{
+    let maxSize = Math.max(sNumber1.length, sNumber2.length);
+    
+    let s1 = sNumber1.padStart(maxSize, "0");
+    let s2 = sNumber2.padStart(maxSize, "0");
+
     let s = "";
     let carry = 0;
     
-    let maxSize = Math.max(sNumber1.length, sNumber2.length);
-    
-    for(let i = 0; i < maxSize; i++){
-        let digit1 = digitFromRight(sNumber1, i);
-        let digit2 = digitFromRight(sNumber2, i);
+    for(let i = maxSize - 1; i >= 0; i--)
+    {
+        let digit1 = parseInt(s1[i]);
+        let digit2 = parseInt(s2[i]);
         
         let sum = digit1 + digit2 + carry;
         let digitSum = sum % 10;
@@ -759,12 +809,4 @@ function add(sNumber1, sNumber2){
         s = carry + s;
     
     return s;
-}
-
-function digitFromRight(s, digitNo){
-    if (digitNo >= s.length)
-        return 0;
-        
-    let char = s[ s.length - 1 - digitNo ];
-    return parseInt(char);
 }
