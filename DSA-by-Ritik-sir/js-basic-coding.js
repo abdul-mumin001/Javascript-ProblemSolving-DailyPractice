706784d1bec84f3dc8971cb2328483f63ae7b80c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-08 19:29:21 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -740,3 +740,37 @@ console.log(countWords("JavaScript!!!   "));
 console.log(countWords("     JavaScript"));
 console.log(countWords("    JavaScript is cool      "));
 console.log(countWords("I like to learn JavaScript with codeguppy"));
+
+
+
+
+
+// 30. Create a function that will capitalize the first letter of each word in a text
+
+console.log(captializeWords("Create a function that will capitalize the first letter of each word in a text"));
+
+function captializeWords(text)
+{
+    let text2 = "";
+    
+    for(let i = 0; i < text.length; i++)
+    {
+        let currChr = text[i];
+        let prevChr = i > 0 ? text[i - 1] : " ";
+        
+        if (!isSeparator(currChr) && isSeparator(prevChr))
+        {
+            currChr = currChr.toUpperCase();
+        }
+        
+        text2 += currChr;
+    }
+    
+    return text2;
+}
+
+function isSeparator(c)
+{
+    let separators = [" ", "\t", "\n", "\r", ",", ";", ".", "!", "?"];
+    return separators.includes(c);
+}
