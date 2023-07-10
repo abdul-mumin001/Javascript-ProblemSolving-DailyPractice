cb0a514f1b49eeaf9ffefcdfe3875b07e72ca222
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-10 22:52:01 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -768,3 +768,26 @@ function isSeparator(c) {
   let separators = [" ", "\t", "\n", "\r", ",", ";", ".", "!", "?"];
   return separators.includes(c);
 }
+
+
+
+
+
+
+// 31. Calculate the sum of numbers received in a comma delimited string
+
+console.log(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
+
+function sumCSV(s)
+{
+    let ar = s.split(",");
+    
+    let sum = 0;
+    
+    for(let n of ar)
+    {
+        sum += parseFloat(n);
+    }
+    
+    return sum;
+}
