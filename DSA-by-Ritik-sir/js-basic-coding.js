8e046e87e0b7da6978b62b18a91fc4b9170b2ab8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-10 22:56:07 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -776,13 +776,14 @@ function isSeparator(c) {
 
 // 31. Calculate the sum of numbers received in a comma delimited string
 
+
 console.log(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
 
 function sumCSV(s){
+
     let ar = s.split(",");
-    
     let sum = 0;
-    
+
     for(let n of ar){
         sum += parseFloat(n);
     }
