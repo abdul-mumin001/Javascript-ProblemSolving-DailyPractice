7dec311360b6d059671107156dcff2e09e3ed4d8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-10 22:52:43 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -778,14 +778,12 @@ function isSeparator(c) {
 
 console.log(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
 
-function sumCSV(s)
-{
+function sumCSV(s){
     let ar = s.split(",");
     
     let sum = 0;
     
-    for(let n of ar)
-    {
+    for(let n of ar){
         sum += parseFloat(n);
     }
     
