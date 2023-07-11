98b78f8de526aa3c31e095c6057183066f85446f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-11 23:01:33 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -790,3 +790,45 @@ function sumCSV(s){
     
     return sum;
 }
+
+
+
+
+
+// 32. Create a function that will return an array with words inside a text
+
+let text = "Create a function, that will return an array (of string), with the words inside the text";
+
+console.log(getWords(text));
+
+function getWords(text)
+{
+    let startWord = -1;
+    let ar = [];
+    
+    for(let i = 0; i <= text.length; i++)
+    {
+        let c = i < text.length ? text[i] : " ";
+
+        if (!isSeparator(c) && startWord < 0)
+        {
+            startWord = i;
+        }
+        
+        if (isSeparator(c) && startWord >= 0)
+        {
+            let word = text.substring(startWord, i);
+            ar.push(word);
+            
+            startWord = -1;
+        }
+    }
+
+    return ar;
+}
+
+function isSeparator(c)
+{
+    let separators = [" ", "\t", "\n", "\r", ",", ";", ".", "!", "?", "(", ")"];
+    return separators.includes(c);
+}
