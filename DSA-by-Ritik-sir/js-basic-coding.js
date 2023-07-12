85a58ff3293e2021eaec8f22c7805c2806ebee83
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-12 23:45:02 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -826,12 +826,10 @@ let data = "John;Smith;954-000-0000\nMich;Tiger;305-000-0000\nMonique;Vasquez;10
 let arr = csvToArray(data);
 console.log(JSON.stringify(arr));
 
-function csvToArray(data)
-{
+function csvToArray(data){
     let arLines = data.split("\n");
 
-    for(let i = 0; i < arLines.length; i++)
-    {
+    for(let i = 0; i < arLines.length; i++){
         let arLine = arLines[i].split(";");
         arLines[i] = arLine;
     }
