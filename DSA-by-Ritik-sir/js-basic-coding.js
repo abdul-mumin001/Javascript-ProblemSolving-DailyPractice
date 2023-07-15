49f6f05ddfdcd299fb30182b3566701a7aa1d80e
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-15 22:18:38 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -865,3 +865,16 @@ function getCharCodes(s){
     return ar;
 }
 console.log(getCharCodes("I like JavaScript"));
+
+
+
+
+
+// 36. Create a function that will convert an array containing ASCII codes in a string
+
+console.log(codesToString([73,32,108,105,107,101,32,74,97,118,97,83,99,114,105,112,116]));
+
+function codesToString(ar)
+{
+    return String.fromCharCode(...ar);
+}
