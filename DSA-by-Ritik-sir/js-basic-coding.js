71b0424c81ad2b2835a8eac717ab28b1644dd79a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-15 22:19:48 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -874,7 +874,8 @@ console.log(getCharCodes("I like JavaScript"));
 
 console.log(codesToString([73,32,108,105,107,101,32,74,97,118,97,83,99,114,105,112,116]));
 
-function codesToString(ar)
-{
+function codesToString(ar){
+
     return String.fromCharCode(...ar);
+    
 }
