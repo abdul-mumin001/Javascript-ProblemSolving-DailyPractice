cc4128f40a74a2633ecf71b4659e8510723b8ae5
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-18 11:36:10 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -948,10 +948,10 @@ function bubbleSort(arrr) {
 
 console.log(getDistance(100, 100, 400, 300));
 
-function getDistance(x1, y1, x2, y2)
-{
+function getDistance(x1, y1, x2, y2){
     let l1 = x2 - x1;
     let l2 = y2 - y1;
     
     return Math.sqrt(l1 * l1 + l2 * l2);
+    
 }
