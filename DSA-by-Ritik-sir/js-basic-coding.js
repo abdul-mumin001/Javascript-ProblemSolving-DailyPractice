ab68b70702500b963919b5180fd74d4b92ef929a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-18 11:35:17 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -939,3 +939,19 @@ function bubbleSort(arrr) {
     }
   }
 }
+
+
+
+
+
+// 39. Create a function to calculate the distance between two points defined by their x, y coordinates
+
+console.log(getDistance(100, 100, 400, 300));
+
+function getDistance(x1, y1, x2, y2)
+{
+    let l1 = x2 - x1;
+    let l2 = y2 - y1;
+    
+    return Math.sqrt(l1 * l1 + l2 * l2);
+}
