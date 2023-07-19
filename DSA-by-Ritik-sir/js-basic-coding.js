bf0f40c7fdd56527c2ec0ea2fd4507835eddd90b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-19 22:53:09 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -961,14 +961,12 @@ function getDistance(x1, y1, x2, y2) {
 
 console.log(collisionCircleCircle(200, 200, 100, 300, 300, 50));
 
-function collisionCircleCircle(circle1X, circle1Y, circle1R, circle2X, circle2Y, circle2R)
-{
+function collisionCircleCircle(circle1X, circle1Y, circle1R, circle2X, circle2Y, circle2R){
     return getDistance(circle1X, circle1Y, circle2X, circle2Y) <= circle1R + circle2R;
 }
 
 // Calculate the distance between the two specified points
-function getDistance(x1, y1, x2, y2)
-{
+function getDistance(x1, y1, x2, y2){
     let l1 = x2 - x1;
     let l2 = y2 - y1;
     
