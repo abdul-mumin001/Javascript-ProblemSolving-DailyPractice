abd8600ac91a58cfc2356e9bc4ed33981c882dc0
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-24 21:54:45 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -1025,28 +1025,78 @@ function sumArray(arrrr) {
 
 // 43. Find the maximum number in a jagged array of numbers or array of numbers
 
+// Method 1
+
+// let arrrrr = [2, 4, 10, [12, 4, [100, 99], 4], [3, 2, 99], 0];
+
+// let max = findMax(arrrrr);
+// console.log("Max  = ", max);
+
+// // Use recursion to find the maximum numeric value in an array of arrays
+// function findMax(arrrrr) {
+//   let max = -Infinity;
+
+//   // Cycle through all the elements of the array
+//   for (let i = 0; i < arrrrr.length; i++) {
+//     let el = arrrrr[i];
+
+//     // If an element is of type array then invoke the same function
+//     // to find out the maximum element of that subarray
+//     if (Array.isArray(el)) {
+//       el = findMax(el);
+//     }
+
+//     if (el > max) {
+//       max = el;
+//     }
+//   }
+//   return max;
+// }
+
+
+// Method 2
+
 let arrrrr = [2, 4, 10, [12, 4, [100, 99], 4], [3, 2, 99], 0];
 
 let max = findMax(arrrrr);
 console.log("Max  = ", max);
 
-// Use recursion to find the maximum numeric value in an array of arrays
-function findMax(arrrrr) {
-  let max = -Infinity;
+// Use a stack to find the maximum numeric value in an array of arrays
+function findMax(arElements)
+{
+    let max = -Infinity;
 
-  // Cycle through all the elements of the array
-  for (let i = 0; i < arrrrr.length; i++) {
-    let el = arrrrr[i];
+    // This is the stack on which will put the first array and then 
+    // all the other sub-arrays that we find as we traverse an array     
+    let arrays = [];
 
-    // If an element is of type array then invoke the same function
-    // to find out the maximum element of that subarray
-    if (Array.isArray(el)) {
-      el = findMax(el);
+    arrays.push(arElements);
+
+    // Loop as long as are arrays added to the stack for processing
+    while(arrays.length > 0)
+    {
+        // Extract an array from the stack
+        arrrrr = arrays.pop();
+        
+        // ... and loop through its elements
+        for(let i = 0; i < arrrrr.length; i++)
+        {
+            let el = arrrrr[i];
+
+            // If an element is of type array, we'll add it to stack
+            // to be processed later
+            if ( Array.isArray(el) )
+            {
+                arrays.push(el);
+                continue;
+            }
+            
+            if ( el > max )
+            {
+                max = el;
+            }
+        }
     }
 
-    if (el > max) {
-      max = el;
-    }
-  }
-  return max;
+    return max;
 }
