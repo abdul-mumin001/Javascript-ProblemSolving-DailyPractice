d2605b0f790da7d685d8e9ea4a26d3a0e25ca62e
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-26 21:34:12 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -1093,3 +1093,28 @@ function findMax(arElements) {
   }
   return max;
 }
+
+
+// 44. Deep copy a jagged array with numbers or other arrays in a new array
+
+let ar1 = [2, 4, 10, [12, 4, [100, 99], 4], [3, 2, 99], 0];
+let ar2 = copyArray(ar1);
+
+console.log(ar2);
+
+function copyArray(ar)
+{
+    let ar2 = [];
+    
+    for(let el of ar)
+    {
+        if (Array.isArray(el))
+        {
+            el = copyArray(el);
+        }
+        
+        ar2.push(el);
+    }
+    
+    return ar2;
+}
