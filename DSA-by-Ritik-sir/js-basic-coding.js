2b4e4844022cb032ef405c281fe89add66511f90
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-29 21:41:42 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -1200,3 +1200,43 @@ function shuffleArray(aarr) {
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+// 47. Create a function that will receive n as argument and return an array of n unique random numbers from 1 to n.
+
+console.log(getRandomNumbers(10));
+
+function getRandomNumbers(n)
+{
+    let ar = [];
+    
+    for(let i = 1; i <= n; i++)
+    {
+        ar.push(i);
+    }
+    
+    shuffleArray(ar);
+    
+    return ar;
+}
+
+// Shuffle array implemented using Fisher–Yates shuffle algorithm
+function shuffleArray(ar)
+{
+    for(let i = ar.length - 1; i > 0; i--)
+    {
+        let j = randomInt(0, i - 1);
+        
+        let t = ar[i];
+        ar[i] = ar[j];
+        ar[j] = t;
+    }
+    
+    return ar;
+}
+
+// Get a random int between min and max (both included)
+function randomInt(min, max) 
+{
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
