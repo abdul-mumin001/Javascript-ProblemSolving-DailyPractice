ac51323bfdc9bd6926d1254425d43e1efeffee28
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-07-29 21:42:51 +0530

    js basic coding practice

--- DSA-by-Ritik-sir/js-basic-coding.js
+++ DSA-by-Ritik-sir/js-basic-coding.js
@@ -1201,37 +1201,36 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 // 47. Create a function that will receive n as argument and return an array of n unique random numbers from 1 to n.
 
 console.log(getRandomNumbers(10));
 
-function getRandomNumbers(n){
-    let ar = [];
-    
-    for(let i = 1; i <= n; i++){
-        ar.push(i);
-    }
-    
-    shuffleArray(ar);
-    
-    return ar;
+function getRandomNumbers(n) {
+  let ar = [];
+
+  for (let i = 1; i <= n; i++) {
+    ar.push(i);
+  }
+
+  shuffleArray(ar);
+
+  return ar;
 }
 
 // Shuffle array implemented using Fisher–Yates shuffle algorithm
-function shuffleArray(ar){
-    for(let i = ar.length - 1; i > 0; i--){
-        let j = randomInt(0, i - 1);
-        
-        let t = ar[i];
-        ar[i] = ar[j];
-        ar[j] = t;
-    }
-    
-    return ar;
+function shuffleArray(ar) {
+  for (let i = ar.length - 1; i > 0; i--) {
+    let j = randomInt(0, i - 1);
+
+    let t = ar[i];
+    ar[i] = ar[j];
+    ar[j] = t;
+  }
+
+  return ar;
 }
 
 // Get a random int between min and max (both included)
-function randomInt(min, max){
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
