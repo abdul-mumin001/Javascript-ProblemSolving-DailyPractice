c9895525c394b61c7f18958f1b7c780f875f086d
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-04 22:26:00 +0530

    reverse of a number

--- DSA-by-Ritik-sir/Problem-solving/reverse-of-a-number.js
+++ DSA-by-Ritik-sir/Problem-solving/reverse-of-a-number.js
@@ -1,11 +1,15 @@
 function reverse(a) {
-  // let x=a%10;
-  // let b=Math.floor(a/10);
-  // let y=b%10;
-  // let z=Math.floor(b/10);
-  // return(x+","+y+","+z)
+  let x=a%10;
+  console.log(x);
+  let b=Math.floor(a/10);
+  console.log(b);
+  let y=b%10;
+  console.log(y);
+  let z=Math.floor(b/10);
+  console.log(z);
+  return(x+","+y+","+z)
 
-  return parseFloat(a.toString().split("").reverse().join("")) * Math.sign(a);
+//   return parseFloat(a.toString().split("").reverse().join("")) * Math.sign(a);
 }
 console.log(reverse(156));
 console.log(reverse(-156));
