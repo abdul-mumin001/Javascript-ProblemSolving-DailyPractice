6d87e7ef64104beb193428c1e4c9014f06d3582d
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-05 23:13:18 +0530

    greater than given condition or not

---
+++ DSA-by-Ritik-sir/Problem-solving/greater-than-10.js
@@ -0,0 +1,11 @@
+// Q. Given an string, return “YES”, if the string length is greater than 10 else return “NO”
+
+function greater(string){
+    if(string.length>=10){
+      return('YES')
+    }else{
+      return("NO")
+    }
+  }
+  console.log(greater('aircampus'))
+  console.log(greater('javascript'))
