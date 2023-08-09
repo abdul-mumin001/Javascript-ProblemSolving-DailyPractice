898e37b4f784dc52e61a310658a1d119665014d2
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-09 19:56:40 +0530

    find count of e

--- DSA-by-Ritik-sir/Problem-solving/e-present.js
+++ DSA-by-Ritik-sir/Problem-solving/e-present.js
@@ -1,12 +1,12 @@
 // Q. Given a string, find out how many times ‘e’ is present in the string. Return the count of ‘e.’
 
-function counte(string){
-    let count=0;
-    for(let i=0;i<string.length;i++){
-      if(string.charAt(i)=='e'){
-        count=count+1;
-      }
+function counte(string) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == "e") {
+      count = count + 1;
     }
-    return count;
   }
-  console.log(counte('I love india'))
+  return count;
+}
+console.log(counte("I love india"));
