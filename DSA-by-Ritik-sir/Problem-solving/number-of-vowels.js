e13aeea4cea477d13ad5b4c005df5fb471048b95
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-10 23:22:25 +0530

    number of vowels

--- DSA-by-Ritik-sir/Problem-solving/number-of-vowels.js
+++ DSA-by-Ritik-sir/Problem-solving/number-of-vowels.js
@@ -1,8 +1,8 @@
 // Q. Given a string, count the number of vowels present in the string and return it.
 
-function vowels(string){
-    const countOfVowels=string.match(/[aeiou]/gi).length;
-    return countOfVowels;
-  }
-  console.log(vowels('aircampus'))
-  console.log(vowels('javascript'))
+function vowels(string) {
+  const countOfVowels = string.match(/[aeiou]/gi).length;
+  return countOfVowels;
+}
+console.log(vowels("aircampus"));
+console.log(vowels("javascript"));
