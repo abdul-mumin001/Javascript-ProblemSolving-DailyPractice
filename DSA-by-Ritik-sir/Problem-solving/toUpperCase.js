0e1a42e83b56ac3dfb568ccccbc494b081fa122b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-11 23:26:13 +0530

    toUpperCase

---
+++ DSA-by-Ritik-sir/Problem-solving/toUpperCase.js
@@ -0,0 +1,9 @@
+// Q. Given a string, change all the characters to uppercase and return the new string.
+
+
+function toUpper(string){
+    let upper=string.toUpperCase();
+      return upper;
+    }
+    console.log(toUpper('aircampus'))
+    console.log(toUpper('Javascript'))
