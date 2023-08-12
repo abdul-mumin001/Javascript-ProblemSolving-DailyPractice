b48d839f8e843bc453ebbd1a26f4eeebf2a727ab
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-12 23:15:37 +0530

    upperToLower

--- DSA-by-Ritik-sir/Problem-solving/upperToLower.js
+++ DSA-by-Ritik-sir/Problem-solving/upperToLower.js
@@ -1,4 +1,4 @@
-// Q. Given a string, return the string after changing the uppercase characters to lowercase and lowercase 
+// Q. Given a string, return the string after changing the uppercase characters to lowercase and lowercase
 // characters to uppercase.
 
 // Input:
@@ -6,23 +6,21 @@
 // Output:
 // aIRcAMPUS
 
-
 function upperLower(strings) {
-    let upperToLower=[];
-     let upperCase=strings.toUpperCase();
-     let a=upperCase.split('')
-   
-     let arr=strings.split('')
+  let upperToLower = [];
+  let upperCase = strings.toUpperCase();
+  let a = upperCase.split("");
 
-     for(let i=0;i<arr.length;i++){
-       if(arr[i]==a[i]){
-         upperToLower.push(arr[i].toLowerCase())
-       }
-       else{
-         upperToLower.push(arr[i].toUpperCase())
-       }
-     }
-     return upperToLower.join('')
-   }
-   console.log(upperLower('AirCampus'))
-   console.log(upperLower('JavaScript'))
+  let arr = strings.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == a[i]) {
+      upperToLower.push(arr[i].toLowerCase());
+    } else {
+      upperToLower.push(arr[i].toUpperCase());
+    }
+  }
+  return upperToLower.join("");
+}
+console.log(upperLower("AirCampus"));
+console.log(upperLower("JavaScript"));
