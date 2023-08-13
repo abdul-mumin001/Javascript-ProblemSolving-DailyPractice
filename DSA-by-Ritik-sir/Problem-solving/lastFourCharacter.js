3c3530a0b59fe80ed5e2f5e010be682395d7a07b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-13 19:49:50 +0530

    lastFourCharacter

---
+++ DSA-by-Ritik-sir/Problem-solving/lastFourCharacter.js
@@ -0,0 +1,18 @@
+// Q. Given a string, return the last 4 characters of the given string.
+// Input:
+// Javascript
+// Output:
+// ript
+
+
+function lastFour(string){
+    let lastFourDigits=''
+      if(string.length>4){
+        lastFourDigits=string.substring(string.length - 4);
+  
+      }else{
+        lastFourDigits=string
+      }
+      return lastFourDigits;
+    }
+    console.log(lastFour('Javascript'))
