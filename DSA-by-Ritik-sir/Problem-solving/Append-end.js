4c1c422b688157382623fc168a393db7612a195d
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-14 22:45:26 +0530

    add two words together

---
+++ DSA-by-Ritik-sir/Problem-solving/Append-end.js
@@ -0,0 +1,16 @@
+// Q. Given a sentence and a word, add the given word to the end of the string.
+// Input:
+// Java,
+// script
+// Output:
+// Javascript
+
+
+
+function appendEnd(str1, str2){
+  
+    let x=str1.concat(str2)
+    return x;
+  }
+  console.log(appendEnd('Java',
+  'script'))
