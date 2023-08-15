2a773828d5658017829160e6feb5063e123ea120
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-15 23:03:27 +0530

    string from start index to end index

---
+++ DSA-by-Ritik-sir/Problem-solving/Substring.js
@@ -0,0 +1,11 @@
+// Q. Given a string and two index values, return the string from the start index to end index.
+// Input:
+// javascript 4 10
+// output:
+// script
+
+
+function substring(str, a, b){
+    return str.substring(a,b)
+  }
+  console.log(substring('javascript', 4, 10))
