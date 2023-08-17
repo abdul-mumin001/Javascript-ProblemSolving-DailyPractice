c5765dbd56a1a6a8498bdb6e61993b04e64f71b8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-17 22:35:18 +0530

    replace the first occurences of Ram with Shyam

--- DSA-by-Ritik-sir/Problem-solving/Replace-Ram-with-shyam.js
+++ DSA-by-Ritik-sir/Problem-solving/Replace-Ram-with-shyam.js
@@ -1,10 +1,10 @@
-// Q. Given a string, replace the first occurrences of Ram with Shyam. 
+// Q. Given a string, replace the first occurrences of Ram with Shyam.
 // Input:
 // Shyam likes to code. Ram is a javascript developer.
 // Output:
 // Shyam likes to code. Shyam is a javascript developer.
 
-function ramShyam(str){
-    return str.replace(/Ram/i,'Shyam')
-   }
-   console.log(ramShyam('Shyam likes to code. Ram is a javascript developer'))
+function ramShyam(str) {
+  return str.replace(/Ram/i, "Shyam");
+}
+console.log(ramShyam("Shyam likes to code. Ram is a javascript developer"));
