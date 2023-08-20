e4d421fd06670e3cb715cdd04dbf273fbbc7fe46
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-20 23:01:08 +0530

    characters comma separated

--- DSA-by-Ritik-sir/Problem-solving/Character-comma-separated.js
+++ DSA-by-Ritik-sir/Problem-solving/Character-comma-separated.js
@@ -1,12 +1,12 @@
 //Q. Given a strings, return all the characters of string separated by comma (,)
 
 // Input:
-// aircampus  
+// aircampus
 // Output:
 // a,i,r,c,a,m,p,u,s
 
-function commaSep(str){
-    return str.split('').join()
-   }
-   console.log(commaSep('Javascript'))
-   console.log(commaSep('AbdulMumin'))
+function commaSep(str) {
+  return str.split("").join();
+}
+console.log(commaSep("Javascript"));
+console.log(commaSep("AbdulMumin"));
