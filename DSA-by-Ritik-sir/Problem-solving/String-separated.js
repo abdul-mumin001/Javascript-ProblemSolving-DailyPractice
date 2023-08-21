8a19dd8d8f87f6d050757db8259026a724b7f0e3
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-21 23:44:05 +0530

    String separated

--- DSA-by-Ritik-sir/Problem-solving/String-separated.js
+++ DSA-by-Ritik-sir/Problem-solving/String-separated.js
@@ -1,15 +1,13 @@
 // Q Given 3 strings, join string1 and string2 separated by string3. Return the final string.
 
 // Input:
-// javascript 
-// cool 
+// javascript
+// cool
 // is
 // Output:
 // javascript is cool
 
-function stringSep(str1, str2, str3){
-    return `${str1} ${str3} ${str2}` 
-   }
-   console.log(stringSep('javascript', 
-   'cool', 
-   'is'))
+function stringSep(str1, str2, str3) {
+  return `${str1} ${str3} ${str2}`;
+}
+console.log(stringSep("javascript", "cool", "is"));
