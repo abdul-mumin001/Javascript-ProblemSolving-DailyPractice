9b4ae2a1fee3e8e483f99e8adfaa0e855d7f6c17
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-26 21:54:23 +0530

    Min max in an array

--- DSA-by-Ritik-sir/Problem-solving/Min-max.js
+++ DSA-by-Ritik-sir/Problem-solving/Min-max.js
@@ -13,17 +13,14 @@
 // Output:
 // ` [3, 5] `
 
-
-function find_min_max(arr){
-    let min=arr[0];
-    let max=arr[0];
-    let i=arr.length
-    while(i--){
-      min=arr[i] < min ? arr[i] : min;
-      max=arr[i] > max ? arr[i] : max;
-  
-    }
-    return [max,min];
+function find_min_max(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let i = arr.length;
+  while (i--) {
+    min = arr[i] < min ? arr[i] : min;
+    max = arr[i] > max ? arr[i] : max;
   }
-  console.log(find_min_max([2,5,7,4,1]))
-  
+  return [max, min];
+}
+console.log(find_min_max([2, 5, 7, 4, 1]));
