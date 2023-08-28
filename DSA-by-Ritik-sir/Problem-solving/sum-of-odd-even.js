10756ed105a147de300410800ef256de9ea67283
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-28 22:22:02 +0530

    sum of odd even

--- DSA-by-Ritik-sir/Problem-solving/sum-of-odd-even.js
+++ DSA-by-Ritik-sir/Problem-solving/sum-of-odd-even.js
@@ -1,25 +1,22 @@
-// Q. Given an integer array, and a number ‘n’, please return the sum of integers at odd position and sum of 
+// Q. Given an integer array, and a number ‘n’, please return the sum of integers at odd position and sum of
 // integers at even position.
 // Input:
 // [1, 5, 7, 8]
 // Output:
 // [13,8]
 
-function odd_even_position(arr){
-    let n=arr.length
-    let odd=0;
-    let even=0;
-    for(let i=0;i<n;i++){
-      if(i%2==0){
-        even+=arr[i];
-     
-      }
-      else{
-        odd+=arr[i];
-     
-      }
+function odd_even_position(arr) {
+  let n = arr.length;
+  let odd = 0;
+  let even = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+      even += arr[i];
+    } else {
+      odd += arr[i];
     }
-    return [odd,even]
   }
-  console.log(odd_even_position([1, 5, 7, 8]))
-  console.log(odd_even_position([1, 2, 3]))
+  return [odd, even];
+}
+console.log(odd_even_position([1, 5, 7, 8]));
+console.log(odd_even_position([1, 2, 3]));
