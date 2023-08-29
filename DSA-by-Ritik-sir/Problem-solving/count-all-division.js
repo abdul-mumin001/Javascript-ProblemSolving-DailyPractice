74698c54fe94fcbdb075fe523b5e908ea54d2e8a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-29 22:54:49 +0530

    count all division

---
+++ DSA-by-Ritik-sir/Problem-solving/count-all-division.js
@@ -0,0 +1,19 @@
+// Q. Given an integer array, find all the elements which are divisible by the index position in which they are
+// present. Consider the index of array starts from 1.
+// Input:
+// [1, 5, 7, 8]
+// Output:
+// 2
+
+function count_all_divisions(arr){
+    let count=0;
+    for(let i=0;i<arr.length;i++){
+      const index=i+1
+      if(arr[i] % index === 0){
+        count++
+       
+      }
+    }
+     return count;
+   }
+   console.log(count_all_divisions([1, 5, 7, 8]))
