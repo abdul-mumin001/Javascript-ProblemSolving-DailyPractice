ea772e60f3cac83f3d038bdeb14866d3ad95cc72
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-30 22:15:15 +0530

    square of an array

--- DSA-by-Ritik-sir/Problem-solving/Square-of-array.js
+++ DSA-by-Ritik-sir/Problem-solving/Square-of-array.js
@@ -1,17 +1,16 @@
 // Q Given an array, start index and an end index, square the elements in the given range and return the array
-Input:
-' [2, 5, 10, 7, 5, 5, 17, 20, 15, 44, 55] 4 8'
-Output:
-' [2,5,10,7,25,25,289,400,15,44,55] '
+// Input:
+// ' [2, 5, 10, 7, 5, 5, 17, 20, 15, 44, 55] 4 8'
+// Output:
+// ' [2,5,10,7,25,25,289,400,15,44,55] '
 
-function square(arr,start,end){
-    let x=[];
-    let arrayElement=(arr.slice(start,end))
-    for(let i of arrayElement){
-      x.push(i**2)
-    }
-    arr.splice(start,x.length,...x)
-    return arr;
+function square(arr, start, end) {
+  let x = [];
+  let arrayElement = arr.slice(start, end);
+  for (let i of arrayElement) {
+    x.push(i ** 2);
   }
-  console.log(square([2, 5, 10, 7, 5, 5, 17, 20, 15, 44, 55],
-  4, 8))
+  arr.splice(start, x.length, ...x);
+  return arr;
+}
+console.log(square([2, 5, 10, 7, 5, 5, 17, 20, 15, 44, 55], 4, 8));
