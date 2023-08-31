7c4b0b18ab18c059a45121080f96f778b4301f24
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-31 10:35:33 +0530

    Binary Search

---
+++ DSA-by-Ritik-sir/Problem-solving/Binary-search.js
@@ -0,0 +1,25 @@
+// Q Given an integer array, return true if it can be used for binary search else return false
+// Input:
+// `[1, 5, 7, 8]
+// `
+// Output:
+// `
+// true
+// `
+// Input:
+// `
+// [1, 3, 2]
+// `
+
+function binary_search(arr){
+    let x=0;
+   
+    for(let i=1;i<x;i++){
+        return false;
+      }
+        return true;
+      
+    }
+  
+  console.log(binary_search([1, 5, 7, 8]))
+  
