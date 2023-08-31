a64e73b3f7f4cb027cd28dc19b2c3e208febeb67
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-08-31 10:48:58 +0530

    Binary Search

--- DSA-by-Ritik-sir/Problem-solving/Binary-search.js
+++ DSA-by-Ritik-sir/Problem-solving/Binary-search.js
@@ -10,15 +10,18 @@
 // `
 // [1, 3, 2]
 // `
+// Output:
+// `
+// false
+// `
 
 function binary_search(arr) {
-  let x = 0;
-
-  for (let i = 1; i < x; i++) {
-    return false;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
   }
   return true;
 }
-
 console.log(binary_search([1, 5, 7, 8]));
 console.log(binary_search([1, 3, 2]));
