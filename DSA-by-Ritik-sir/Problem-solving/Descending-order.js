1a3db36c15b45f4ef7049ec1457b9856c839da07
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-01 22:29:49 +0530

    Descending order

---
+++ DSA-by-Ritik-sir/Problem-solving/Descending-order.js
@@ -0,0 +1,25 @@
+// // Q Given an array, return the descending order
+// Input:
+// ```
+// [1, 5, 7, 8]
+// ```
+// Output:
+// ```
+// [8,7,5,1]
+// ```
+// Input:
+// ```
+// [1, 2, 3, 4, 3]
+ 
+// ```
+// Output:
+// ```
+// [4,3,3,2,1]
+// ```
+
+function descending_order(arr){
+    arr.sort((a,b)=>a-b)
+   return arr.reverse()
+   
+ }
+ console.log(descending_order([1, 5, 7, 8]))
