e64c3b8f0268f6de96e5a31b65677235f16fecb0
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-05 21:58:57 +0530

    Even Digits

--- DSA-by-Ritik-sir/Problem-solving/Even-digits.js
+++ DSA-by-Ritik-sir/Problem-solving/Even-digits.js
@@ -4,7 +4,7 @@
 // Input:
 // A single integer is provided as input.
 // Output:
-// Return a list of even digits of the number if multiple digits are combine it in string.Return -1 in case there 
+// Return a list of even digits of the number if multiple digits are combine it in string.Return -1 in case there
 // are no even digits
 
 // all ans should be in string
@@ -15,22 +15,20 @@
 // Output:
 // 204
 
-function evenDigits(n){
-  
-    if(n==0){
-      return "0"
-    }else{
-      let str = "";
-      let digit;
-      while(n!= 0){
-      digit = n%10;
-      n = Math.floor(n/10);
-      if(digit%2==0){
-          str = digit+str;
+function evenDigits(n) {
+  if (n == 0) {
+    return "0";
+  } else {
+    let str = "";
+    let digit;
+    while (n != 0) {
+      digit = n % 10;
+      n = Math.floor(n / 10);
+      if (digit % 2 == 0) {
+        str = digit + str;
       }
     }
-    return str == ""?"-1":str;
-      
-    }
+    return str == "" ? "-1" : str;
   }
-  console.log(evenDigits(12034))
+}
+console.log(evenDigits(12034));
