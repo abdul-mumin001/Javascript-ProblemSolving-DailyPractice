6311555cae9b897b2713513bc3046af21604ad35
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-07 22:35:53 +0530

    sum pattern

--- DSA-by-Ritik-sir/Problem-solving/sum-pattern.js
+++ DSA-by-Ritik-sir/Problem-solving/sum-pattern.js
@@ -1,4 +1,4 @@
-// Q Display this following sum pattern: 
+// Q Display this following sum pattern:
 
 // no test cases are provided
 //   1
@@ -7,16 +7,17 @@
 //   28 36 45 55
 //   66 78 91 105 120
 
-function sum(n){
-    let count=0,sum=0;
-    for(i=1; i<=n; i++){
-      digit="";
-      for(j=1; j<=i; j++){
-        count++;
-        sum=sum+count;
-        digit+=`${sum} `;
-      }
-      console.log(digit)
-     }
+function sum(n) {
+  let count = 0,
+    sum = 0;
+  for (i = 1; i <= n; i++) {
+    digit = "";
+    for (j = 1; j <= i; j++) {
+      count++;
+      sum = sum + count;
+      digit += `${sum} `;
     }
-    sum(5)
+    console.log(digit);
+  }
+}
+sum(5);
