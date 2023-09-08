646e239604dead2415ba03bfa83c1b605d76b1c8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-08 22:05:09 +0530

    sum of factors

---
+++ DSA-by-Ritik-sir/Problem-solving/sum-of-factors.js
@@ -0,0 +1,26 @@
+// Q. Only use recursion
+// Input a Number and return number of digits.
+// Constraints:
+// 1<=n<=100000
+// Input:
+// "N" - integer number as a function parameter
+
+// Output:
+// number of digits
+// Input
+
+// eg 1: 
+// n = 10
+// output:
+// 2
+
+function numberOfDigits(n){
+    if(n<10){
+      return n;
+    }
+     let last=n%10;
+     let remaining=Math.floor(n/10)
+     return numberOfDigits((last+1)+ numberOfDigits(remaining));
+   
+   }
+   console.log(numberOfDigits(10))
