e2337001af5638de867a71cccf9736caa0895262
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-13 22:19:28 +0530

    Am and Pm

--- DSA-by-Ritik-sir/Problem-solving/amAndPm.js
+++ DSA-by-Ritik-sir/Problem-solving/amAndPm.js
@@ -1,13 +1,13 @@
-// Q Take a time in 24 hour fomrat and onvert it to 12 hour format. 
+// Q Take a time in 24 hour fomrat and onvert it to 12 hour format.
 // Also if hour or minute is of single digit add "0" before it
 
-// Input: 
+// Input:
 // hour and min will be provided as two different parameters in function
-// output: 
+// output:
 // time in 12 hour format with AM and PM written
 
-// eg: 
-// hour = 13 
+// eg:
+// hour = 13
 // min = 56
 // output:
 // 1:56 PM
@@ -18,11 +18,11 @@
 // output:
 // 07:25 AM
 
-function amPm(hr,min){
-    // let time=24hour;
-    var s=hr>=12 ? "PM":"AM"
-   var hr=((hr+11)%12+1)
-   return (hr+":"+min+" "+s)
- }
- console.log(amPm(13,56))
- console.log(amPm(7,25));
+function amPm(hr, min) {
+  // let time=24hour;
+  var s = hr >= 12 ? "PM" : "AM";
+  var hr = ((hr + 11) % 12) + 1;
+  return hr + ":" + min + " " + s;
+}
+console.log(amPm(13, 56));
+console.log(amPm(7, 25));
