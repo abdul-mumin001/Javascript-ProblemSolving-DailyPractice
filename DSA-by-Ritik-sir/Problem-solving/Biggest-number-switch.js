39c6904c81d488415da790cc0d74535f2a4ce168
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-14 22:33:58 +0530

    Biggest number

---
+++ DSA-by-Ritik-sir/Problem-solving/Biggest-number-switch.js
@@ -0,0 +1,48 @@
+// Q. Use Switch case:
+// #No if else is allowed.
+// Take 3 numbers and find  and return biggest of them.
+// If all three numbers are same than print "None of them is biggest"
+
+// if there are two numbers same as biggest than also print "None of them is biggest"
+
+// Eg: 
+// 10<br>
+// 30 <br>
+// 20 <br>
+
+// Output: 
+// 30
+
+// Eg: 
+// 11<br>
+// 22<br>
+// 22<br>
+// Output:
+// "None of them is biggest"
+
+
+function biggest(a,b,c){
+
+    switch(a>b && a>c){
+      case true:
+        return a;
+        break;
+    }
+    switch(a<b && b>c){
+      case true:
+        return b;
+        break;
+    }
+    switch(a<c && b<c){
+      case true:
+        return c;
+        break;
+    }
+    switch(a==b || b==c || a==c){
+      case true:
+        return ( "None of them is biggest");
+        break;
+    }
+  }
+  console.log(biggest(10,30,20))
+  
