ef0f61897a42f223fec32347bf1a075160c0f5aa
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-17 22:42:28 +0530

    Timely

--- DSA-by-Ritik-sir/Problem-solving/Timely.js
+++ DSA-by-Ritik-sir/Problem-solving/Timely.js
@@ -6,19 +6,18 @@
 // hour, mins and seconds with a
 // message as in example.
 
-// Eg: 
+// Eg:
 // input: <br>
 // 5000
 // Output: <br>
 // In 5000 seconds 1 hours , 23 minutes and 20 seconds are possible
 
-function time(seconds){
-    let num=seconds;
-      let h=Math.floor(num/(60*60));
-      let m=Math.floor(num/60)-h*60;
-      let s=num%60
-      return (h+":"+m+":"+s);
-     
-    }
-    console.log(time(5000))
-    console.log(time(3000));
+function time(seconds) {
+  let num = seconds;
+  let h = Math.floor(num / (60 * 60));
+  let m = Math.floor(num / 60) - h * 60;
+  let s = num % 60;
+  return h + ":" + m + ":" + s;
+}
+console.log(time(5000));
+console.log(time(3000));
