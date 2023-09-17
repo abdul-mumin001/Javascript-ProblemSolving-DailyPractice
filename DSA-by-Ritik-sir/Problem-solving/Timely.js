f2f7dcc95f90fb26d87ce29545e490fb13f14d03
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-17 22:41:13 +0530

    Timely

---
+++ DSA-by-Ritik-sir/Problem-solving/Timely.js
@@ -0,0 +1,23 @@
+// Q. Write a code to take time in seconds and convert it into hour, mins and seonds
+
+// Input:<br>
+// already given as function parameter
+// Output: <br>
+// hour, mins and seconds with a
+// message as in example.
+
+// Eg: 
+// input: <br>
+// 5000
+// Output: <br>
+// In 5000 seconds 1 hours , 23 minutes and 20 seconds are possible
+
+function time(seconds){
+    let num=seconds;
+      let h=Math.floor(num/(60*60));
+      let m=Math.floor(num/60)-h*60;
+      let s=num%60
+      return (h+":"+m+":"+s);
+     
+    }
+    console.log(time(5000))
