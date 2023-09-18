1edbc73bbb56900e23ac2b5936e1c3ce3a79757a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-18 22:23:24 +0530

    cube of a number

--- DSA-by-Ritik-sir/Problem-solving/cube-of-a-number.js
+++ DSA-by-Ritik-sir/Problem-solving/cube-of-a-number.js
@@ -1,12 +1,12 @@
-// Q . Take a number and find its cube. 
-// Input: 
+// Q . Take a number and find its cube.
+// Input:
 // Input is provided as Functional Parameter.
-// Output: 
+// Output:
 // Return cube of the number.
 
-// Eg 1: 
+// Eg 1:
 // 1 <br>
-// output: 
+// output:
 // 1
 
 // Eg 2 :
@@ -14,10 +14,9 @@
 // output :
 // -1
 
-function cube(a){
-  let z=a**3;
-    return z;
-
-  }
-  console.log(cube(3))
-  console.log(cube(5));
+function cube(a) {
+  let z = a ** 3;
+  return z;
+}
+console.log(cube(3));
+console.log(cube(5));
