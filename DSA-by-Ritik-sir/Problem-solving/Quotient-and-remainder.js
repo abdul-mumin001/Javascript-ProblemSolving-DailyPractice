821da621e95d3cb0752a12ac370fa92ba7a46e3f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-19 22:35:01 +0530

    Quotient and remainder

--- DSA-by-Ritik-sir/Problem-solving/Quotient-and-remainder.js
+++ DSA-by-Ritik-sir/Problem-solving/Quotient-and-remainder.js
@@ -5,10 +5,10 @@
 // Output: <br>
 // Return comma seperated quotient and remainder.
 
-// Eg 1: 
+// Eg 1:
 // 10 <br>
 // 2
-// output: 
+// output:
 // 5,0
 
 // Eg 2:
@@ -19,15 +19,12 @@
 
 // Note :- If you cannot divide the numbers then the output must be "-1,-1".
 
+function quo_rem(a, b) {
+  let ans = b == 0 ? `-1,-1` : ``;
 
-function quo_rem(a,b){
-
-    let ans= b==0 ? `-1,-1`:``;
-
-    let quo=Math.floor(a/b);
-    let rem=a%b;
-    return (quo+","+rem)
-
+  let quo = Math.floor(a / b);
+  let rem = a % b;
+  return quo + "," + rem;
 }
-console.log(quo_rem(7,3));
-console.log(quo_rem(10,2));
+console.log(quo_rem(7, 3));
+console.log(quo_rem(10, 2));
