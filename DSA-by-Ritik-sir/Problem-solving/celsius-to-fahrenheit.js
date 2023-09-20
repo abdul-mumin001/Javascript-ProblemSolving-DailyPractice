eb56c0bb27e42ea416756aef03ab01bd0a899cce
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-20 22:36:52 +0530

    Celsius to Fahrenheit

---
+++ DSA-by-Ritik-sir/Problem-solving/celsius-to-fahrenheit.js
@@ -0,0 +1,20 @@
+// Q. Celsius to Fahrenheit
+// Take temperature as input and convert it from celsius to fahrenheit. 
+
+// Eg 1: 
+// 32 <br>
+// Output: 
+// 89.60
+
+// Eg 2:
+// 25 <br>
+// Output :
+// 77
+
+
+function celToFah(a){
+    let temp = a;
+    let Fah = (temp*9/5)+32;
+    return Fah;
+}
+console.log(celToFah(25));
