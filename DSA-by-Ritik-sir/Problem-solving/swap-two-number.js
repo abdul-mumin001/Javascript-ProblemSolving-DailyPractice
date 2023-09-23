ff967c48943cc19db392dc87a5342642e150add7
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-23 23:46:39 +0530

    Swap two number

--- DSA-by-Ritik-sir/Problem-solving/swap-two-number.js
+++ DSA-by-Ritik-sir/Problem-solving/swap-two-number.js
@@ -1,10 +1,10 @@
 // Q. Swap two numbers using a 3rd variable
 // Take two numbers and swap them.
 
-// Eg 1: 
+// Eg 1:
 // 10 <br>
 // 2
-// output: 
+// output:
 // 2,10
 
 // Eg 2:
@@ -13,14 +13,11 @@
 // output :
 // 2,-10
 
-
-function swap(a,b){
-
-  a=a+b;
-    b=a-b;
-    a=a-b;
-    return(a+","+b);
-
-  } 
-  console.log(swap(-10,2))
-  console.log(swap(2,10));
+function swap(a, b) {
+  a = a + b;
+  b = a - b;
+  a = a - b;
+  return a + "," + b;
+}
+console.log(swap(-10, 2));
+console.log(swap(2, 10));
