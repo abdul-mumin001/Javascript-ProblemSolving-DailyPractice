607af98eefdab8571d306a98c250f5258cba763f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-26 23:40:06 +0530

    Average salary

--- DSA-by-Ritik-sir/Problem-solving/average-salary.js
+++ DSA-by-Ritik-sir/Problem-solving/average-salary.js
@@ -1,9 +1,9 @@
 // Q. Average Salary
 // Take salary of five employees and print the average salary of all the employees.
 
-// Eg 1: 
+// Eg 1:
 // 10 20 30 40 50 <br>
-// output: 
+// output:
 // 30
 
 // Eg 2:
@@ -11,11 +11,9 @@
 // output :
 // 30.6
 
-function average_salary(a,b,c,d,e){
-    
-    let avgSal=(a+b+c+d+e)/5;
-    return avgSal;
-   
-  }
-  console.log(average_salary(10,20,30,40,50))
-  console.log(average_salary(10,22,31,40,50));
+function average_salary(a, b, c, d, e) {
+  let avgSal = (a + b + c + d + e) / 5;
+  return avgSal;
+}
+console.log(average_salary(10, 20, 30, 40, 50));
+console.log(average_salary(10, 22, 31, 40, 50));
