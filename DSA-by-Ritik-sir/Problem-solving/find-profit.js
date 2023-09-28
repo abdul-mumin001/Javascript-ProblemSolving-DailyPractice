f72c9f7ed0c275aa6b8ddba5eb90b986872955b2
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-28 23:46:19 +0530

    find profit

---
+++ DSA-by-Ritik-sir/Problem-solving/find-profit.js
@@ -0,0 +1,18 @@
+// Q. Find Profit
+// Aman is buying the apple at price x and selling the same apple to Ankush at price y. Take input as x and y and print the profit of Aman.
+
+// Eg 1: 
+// X = 100 <br>
+// Y = 120
+// Output: 
+// 20
+
+function find_profit(sellPrice,costPrice){
+    
+    let profit=costPrice-sellPrice;
+    return profit;
+    
+  }
+  console.log(find_profit(100,120))
+ 
+
