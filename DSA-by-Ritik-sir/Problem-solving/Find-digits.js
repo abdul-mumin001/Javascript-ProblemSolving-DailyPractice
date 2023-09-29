11179423c0b1a4aa7b8e13d826e25aa48a2dc1cc
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-29 23:52:11 +0530

    find digits

--- DSA-by-Ritik-sir/Problem-solving/Find-digits.js
+++ DSA-by-Ritik-sir/Problem-solving/Find-digits.js
@@ -1,9 +1,9 @@
 // Q First Digit
 // Take a number and print out the first digit of the number.
 
-// Eg 1: 
+// Eg 1:
 // 123 <br>
-// Output: 
+// Output:
 // 1
 
 // Eg 2:
@@ -11,11 +11,10 @@
 // Output :
 // 5
 
-function first_digit(a){
-    //write your code here
-    let z=Math.floor(a/100);
-    return z;
-    // return the output
-  }
-  console.log(first_digit(123))
-  
+function first_digit(a) {
+  //write your code here
+  let z = Math.floor(a / 100);
+  return z;
+  // return the output
+}
+console.log(first_digit(123));
