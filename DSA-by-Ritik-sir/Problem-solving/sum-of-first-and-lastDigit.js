8dedcb127abf9e7f0a9e781171d27b53a346cd66
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-09-30 22:57:37 +0530

    sum of first and last digit

---
+++ DSA-by-Ritik-sir/Problem-solving/sum-of-first-and-lastDigit.js
@@ -0,0 +1,31 @@
+// Q Sum Of First and Last Digit of a Number
+// Take a number and return the sum of first and last digit.
+
+// Eg 1: 
+// 10 <br>
+// Output: 
+// 1
+
+// Eg 2:
+// 78 <br>
+// Output :
+// 15
+
+function sum(a){
+
+    let sum=0;
+    
+    let lastDigit=(a%10)
+    a=Math.abs(a)
+    
+    let firstDigit=a;
+    
+    while(firstDigit>=10){
+      firstDigit=Math.floor(firstDigit/10)
+    }
+    
+    sum=firstDigit+lastDigit;
+    return sum;
+  
+  }
+  console.log(sum(10))
