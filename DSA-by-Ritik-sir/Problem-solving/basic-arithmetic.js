1abc6293edcdf7e9ae65e7448a7585119bc022ea
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-03 22:52:14 +0530

    basic arithmetic

--- DSA-by-Ritik-sir/Problem-solving/basic-arithmetic.js
+++ DSA-by-Ritik-sir/Problem-solving/basic-arithmetic.js
@@ -8,7 +8,7 @@
 // ```
 // 12 -8 20 0 2
 // ```
-// Explanation 1: 
+// Explanation 1:
 // ```
 // 2 + 10 = 12
 // 2 - 10 = -8
@@ -17,14 +17,12 @@
 // 2 % 10 = 2
 
 function basicArithmetic(num1, num2) {
-
-    let sum=num1 + num2;
-    let sub=num1 - num2;
-    let mul=num1 * num2;
-    let quo= Math.floor(num1 / num2);
-    let mod=num1 % num2;
-    return (sum+" "+sub+" "+mul+" "+quo+" "+mod);
-
-  }
-  console.log(basicArithmetic(2,10));
-  console.log(basicArithmetic(4,20));
+  let sum = num1 + num2;
+  let sub = num1 - num2;
+  let mul = num1 * num2;
+  let quo = Math.floor(num1 / num2);
+  let mod = num1 % num2;
+  return sum + " " + sub + " " + mul + " " + quo + " " + mod;
+}
+console.log(basicArithmetic(2, 10));
+console.log(basicArithmetic(4, 20));
