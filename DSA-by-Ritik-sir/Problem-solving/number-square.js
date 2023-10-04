b8b48b108eb204b88077906fe1c98eec6368f18d
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-04 23:11:10 +0530

    number square

---
+++ DSA-by-Ritik-sir/Problem-solving/number-square.js
@@ -0,0 +1,36 @@
+// Q Take a number and print the square of the number
+
+// Input 1
+// ```
+// 2
+// ```
+// Output 1:
+// ```
+// 4
+// ```
+// Explanation 1: 
+// ```
+// 2 * 2 = 4
+// ```
+// Input 2
+// ```
+// 11
+// ```
+// Output 2:
+// ```
+// 121
+// ```
+// Explanation 2: 
+// ```
+// 11 * 11 = 121
+// ```
+
+function numberSquare(num) {
+ 
+    let a=num;
+    a=num**2
+    return a;
+
+  }
+  console.log(numberSquare(5))
+  
