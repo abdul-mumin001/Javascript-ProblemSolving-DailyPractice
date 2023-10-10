51f89fac23301e295309f65ca218dcb491f9aac3
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-10 23:06:14 +0530

    Profit

---
+++ DSA-by-Ritik-sir/Problem-solving/profit.js
@@ -0,0 +1,25 @@
+// Q Take the cost price and selling price of a product, and print the profit obtained on the product.
+
+// Input 1
+// ```
+// 50 100
+// ```
+
+// Output 1:
+// ```
+// 50
+// ```
+// Explanation 1: 
+
+// ```
+// 100 - 50 = 50
+// ```
+
+function profit(costPrice, sellingPrice){
+
+    let a=costPrice;
+    let b=sellingPrice;
+    z=b-a
+    return z;
+  }
+  console.log(profit(50,100))
