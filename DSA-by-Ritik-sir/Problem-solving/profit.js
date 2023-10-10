1277a00498dee1ae36b55fba6db5d4e6cd5914a8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-10 23:07:42 +0530

    Profit

--- DSA-by-Ritik-sir/Problem-solving/profit.js
+++ DSA-by-Ritik-sir/Problem-solving/profit.js
@@ -9,18 +9,17 @@
 // ```
 // 50
 // ```
-// Explanation 1: 
+// Explanation 1:
 
 // ```
 // 100 - 50 = 50
 // ```
 
-function profit(costPrice, sellingPrice){
-
-    let a=costPrice;
-    let b=sellingPrice;
-    z=b-a
-    return z;
-  }
-  console.log(profit(50,100))
-  console.log(profit(30,20));
+function profit(costPrice, sellingPrice) {
+  let a = costPrice;
+  let b = sellingPrice;
+  z = b - a;
+  return z;
+}
+console.log(profit(50, 100));
+console.log(profit(30, 20));
