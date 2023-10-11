b3f43e9e22ecb564d8e30eb9543247dd93c2d4a3
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-11 23:01:23 +0530

    Last of number

---
+++ DSA-by-Ritik-sir/Problem-solving/Last-digit-of-number.js
@@ -0,0 +1,26 @@
+// Q Take a number and print out the last digit of the number.
+
+// Input 1
+
+// ```
+// 123
+// ```
+// Output 1:
+
+// ```
+// 3
+// ```
+// Explanation 1: 
+
+// ```
+// Last Digit of 123 is 3
+// ````
+
+function lastDigit(num){
+
+    let a=num;
+    b=Math.ceil(a%10)
+    return b;
+    
+  }
+  console.log(lastDigit(123)) 
