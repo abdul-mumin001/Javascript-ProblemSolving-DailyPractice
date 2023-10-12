2b98f60623cd27a8d1c9f50347bce6dfbf250cdf
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-12 22:26:50 +0530

    First and Last Digit of a number

---
+++ DSA-by-Ritik-sir/Problem-solving/FirstAndLastDigit-of-a-number.js
@@ -0,0 +1,27 @@
+// Q Take a number and return the first and last digit of the number.
+
+// Input 1
+
+// ```
+// 123
+// ```
+// Output 1:
+
+// ```
+// 1 3
+// ```
+// Explanation 1: 
+// ```
+// First digit of 123 is 1
+// and last digit of 123 is 3
+// ```
+
+function firstAndLast(num) {
+
+    let a = num;
+    b = (a % 10)
+    c = Math.floor(a / 100)
+    return (c + " " + b)
+  
+  }
+  console.log(firstAndLast(123))
