7978a16e735622068151888dd56f576c4892a15f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-13 23:11:22 +0530

    Two digits reverse

---
+++ DSA-by-Ritik-sir/Problem-solving/Two-digits-reverse.js
@@ -0,0 +1,32 @@
+// Q You will be given a two digit number you have to reverse that number
+
+// Input Format:
+// A two digit integer ```num``` is given as a function parameters.
+// Input Constraints
+// ```
+// 10 < a < 100
+// ```
+// Input 
+// ```
+// 32
+// ```
+// Output :
+
+// ```
+// 23
+// ```
+// Explanation : 
+// ```
+// Reverse of 32 will be 23
+// ```
+
+function twoDigitReverse(num) {
+
+    let a = num;
+    let first_part = Math.floor(a / 10)
+    let second_part = a % 10
+    let reverse_number = second_part * 10 + first_part
+    return reverse_number;
+  
+  }
+  console.log(twoDigitReverse(32))
