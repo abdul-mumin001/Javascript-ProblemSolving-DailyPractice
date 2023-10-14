690a6f1801c16d6e5f27245f8d9fa67b1a1820f9
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-14 21:17:48 +0530

    Area Of ractangle

---
+++ DSA-by-Ritik-sir/Problem-solving/Area-of-ractangle.js
@@ -0,0 +1,22 @@
+// Q Area of a Rectangle
+// Given length and breadth of a rectangle return the area of a rectangle.
+
+// Eg 1: 
+// 10 <br>
+// 2
+// output: 
+// 20
+// Eg 2:
+// 7 <br>
+// 3
+// output :
+// 21
+
+function rectangle_area(length,breadth){
+    // write your code here
+  let areaOfRectangle=length*breadth;
+    return areaOfRectangle;
+    // return your answer
+  }
+  console.log(rectangle_area(10,2))
+  
