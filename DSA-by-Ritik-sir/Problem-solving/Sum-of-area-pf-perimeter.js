f493d1099a9a4c44210679c0ddd91d2529d79687
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-15 21:55:57 +0530

    Sum-of-area-pf-perimeeter

--- DSA-by-Ritik-sir/Problem-solving/Sum-of-area-pf-perimeter.js
+++ DSA-by-Ritik-sir/Problem-solving/Sum-of-area-pf-perimeter.js
@@ -1,10 +1,10 @@
 // Q Sum of area and perimeter of a rectangle
 // Given length and breadth of a rectangle return the sum of area and perimeter of the rectangle.
 
-// Eg 1: 
+// Eg 1:
 // 10 <br>
 // 2
-// output: 
+// output:
 // 44
 
 // Eg 2:
@@ -13,15 +13,13 @@
 // output :
 // 41
 
-function sum(length,breadth){
-    
-    let sum=0;
-    let area=length*breadth;
-    let perimeter=2*(length+breadth);
-    console.log(area);
-    console.log(perimeter);
-    sum=area+perimeter;
-    return sum;
-   
-  }
-  console.log(sum(10,2))
+function sum(length, breadth) {
+  let sum = 0;
+  let area = length * breadth;
+  let perimeter = 2 * (length + breadth);
+  console.log(area);
+  console.log(perimeter);
+  sum = area + perimeter;
+  return sum;
+}
+console.log(sum(10, 2));
