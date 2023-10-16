c78251917d34bcc0c6b1469f7a58f38f107b1c61
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-16 21:53:16 +0530

    Number of Days

---
+++ DSA-by-Ritik-sir/Problem-solving/Number-of-days.js
@@ -0,0 +1,22 @@
+// Q Number of Days
+// Given years,months and days as input, return the number of days as output.
+
+// Eg 1: 
+// Years = 0 , Months = 1, Days = 5
+// output: 
+// 35
+
+// Eg 2:
+// Years = 1 , Months = 3 , Days = 12
+// output :
+// 467
+
+
+function days(years,months,days){
+    
+    
+    let NoOfDays=(years*365)+(months*30)+days;
+    return NoOfDays;
+  
+  }
+  console.log(days(1,3,12))
