1620e6ed8259898bba50f62b86c36ce129682054
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-17 21:37:00 +0530

    Number of seconds

--- DSA-by-Ritik-sir/Problem-solving/Number-of-seconds.js
+++ DSA-by-Ritik-sir/Problem-solving/Number-of-seconds.js
@@ -1,9 +1,9 @@
 // Q Number of Seconds
 // Given the number of hours,minutes and seconds, return the number of seconds.
 
-// Eg 1: 
+// Eg 1:
 // Hours = 1, Minutes = 2 , Seconds = 32
-// output: 
+// output:
 // 3752
 
 // Eg 2:
@@ -11,12 +11,9 @@
 // output :
 // 359
 
-
-function seconds(hours,minutes,seconds){
-    
-  let ns=(hours*60*60)+(minutes*60)+seconds;
-    return ns;
-   
-  }
-  console.log(seconds(1,2,32))
-  console.log(seconds(0,5,59));
+function seconds(hours, minutes, seconds) {
+  let ns = hours * 60 * 60 + minutes * 60 + seconds;
+  return ns;
+}
+console.log(seconds(1, 2, 32));
+console.log(seconds(0, 5, 59));
