b6564acb71c80178b12851b0dff7d1290cc203cb
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-18 23:26:00 +0530

    Sum-0f-square-and-cube

---
+++ DSA-by-Ritik-sir/Problem-solving/Sum-0f-square-and-cube.js
@@ -0,0 +1,20 @@
+// Q Sum of Square and Cube of a Number
+// Given a number return its sum of square and cube.
+
+// Eg 1: 
+// 10 <br>
+// Output: 
+// 1100
+
+// Eg 2:
+// 25 <br>
+// output :
+// 16250
+
+function sum(num){
+    
+  let SumOfSquareaCube=num**2+num**3;
+    return SumOfSquareaCube;
+    
+  }
+  console.log(sum(10))
