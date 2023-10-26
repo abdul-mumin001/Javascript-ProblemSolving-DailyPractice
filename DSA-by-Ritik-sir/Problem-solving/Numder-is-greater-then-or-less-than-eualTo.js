c6013e7e032efb5a5c40c69b1787b94c829ead55
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-26 21:29:12 +0530

    number is equal to zero, less than zero, or greater than zero

--- DSA-by-Ritik-sir/Problem-solving/Numder-is-greater-then-or-less-than-eualTo.js
+++ DSA-by-Ritik-sir/Problem-solving/Numder-is-greater-then-or-less-than-eualTo.js
@@ -1,9 +1,9 @@
 // Q number is equal to zero, less than zero, or greater than zero.
 // Take one number and check whether number is equal to zero, less than zero, or greater than zero.
 
-// Eg 1: 
+// Eg 1:
 // 10 <br>
-// output: 
+// output:
 // 1
 // Eg 2:
 // -7 <br>
@@ -15,18 +15,15 @@
 // output :
 // 0
 
-
-function checkNumber(num){
-
-  if(num>1){
+function checkNumber(num) {
+  if (num > 1) {
     return 1;
-    
-  }if(num<0){
-      return -1
-    }else {
-      return 0;
-    }
-   
-  }  
-  console.log(checkNumber(10))
-  console.log(checkNumber(-7))
+  }
+  if (num < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+console.log(checkNumber(10));
+console.log(checkNumber(-7));
