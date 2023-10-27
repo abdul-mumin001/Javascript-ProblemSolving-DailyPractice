1f80768f43347083226d1c2fb37bf420c55ff731
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-27 22:49:07 +0530

    Profit-or-loss

---
+++ DSA-by-Ritik-sir/Problem-solving/Profit-or-loss.js
@@ -0,0 +1,24 @@
+// Q Profit or Loss
+// Aman is buying the apple at price x and selling the same apple to Ankush at price y. Check if Aman is in Profit
+// or Loss.
+
+// Eg 1: 
+// x = 50, y = 70
+// Output: 
+// profit
+
+// Eg 2:
+// x = 50, y = 30
+// output :
+// loss
+
+function determine(x,y){
+   
+  if(x<y){
+    return "profit"
+  }else{
+    return "loss"
+  }
+    
+  }
+  console.log(determine(50,70))
