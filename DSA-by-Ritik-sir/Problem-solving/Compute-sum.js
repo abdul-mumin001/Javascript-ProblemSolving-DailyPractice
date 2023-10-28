d8efde56176ba52a5343a25c60fafe61fab665fc
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-28 22:37:59 +0530

    Compute sum

---
+++ DSA-by-Ritik-sir/Problem-solving/Compute-sum.js
@@ -0,0 +1,28 @@
+// Q Compute Sum
+// Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then 
+// returns triple their sum.
+
+// Eg 1: 
+// 10 <br>
+// 2
+// Output: 
+// 12
+
+// Eg 2:
+// 7 <br>
+// 7
+// Output :
+// 42
+
+function compute_sum(num1,num2){
+
+  let x=num1+num2;
+    let y=(num1+num2)*3;
+    if(num1==num2){
+      return y;
+    }else{
+      return x;
+    }
+
+  }
+  console.log(compute_sum(10,2))
