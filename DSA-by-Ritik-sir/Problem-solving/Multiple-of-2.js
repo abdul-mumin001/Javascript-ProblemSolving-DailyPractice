ff539b52b02a789393e19941a76688294a9e5961
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-30 22:09:51 +0530

    Multiple of 2

---
+++ DSA-by-Ritik-sir/Problem-solving/Multiple-of-2.js
@@ -0,0 +1,24 @@
+// Q Multiple of 2
+// Write a JS code to check if the given number is multiple of two.
+
+// Eg 1: 
+// 10 <br>
+// Output: 
+// yes
+
+// Eg 2:
+// 7 <br>
+// Output :
+// no
+
+
+function is_multiple(num){
+    
+  if(num%2==0){
+    return "yes"
+  }else{
+    return "no"
+  }
+   
+  }
+  console.log(is_multiple(7))
