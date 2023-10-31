5511fccd11bcb8e2284729f1d94dfb83c75f1b6f
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-31 21:39:12 +0530

    Hello bye

---
+++ DSA-by-Ritik-sir/Problem-solving/Hello-bye.js
@@ -0,0 +1,24 @@
+// Q Hello-Bye
+// Write a program to display "Hello" if a number entered by the user is a multiple of five , otherwise print 
+// "Bye".
+
+// Eg 1: 
+// 10 <br>
+// Output: 
+// "Hello"
+
+// Eg 2:
+// 7 <br>
+// Output :
+// "Bye"
+
+function hello_bye(num){
+   
+  if(num%5==0){
+    return "Hello"
+  }else{
+    return "Bye"
+  }
+  
+  }
+  console.log(hello_bye(7))
