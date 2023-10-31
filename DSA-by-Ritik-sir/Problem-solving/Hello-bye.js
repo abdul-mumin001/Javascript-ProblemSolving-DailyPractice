c2c2afb78f83f8e800ed74049ed4afb12093c2b7
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-10-31 21:40:17 +0530

    Hello bye

--- DSA-by-Ritik-sir/Problem-solving/Hello-bye.js
+++ DSA-by-Ritik-sir/Problem-solving/Hello-bye.js
@@ -1,10 +1,10 @@
 // Q Hello-Bye
-// Write a program to display "Hello" if a number entered by the user is a multiple of five , otherwise print 
+// Write a program to display "Hello" if a number entered by the user is a multiple of five , otherwise print
 // "Bye".
 
-// Eg 1: 
+// Eg 1:
 // 10 <br>
-// Output: 
+// Output:
 // "Hello"
 
 // Eg 2:
@@ -12,14 +12,12 @@
 // Output :
 // "Bye"
 
-function hello_bye(num){
-   
-  if(num%5==0){
-    return "Hello"
-  }else{
-    return "Bye"
+function hello_bye(num) {
+  if (num % 5 == 0) {
+    return "Hello";
+  } else {
+    return "Bye";
   }
-  
-  }
-  console.log(hello_bye(7))
-  console.log(hello_bye(10))
+}
+console.log(hello_bye(7));
+console.log(hello_bye(10));
