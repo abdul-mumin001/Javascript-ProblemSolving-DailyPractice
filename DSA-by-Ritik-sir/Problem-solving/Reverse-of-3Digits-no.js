743399f17015e5e345383cf65549fd623532380a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-02 22:24:29 +0530

    Reverse of 3-Digits number

--- DSA-by-Ritik-sir/Problem-solving/Reverse-of-3Digits-no.js
+++ DSA-by-Ritik-sir/Problem-solving/Reverse-of-3Digits-no.js
@@ -1,9 +1,9 @@
 // Q Reverse Of a Number
 // Write a program to check if the given 3 digit number is equal to its reverse or not.
 
-// Eg 1: 
+// Eg 1:
 // 101
-// Output: 
+// Output:
 // yes
 
 // Eg 2:
@@ -11,18 +11,16 @@
 // Output :
 // no
 
-function reverse(num){
-  let x=num%10;
-    let y=Math.floor((num/10)%10);
-    let z=Math.floor(num/100);
-    if(x==y==x || (x==z && y>=0)){
-      return "yes"
-    }else{
-      return "no"
-    }
-    
+function reverse(num) {
+  let x = num % 10;
+  let y = Math.floor((num / 10) % 10);
+  let z = Math.floor(num / 100);
+  if ((x == y) == x || (x == z && y >= 0)) {
+    return "yes";
+  } else {
+    return "no";
   }
-  console.log(reverse(111))
-  console.log(reverse(101))
-  console.log(reverse(527))
-  
+}
+console.log(reverse(111));
+console.log(reverse(101));
+console.log(reverse(527));
