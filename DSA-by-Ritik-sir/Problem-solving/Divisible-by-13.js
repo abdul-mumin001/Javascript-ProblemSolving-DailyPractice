bdf27b14b06f7aea33c541de0a33ccc984d2a7c0
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-04 22:06:16 +0530

    Divisible by 13

--- DSA-by-Ritik-sir/Problem-solving/Divisible-by-13.js
+++ DSA-by-Ritik-sir/Problem-solving/Divisible-by-13.js
@@ -1,10 +1,10 @@
 // Q Divisible by 13
-// Write a program to check if the given number is multiple of 13 or not. If the given number is multiple of 13 
+// Write a program to check if the given number is multiple of 13 or not. If the given number is multiple of 13
 // then return the 5th power of the number,else return the same number.
 
-// Eg 1: 
+// Eg 1:
 // 10 <br>
-// Output: 
+// Output:
 // 10
 
 // Eg 2:
@@ -12,15 +12,12 @@
 // Output :
 // 371293
 
-
-function divisible(num){
-    
-  if(num%13==0){
-    return num**5;
-  }else{
+function divisible(num) {
+  if (num % 13 == 0) {
+    return num ** 5;
+  } else {
     return num;
   }
-
-  }
-  console.log(divisible(10))
-  console.log(divisible(13))
+}
+console.log(divisible(10));
+console.log(divisible(13));
