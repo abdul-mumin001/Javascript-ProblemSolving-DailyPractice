3333133773190db51b0480e4ba2fee95a0374121
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-05 23:04:24 +0530

    Right triangle

---
+++ DSA-by-Ritik-sir/Problem-solving/Right-triangle.js
@@ -0,0 +1,23 @@
+// Q Take two numbers as input perpendicular and base respectively and write a program to print the hypotenuse of
+//  the right angle triangle.
+
+// Example 1
+// Input
+// ```
+// perpendicular = 3
+// base = 4
+// ```
+// Output
+// ```
+// 5
+// ```
+
+const hypotenuse = (perpendicular, base) => {
+
+    const b=base**2;
+    const p=perpendicular**2;
+    const sum= Math.sqrt(b+p);
+    return sum;
+    console.log(sum)
+  }
+  console.log(hypotenuse(4,3))
