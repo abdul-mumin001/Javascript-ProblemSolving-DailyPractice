2fb44b120be8e01dd63ceec3a75a6291c464deab
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-06 23:05:57 +0530

    Area of circle

---
+++ DSA-by-Ritik-sir/Problem-solving/Area-of-circle.js
@@ -0,0 +1,28 @@
+// Q Write a program to print the area of a circle by taking the radius as input.
+
+// Example 1
+// Input
+// ```
+// 4
+// ```
+// Output
+// ```
+// 50.26548245743669
+// ```
+// Example 2
+// Input
+// ```
+// 37
+// ```
+// Output
+// ```
+// 4300.840342764427
+// ```
+
+const calculateArea = (radius) => {
+
+  
+    const z= Math.PI*radius*radius;
+    return z;
+  }
+  console.log(calculateArea (4))
