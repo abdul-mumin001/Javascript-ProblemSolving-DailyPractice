5e9984dcfa5a3e36f48016ff598bbc8a1d5e5d6b
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-07 23:01:38 +0530

    Area of Triangle

--- DSA-by-Ritik-sir/Problem-solving/Area-of-Triangle.js
+++ DSA-by-Ritik-sir/Problem-solving/Area-of-Triangle.js
@@ -13,8 +13,8 @@
 // ```
 
 const area = (a, b, c) => {
-    const s=(a+b+c)/2;
-    const area=Math.sqrt(s*((s-a)*(s-b)*(s-c)))
-    return area;
-  }
-  console.log(area(3,4,5))
+  const s = (a + b + c) / 2;
+  const area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+  return area;
+};
+console.log(area(3, 4, 5));
