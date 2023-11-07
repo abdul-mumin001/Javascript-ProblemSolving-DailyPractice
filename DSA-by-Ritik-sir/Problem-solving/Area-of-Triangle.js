56c3fac3b108649d7920129bf9ba497d744156e9
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-07 23:01:06 +0530

    Area of Triangle

---
+++ DSA-by-Ritik-sir/Problem-solving/Area-of-Triangle.js
@@ -0,0 +1,20 @@
+// Q Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are given.
+
+// Example 1
+// Input
+// ```
+// side 1 = 3
+// side 2 = 4
+// side 3 = 5
+// ```
+// #### Output
+// ```
+// 6
+// ```
+
+const area = (a, b, c) => {
+    const s=(a+b+c)/2;
+    const area=Math.sqrt(s*((s-a)*(s-b)*(s-c)))
+    return area;
+  }
+  console.log(area(3,4,5))
