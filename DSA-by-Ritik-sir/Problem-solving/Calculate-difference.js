a27dc0a67e7fb1034ded1f5692d70b23b83902e3
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-08 23:02:06 +0530

    Calculate difference

---
+++ DSA-by-Ritik-sir/Problem-solving/Calculate-difference.js
@@ -0,0 +1,26 @@
+// Q Write a JavaScript program to get the difference between a given number and 13.
+
+// Example 1
+// Input
+// ```
+// 12
+// ```
+// Output
+// ```
+// -1
+// ```
+// Example 2
+// Input
+// ```
+// 17
+// ```
+// Output
+// ```
+// 4
+// ```
+
+const difference = (num) => {
+    const z=num-13;
+    return z;
+  }
+  console.log(difference(17))
