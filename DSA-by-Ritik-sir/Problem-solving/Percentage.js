549ee3ea3d5eca728e0f7064cf688ae0bb226861
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-09 21:36:24 +0530

    Percentage

---
+++ DSA-by-Ritik-sir/Problem-solving/Percentage.js
@@ -0,0 +1,25 @@
+// Q Take input a as a number and b as percentage. Write a program to calculate the percentage of a number.
+
+// Input 1
+// ```
+//  20 5
+// ```
+// Output 1:
+// ```
+// 1
+// ```
+// Explanation 1: 
+// ```
+// 5% of 20 is 1
+// ```
+
+
+function percent(num, percentage) {
+
+    let a=num;
+    let b=percentage;
+    let z=a/100*b
+    return z;
+    
+  }
+  console.log(percent(20,5))
