02367d83cd504729a136c996310087378c1bfb5c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-10 21:55:37 +0530

    Sum of area and perimeter

---
+++ DSA-by-Ritik-sir/Problem-solving/Sum-of-area-and-perimeter.js
@@ -0,0 +1,24 @@
+// Q Write a program to print the sum of area and perimeter of the circle.
+
+// Input 1
+
+// ```
+// 25
+// ```
+// Output 1:
+
+// ```
+// 2120.5750411731105
+// ```
+
+
+function sumAreaPerimeter(radius){
+
+    let sum=0;
+    let x= Math.PI*radius*radius;
+    let y=2*Math.PI*radius;
+    sum=x+y;
+    return(sum)
+  
+  }
+  console.log(sumAreaPerimeter(25))
