5c683aaec8def2f034d9d3cefe7268d2899eb8da
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-11 22:01:12 +0530

    Time Conversion

---
+++ DSA-by-Ritik-sir/Problem-solving/Time-conversion.js
@@ -0,0 +1,37 @@
+// Q Write a code to take time in seconds and convert it into hour, mins and seconds
+
+// Input 1
+// ```
+// 2000
+// ```
+// Output 1:
+// ```
+// 00:33:20
+// ```
+// Explanation 1: 
+
+// ```
+// 2000 /(60 * 60) = 0.555
+// Therefore Hour = 0
+
+// 2000 / 60 = 33.33
+// Therefore Minutes = 33
+
+// and 2000 % 60 = 20
+// Therefore seconds = 20
+
+// So, the answer become 
+// 00:30:20
+
+// ```
+
+function time(seconds){
+
+    let num=seconds;
+    let h=Math.floor(num/(60*60));
+    let m=Math.floor(num/60)-h*60;
+    let s=num%60
+    return (h+":"+m+":"+s)
+    
+  }
+  console.log(time(2000))
