e38caffd274cc871276c526de5fa85adf855d547
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-14 23:12:03 +0530

    Positive negative

---
+++ DSA-by-Ritik-sir/Problem-solving/Positive-negative.js
@@ -0,0 +1,27 @@
+// Q Positive and Negative
+// Given a number determine if it positive or negative.
+// Input: <br>
+// Input is provided as functional parameter.
+// Output: <br>
+// Return the answer.
+
+// Eg 1: 
+// 127
+// output: 
+// positive
+
+// Eg 2:
+// -127
+// output :
+// negative
+
+function is_positive(number){
+    
+  if(number>0){
+    return "positive";
+  }else{
+    return "negative";
+  }
+   
+  }
+  console.log(is_positive(-2))
