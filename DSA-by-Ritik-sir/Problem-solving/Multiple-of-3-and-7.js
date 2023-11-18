ee7f09e87fdfa0274871577c2be415d94bcb3da1
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-18 23:40:44 +0530

    Multiple of 3 and 5

---
+++ DSA-by-Ritik-sir/Problem-solving/Multiple-of-3-and-7.js
@@ -0,0 +1,22 @@
+// Q Multiple of 3 and 7
+// Check if the given number is a multiple of 3 and 7.
+
+// Eg 1: 
+// 10
+// output: 
+// no
+
+// Eg 2:
+// 21
+// output :
+// yes 
+
+function check(num1){
+  if(3*7==num1){
+    return "yes";
+  }else{
+    return "no";
+  }
+    
+  }
+  console.log(check(21))
