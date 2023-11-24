3584ab0a5ba6b24cbb54ab3bfbfd7df5244467f8
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-24 22:24:36 +0530

    Find element

---
+++ DSA-by-Ritik-sir/Problem-solving/Find-element.js
@@ -0,0 +1,17 @@
+// Q Create an Array with the following elements: `“Bike”, “Scooter”, “Car”, “Truck”, “JCB” , “Helicopter”` 
+// and return the 4th element from the Array.
+
+// ### Output Format
+// 4th Element from the Array
+
+// ### Sample Output :
+// `
+// Truck
+// `
+
+function createArray(names){
+  
+    return names[3]
+    
+  }
+  console.log(createArray(['Bike', 'Scooter', 'Car', 'Truck', 'JCB' ,'Helicopter']))
