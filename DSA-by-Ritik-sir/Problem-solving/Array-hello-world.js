00a710209304f9cccdcbe6a38556496e758bed1c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-25 22:30:44 +0530

    Array-hello-world

---
+++ DSA-by-Ritik-sir/Problem-solving/Array-hello-world.js
@@ -0,0 +1,23 @@
+// Q You are given an array arr, if the length of the array is greater than 5 return “Hello” else return “World”
+
+// Input:
+// ```
+// [1, 2, 3 ,4 ,5 ,6 7]
+// ```
+
+// Output :
+
+// ```
+// Hello
+// ```
+
+function arrayHelloWorld(arr){
+ 
+    if([arr.length]>5){
+      return "Hello"
+    }else{
+      return "World"
+    }
+  }
+  console.log(arrayHelloWorld([1, 2, 3 ,4 ,5 ,6,7]))
+  
