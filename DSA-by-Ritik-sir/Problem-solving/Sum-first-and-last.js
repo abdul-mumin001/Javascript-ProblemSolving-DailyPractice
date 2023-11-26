7c26c7d4a0c6abc45c4e08170030fe937a279986
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-26 22:51:53 +0530

    Sum first and last

---
+++ DSA-by-Ritik-sir/Problem-solving/Sum-first-and-last.js
@@ -0,0 +1,20 @@
+// Q You are given an integer array as function parameters. Find and return the sum of the first and last 
+// element of the array.
+
+// Input:
+// ```
+// [1, 2, 3 ,4 ,5 ,6 7]
+// ```
+// Output :
+
+// ```
+// 8
+// ```
+
+function sumFirstLast(arr){
+ 
+    let x=(arr[0]+arr[arr.length-1])
+    
+    return x;
+  }
+  console.log(sumFirstLast([1, 2, 3 ,4 ,5 ,6 ,7]))
