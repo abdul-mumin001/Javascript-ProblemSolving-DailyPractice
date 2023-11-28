b4b8858893b4eb045adbf5ecd3afe148d27c4d7d
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-28 21:55:57 +0530

    Aunt may want the candies

--- DSA-by-Ritik-sir/Problem-solving/Aunt-may-want-the-candies.js
+++ DSA-by-Ritik-sir/Problem-solving/Aunt-may-want-the-candies.js
@@ -1,4 +1,4 @@
-// Q Aunt May wants to distribute candies to 5 childrens, you are given an array of 5 elements, each element 
+// Q Aunt May wants to distribute candies to 5 childrens, you are given an array of 5 elements, each element
 // depicts how many candies each child will get. Find and return how many candies Aunt May have in total.
 
 // Input:
@@ -11,11 +11,11 @@
 // 20
 // ```
 
-function totalCandies(arr){
-    let sum=0;
-   for(let i=0;i<arr.length;i++){
-     sum+=arr[i];
-   }
-    return sum;
+function totalCandies(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
-  console.log(totalCandies([1, 3, 4, 7, 5]))
+  return sum;
+}
+console.log(totalCandies([1, 3, 4, 7, 5]));
