164de173228244d9b24fe97bbddf04e34b687871
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-11-29 22:34:07 +0530

    Array Join

---
+++ DSA-by-Ritik-sir/Problem-solving/Array-join.js
@@ -0,0 +1,20 @@
+// Q You are given two arrays arr1 and arr2, concatenate arr1  and arr2, and return the resulting array.
+
+// Input:
+// ```
+// [1, 3]
+// [2, 5]
+// ```
+// Output :
+
+// ```
+// [1, 3, 2, 5]
+// ```
+
+function arrayConcat(arr1, arr2){
+  
+    let x=(arr1.concat(arr2))
+   
+    return x
+  }
+  console.log(arrayConcat([1, 3],[2, 5]))
