ea514312fc6b9ecd7e22eabef85bc8350de734e7
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-03 21:42:55 +0530

    element + Index

--- DSA-by-Ritik-sir/Problem-solving/elementIndex.js
+++ DSA-by-Ritik-sir/Problem-solving/elementIndex.js
@@ -1,5 +1,5 @@
-// Q You are given two variables  arr and element as function parameters. Check if the given variable arr is an 
-// array or not, if it is an array return the index of element in the array, else return “Not Array”. 
+// Q You are given two variables  arr and element as function parameters. Check if the given variable arr is an
+// array or not, if it is an array return the index of element in the array, else return “Not Array”.
 
 // Input:
 // ```
@@ -18,21 +18,21 @@
 // Not Array
 // ```
 
-function elementIndex(arr, element){
-    if(Array.isArray(arr)===true){
-      var flag2 = true;
-      for(var i =0; i<arr.length; i++){
-        if(arr[i]===element){
-         flag2 = false;
-          return i;
-        }
-      }  
-      if(flag2 === true){
-        return -1;
+function elementIndex(arr, element) {
+  if (Array.isArray(arr) === true) {
+    var flag2 = true;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === element) {
+        flag2 = false;
+        return i;
       }
-    } else {
-      return "Not Array";
     }
+    if (flag2 === true) {
+      return -1;
+    }
+  } else {
+    return "Not Array";
   }
-  console.log(elementIndex([1,2,3],4))
-  console.log(elementIndex([1,2,3],3))
+}
+console.log(elementIndex([1, 2, 3], 4));
+console.log(elementIndex([1, 2, 3], 3));
