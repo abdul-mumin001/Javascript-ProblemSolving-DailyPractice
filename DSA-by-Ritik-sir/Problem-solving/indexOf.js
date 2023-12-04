65873867a5b8870a8793ba23dacd5c2ba10d160c
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-04 22:17:12 +0530

    indexOf

---
+++ DSA-by-Ritik-sir/Problem-solving/indexOf.js
@@ -0,0 +1,46 @@
+// Q Given an integer array and an element as function parameter, return the index of the given element if it 
+// exists, else return “Not Found”. If the array contains multiple occurence of an element return the index of 
+// last occurrence of the element.
+
+// Input:
+// ```
+// [1, 2, 3,] 2
+// ```
+// Output:
+// ```
+// 1
+// ```
+// Input:
+// ```
+// [2,5,10] 9
+// ```
+// Output:
+// ```
+// Not Found
+// ```
+// Input:
+// ```
+// [2, 5, 10, 7, 5, 19, 10] 5
+// ```
+// Output:
+// ```
+// 4
+// ```
+
+function indexOf(arr, element){
+
+    if(Array.isArray(arr)===true){
+      flag=false;
+      for(var i=0;i<arr.length;i++){
+        if(arr[i]===element){
+          flag=true;
+        return i
+        }else {
+      return 'Not Found'
+    }
+        
+      }
+    }
+    
+  }
+  console.log(indexOf([1, 2, 3,], 2))
