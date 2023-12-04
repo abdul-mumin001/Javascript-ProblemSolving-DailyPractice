fa3ff1630d5ace7a3441c2b8b5df5ea110040cce
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-04 22:18:03 +0530

    indexOf

--- DSA-by-Ritik-sir/Problem-solving/indexOf.js
+++ DSA-by-Ritik-sir/Problem-solving/indexOf.js
@@ -1,5 +1,5 @@
-// Q Given an integer array and an element as function parameter, return the index of the given element if it 
-// exists, else return “Not Found”. If the array contains multiple occurence of an element return the index of 
+// Q Given an integer array and an element as function parameter, return the index of the given element if it
+// exists, else return “Not Found”. If the array contains multiple occurence of an element return the index of
 // last occurrence of the element.
 
 // Input:
@@ -27,21 +27,18 @@
 // 4
 // ```
 
-function indexOf(arr, element){
-
-    if(Array.isArray(arr)===true){
-      flag=false;
-      for(var i=0;i<arr.length;i++){
-        if(arr[i]===element){
-          flag=true;
-        return i
-        }else {
-      return 'Not Found'
-    }
-        
+function indexOf(arr, element) {
+  if (Array.isArray(arr) === true) {
+    flag = false;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === element) {
+        flag = true;
+        return i;
+      } else {
+        return "Not Found";
       }
     }
-    
   }
-  console.log(indexOf([1, 2, 3,], 2))
-  console.log(indexOf([1, 2], 1))
+}
+console.log(indexOf([1, 2, 3], 2));
+console.log(indexOf([1, 2], 1));
