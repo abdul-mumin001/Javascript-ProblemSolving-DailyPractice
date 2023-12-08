85803597a85903d2005d97a6ccbff85796f42ce1
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-08 22:15:08 +0530

    Array sort

---
+++ Array-sort.js
@@ -0,0 +1,26 @@
+// Q Given an array of numbers, sort the array in ascending order and then return the sorted array. 
+
+// Input:
+// ```
+// ['c', 'a', 'd']
+// ```
+
+// Output:
+// ```
+// ['a', 'b', 'c']
+// ```
+
+// Input:
+// ```
+// [2, 5, 7, 5]
+// ```
+
+// Output:
+// ```
+// [2, 5, 5, 7]
+// ```
+
+function sort(arr){
+    return [...arr.sort()]
+  }
+  console.log(sort(['c', 'a', 'd']))
