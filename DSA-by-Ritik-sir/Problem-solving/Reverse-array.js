01d97e4d3567734b2dd807dee04dba5580959787
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-11 21:45:24 +0530

    Reverse array

---
+++ DSA-by-Ritik-sir/Problem-solving/Reverse-array.js
@@ -0,0 +1,27 @@
+// Q Given an array, return the array in reverse order.
+
+// Input:
+// ```
+// [1, 2, 3]
+// ```
+
+// Output:
+// ```
+// [3, 2, 1]
+// ```
+
+// Input:
+// ```
+// [2, 5, 10, 7, 5]
+// ```
+
+// Output:
+// ```
+// [5, 7, 10, 5, 2]
+// ```
+
+function reverseArray(arr){
+    arr.reverse()
+      return arr;
+    }
+    console.log(reverseArray([1, 2, 3]))
