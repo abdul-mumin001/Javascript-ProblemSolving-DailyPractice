a99a974178b632e3feeefebdea18a2f2a4257b3a
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-12 23:10:08 +0530

    Change to zero

--- DSA-by-Ritik-sir/Problem-solving/Change-to-zero.js
+++ DSA-by-Ritik-sir/Problem-solving/Change-to-zero.js
@@ -1,9 +1,9 @@
-// Q Given an array, a start index and an end index, change all the elements to 0 from the start index 
+// Q Given an array, a start index and an end index, change all the elements to 0 from the start index
 // to end index.
 
 // Input:
 // ```
-// [1, 2, 3] 
+// [1, 2, 3]
 // 1 2
 // ```
 // Output:
@@ -12,7 +12,7 @@
 // ```
 // Input:
 // ```
-// [2, 5, 10, 7, 5] 
+// [2, 5, 10, 7, 5]
 // 2 3
 // ```
 // Output:
@@ -20,14 +20,12 @@
 // [2, 5, 0, 7, 5]
 // ```
 
-function changeToZero(arr, start, end){
-  
-    for(let i=start; i<end; i++){
-     arr.splice(start,1,0)
-      
-    }
-    return arr
+function changeToZero(arr, start, end) {
+  for (let i = start; i < end; i++) {
+    arr.splice(start, 1, 0);
   }
-  console.log(changeToZero([1,3,4,5,7,8,9] , 2 , 5))
-  console.log(changeToZero([1, 2, 3],1, 2))
-  console.log(changeToZero([2, 5, 10, 7, 5],2, 3))
+  return arr;
+}
+console.log(changeToZero([1, 3, 4, 5, 7, 8, 9], 2, 5));
+console.log(changeToZero([1, 2, 3], 1, 2));
+console.log(changeToZero([2, 5, 10, 7, 5], 2, 3));
