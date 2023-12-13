4846206ee3d1391cfc4bad025c33aab3486f0fac
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-13 22:01:03 +0530

    Array to string

---
+++ DSA-by-Ritik-sir/Problem-solving/Array-to-string.js
@@ -0,0 +1,25 @@
+// Q Given an array, convert the given array to string.
+
+// Input:
+// ```
+// [1, 2, 3]
+// ```
+// Output:
+// ```
+// '1,2,3'
+// ```
+// Input:
+// ```
+// [2, 5, 10, 7, 5]
+// ```
+// Output:
+// ```
+// '2,5,10,7,5'
+// ```
+
+function arrString(arr){
+    let convertToString=arr.toString()
+    return convertToString;
+  }
+  console.log(arrString([1, 2, 3]))
+  
