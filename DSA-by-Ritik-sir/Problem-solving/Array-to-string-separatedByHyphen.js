242b9f3fc6e933e0c5cdfe24c4b870746210bb19
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-14 21:25:14 +0530

    array to string separated by hyphens

---
+++ DSA-by-Ritik-sir/Problem-solving/Array-to-string-separatedByHyphen.js
@@ -0,0 +1,25 @@
+// Q Given an array of characters, convert the given array to string separated by '-' hyphens.
+
+// Input:
+// ```
+// [1, 2, 3]
+// ```
+// Output:
+// ```
+// '1-2-3'
+// ```
+// Input:
+// ```
+// [2, 5, 10, 7, 5]
+// ```
+// Output:
+// ```
+// '2-5-10-7-5'
+// ```
+
+function arrString(arr){
+    let stringWithHyphen=arr.map(element=>element).join('-');
+    return stringWithHyphen
+  }
+  console.log(arrString([1, 2, 3]))
+  
