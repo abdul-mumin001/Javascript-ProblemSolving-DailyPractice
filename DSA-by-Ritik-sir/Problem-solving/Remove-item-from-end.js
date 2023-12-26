ce921fd9bf1081bee5cf70c51a5b830ce3a03bdd
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-26 23:48:45 +0530

    Remove-item-from-end

--- DSA-by-Ritik-sir/Problem-solving/Remove-item-from-end.js
+++ DSA-by-Ritik-sir/Problem-solving/Remove-item-from-end.js
@@ -2,7 +2,7 @@
 
 // Input:
 // ```
-// [1, 2, 3] 
+// [1, 2, 3]
 // ```
 // Output:
 // ```
@@ -10,15 +10,15 @@
 // ```
 // Input:
 // ```
-// [2, 5, 10, 7, 5] 
+// [2, 5, 10, 7, 5]
 // ```
 // Output:
 // ```
 // [2, 5, 10, 7]
 // ```
 
-function removeEnd(arr){
-    return arr.splice(0,arr.length-1)
-  }
-  console.log(removeEnd([1, 2, 3]))
-  console.log(removeEnd([2, 5, 10, 7, 5]))
+function removeEnd(arr) {
+  return arr.splice(0, arr.length - 1);
+}
+console.log(removeEnd([1, 2, 3]));
+console.log(removeEnd([2, 5, 10, 7, 5]));
