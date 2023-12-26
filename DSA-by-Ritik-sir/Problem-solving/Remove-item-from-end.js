87853560cc31060103420b824041e54e52961eea
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-26 23:43:30 +0530

    Remove-item-from-end

---
+++ DSA-by-Ritik-sir/Problem-solving/Remove-item-from-end.js
@@ -0,0 +1,23 @@
+// Q Given an array, delete the last element of the array
+
+// Input:
+// ```
+// [1, 2, 3] 
+// ```
+// Output:
+// ```
+// [1, 2]
+// ```
+// Input:
+// ```
+// [2, 5, 10, 7, 5] 
+// ```
+// Output:
+// ```
+// [2, 5, 10, 7]
+// ```
+
+function removeEnd(arr){
+    return arr.splice(0,arr.length-1)
+  }
+  console.log(removeEnd([1, 2, 3]))
