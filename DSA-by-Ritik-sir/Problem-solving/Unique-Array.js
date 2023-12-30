1bfedfbabfedc325ff1cc84f8dccb072c4f33832
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-30 23:28:51 +0530

    Unique Array

---
+++ DSA-by-Ritik-sir/Problem-solving/Unique-Array.js
@@ -0,0 +1,24 @@
+// Q You are given a sorted array in ascending order, remove the duplicate elements and return the new 
+// sorted array.
+
+// Input:
+// ```
+// [1, 2, 2, 3, 4, 4, 5]
+// ```
+// Output:
+// ```
+// [1, 2, 3, 4, 5]
+// ```
+// Input:
+// ```
+// [2, 5, 5, 7, 10, 12, 12, 15]
+// ```
+// Output:
+// ```
+// [2, 5, 7, 10, 12, 15]
+// ```
+
+function deleteDuplicate(arr){
+    return [...new Set(arr)]
+    }
+    console.log(deleteDuplicate([1, 2, 2, 3, 4, 4, 5]))
