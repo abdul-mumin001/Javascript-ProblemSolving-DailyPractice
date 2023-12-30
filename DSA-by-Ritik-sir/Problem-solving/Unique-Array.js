d2c7d3398f2e54a20d4b9a9fc5a1cadb584afdfc
abdul-mumin001 <abdulmuminbar@gmail.com>
2023-12-30 23:29:48 +0530

    Unique Array

--- DSA-by-Ritik-sir/Problem-solving/Unique-Array.js
+++ DSA-by-Ritik-sir/Problem-solving/Unique-Array.js
@@ -22,3 +22,4 @@ function deleteDuplicate(arr){
     return [...new Set(arr)]
     }
     console.log(deleteDuplicate([1, 2, 2, 3, 4, 4, 5]))
+    console.log(deleteDuplicate([2, 5, 5, 7, 10, 12, 12, 15]))
