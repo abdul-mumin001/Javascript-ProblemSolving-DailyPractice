ccbf86e334ab3bd38aa2d91ec864e315427dbc69
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-01 23:18:05 +0530

    Good manager

--- DSA-by-Ritik-sir/Problem-solving/Good-manager.js
+++ DSA-by-Ritik-sir/Problem-solving/Good-manager.js
@@ -1,5 +1,5 @@
 // Q  A manager wants to increment each of its employees' salaries by Rs.1000. You are given an array with the
-//  current salary of each employee. Return the array containing the incremented salary. Use array callback 
+//  current salary of each employee. Return the array containing the incremented salary. Use array callback
 //  methods.
 
 // Input:
@@ -19,11 +19,11 @@
 // [21000, 41000, 501000, 21000, 11000]
 // ```
 
-function goodManager(arr){
-    let newArray=arr.map(function(element){
-      return element+1000
-    })
-    return newArray;
-  }
-  console.log(goodManager([1000,2000, 3000]))
-  console.log(goodManager([20000, 40000, 500000, 20000, 10000]))
+function goodManager(arr) {
+  let newArray = arr.map(function (element) {
+    return element + 1000;
+  });
+  return newArray;
+}
+console.log(goodManager([1000, 2000, 3000]));
+console.log(goodManager([20000, 40000, 500000, 20000, 10000]));
