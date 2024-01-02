a45ea0139f811d402721ecf1b0296ce4a9913f43
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-02 22:19:03 +0530

    squareNumbers

--- DSA-by-Ritik-sir/Problem-solving/squareNumbers.js
+++ DSA-by-Ritik-sir/Problem-solving/squareNumbers.js
@@ -1,6 +1,6 @@
-// Q Mickey Mouse gave a task to Goofy. The task is that Mickey will give Goofy an array of numbers and 
-// Goofy will find the square of all the numbers.. Help Goofy to find the square of given numbers. Use 
-// callback methods. 
+// Q Mickey Mouse gave a task to Goofy. The task is that Mickey will give Goofy an array of numbers and
+// Goofy will find the square of all the numbers.. Help Goofy to find the square of given numbers. Use
+// callback methods.
 
 // Input:
 // ```
@@ -19,11 +19,11 @@
 // [100, 49, 225, 9801]
 // ```
 
-function square(arr){
-    let newArray=arr.map(function(element){
-      return element**2
-    })
-    return newArray
-  }
-  console.log(square([2, 3, 6])) 
-  console.log(square([10, 7, 15, 99])) 
+function square(arr) {
+  let newArray = arr.map(function (element) {
+    return element ** 2;
+  });
+  return newArray;
+}
+console.log(square([2, 3, 6]));
+console.log(square([10, 7, 15, 99]));
