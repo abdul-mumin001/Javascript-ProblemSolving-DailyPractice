90250e48378b76a22a96f10e2cfe6c4cea51dfad
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-03 23:19:41 +0530

    Amit need help

--- DSA-by-Ritik-sir/Problem-solving/Amit-need-help.js
+++ DSA-by-Ritik-sir/Problem-solving/Amit-need-help.js
@@ -1,5 +1,5 @@
 // Q Amit goes to a restaurant, but after seeing the huge menu. He was confused about what to order. Amit
-//  wanted to have something which has Paneer and does not have Rice. Help Amit choose a food item. 
+//  wanted to have something which has Paneer and does not have Rice. Help Amit choose a food item.
 
 // Input:
 // ```
@@ -18,12 +18,26 @@
 // Paneer Paratha
 // ```
 
-function order(menu){
-    for(let i=0;i<menu.length;i++){
-      if(menu[i].includes('Paneer') && !menu[i].includes('Rice')){
-        return menu[i]
-      }
+function order(menu) {
+  for (let i = 0; i < menu.length; i++) {
+    if (menu[i].includes("Paneer") && !menu[i].includes("Rice")) {
+      return menu[i];
     }
   }
-  console.log(order(['Paneer Butter Masala', 'Paneer Fried Rice', 'Chicken Biryani', 'Cold Coffee']))
-  console.log(order(['Panner Fried Rice', 'Chicken Biryani', 'Paneer Paratha', 'Cold Coffee'])) 
+}
+console.log(
+  order([
+    "Paneer Butter Masala",
+    "Paneer Fried Rice",
+    "Chicken Biryani",
+    "Cold Coffee",
+  ])
+);
+console.log(
+  order([
+    "Panner Fried Rice",
+    "Chicken Biryani",
+    "Paneer Paratha",
+    "Cold Coffee",
+  ])
+);
