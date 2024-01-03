7b552fbdcff4b0bd94f0c32bc90e15c8c22f617a
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-03 23:17:23 +0530

    Amit need help

---
+++ DSA-by-Ritik-sir/Problem-solving/Amit-need-help.js
@@ -0,0 +1,28 @@
+// Q Amit goes to a restaurant, but after seeing the huge menu. He was confused about what to order. Amit
+//  wanted to have something which has Paneer and does not have Rice. Help Amit choose a food item. 
+
+// Input:
+// ```
+// ['Panner Butter Masala', 'Panner Fried Rice', 'Chicken Biryani', 'Cold Coffee']
+// ```
+// Output:
+// ```
+// Paneer Butter Masala
+// ```
+// Input:
+// ```
+// ['Panner Fried Rice', 'Chicken Biryani', 'Paneer Paratha', Cold Coffee']
+// ```
+// Output:
+// ```
+// Paneer Paratha
+// ```
+
+function order(menu){
+    for(let i=0;i<menu.length;i++){
+      if(menu[i].includes('Paneer') && !menu[i].includes('Rice')){
+        return menu[i]
+      }
+    }
+  }
+  console.log(order(['Paneer Butter Masala', 'Paneer Fried Rice', 'Chicken Biryani', 'Cold Coffee']))
