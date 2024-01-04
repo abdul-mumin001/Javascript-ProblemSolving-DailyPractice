d29a249436f96535107d9ecea1435e23e79a2bba
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-04 22:16:50 +0530

    Square root multiplication

--- DSA-by-Ritik-sir/Problem-solving/Square-root-multiplication.js
+++ DSA-by-Ritik-sir/Problem-solving/Square-root-multiplication.js
@@ -1,4 +1,4 @@
-// Q Given an array of numbers, create a new array by multiplying each number with its square root from 
+// Q Given an array of numbers, create a new array by multiplying each number with its square root from
 // the given array. Return original array concatenated with the new array. Use map callback method.
 
 // Input:
@@ -18,12 +18,12 @@
 // [11.180339887498949, 27, 343, 252.98221281347037, 58.09475019311125]
 // ```
 
-function multiply(arr){
-    let newArray=[];
-    for(let i=0;i<arr.length;i++){
-      newArray[i]=Math.sqrt(arr[i])*arr[i]
-    }
-    return newArray;
+function multiply(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray[i] = Math.sqrt(arr[i]) * arr[i];
   }
-  console.log(multiply([4, 9, 25, 49]))
-  console.log(multiply([5, 9, 49, 40, 15]))
+  return newArray;
+}
+console.log(multiply([4, 9, 25, 49]));
+console.log(multiply([5, 9, 49, 40, 15]));
