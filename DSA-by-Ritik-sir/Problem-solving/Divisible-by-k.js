d00f0d8cddeb48f64d867e070437bcd43c2ace10
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-07 20:58:06 +0530

    Divisible by k

--- DSA-by-Ritik-sir/Problem-solving/Divisible-by-k.js
+++ DSA-by-Ritik-sir/Problem-solving/Divisible-by-k.js
@@ -1,4 +1,4 @@
-// Q Given an array of integers and a number ‘k’, increments the numbers in the array such that all the 
+// Q Given an array of integers and a number ‘k’, increments the numbers in the array such that all the
 // numbers in the array are divisible by ‘k’
 
 // Input:
@@ -20,20 +20,17 @@
 // [12, 12, 12]
 // ```
 
-function divisibleByK(arr, k){
-    let arr1=[]
-      for(let i=0;i<arr.length;i++){
-        if(arr[i]%k !=0){
-          while(arr[i]%k !=0){
-            arr[i]=arr[i]+1
-          }
-        }
-        if(arr[i]%k==0)
-          arr1.push(arr[i])
+function divisibleByK(arr, k) {
+  let arr1 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % k != 0) {
+      while (arr[i] % k != 0) {
+        arr[i] = arr[i] + 1;
       }
-      return arr1
     }
-    console.log(divisibleByK([1, 5, 7, 8],
-    3))
-    console.log(divisibleByK([7, 2, 5],
-        12))
+    if (arr[i] % k == 0) arr1.push(arr[i]);
+  }
+  return arr1;
+}
+console.log(divisibleByK([1, 5, 7, 8], 3));
+console.log(divisibleByK([7, 2, 5], 12));
