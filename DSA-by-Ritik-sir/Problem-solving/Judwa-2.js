dd3734ff74cc7bc0edb7a6afe69a878ff3c09677
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-08 21:48:47 +0530

    Judwa-2

---
+++ DSA-by-Ritik-sir/Problem-solving/Judwa-2.js
@@ -0,0 +1,36 @@
+// Q Given an array of integers, you are required to create Judwaa of each occurrence of 2 i.e duplicates
+//  of 2 and shift the elements to right as shown in the example.
+
+// Input:
+// ```
+// [1, 2, 5 , 2, 7, 9, 1, 3]
+// ```
+// Output:
+// ```
+// [1, 2, 2, 5, 2, 2, 7, 9]
+// ```
+// Input:
+// ```
+// [1, 3, 5, 9, 10]
+// ```
+// Output:
+// ```
+// [1, 3, 5, 9, 10]
+// ```
+
+function judwaa(arr){
+    var result =[]
+    var n=arr.length;
+    var totalL=n;
+    for(var i=0;i<totalL;i++){
+      if(arr[i]==2){
+        result.push(arr[i]);
+        result.push(arr[i]);
+        totalL--;
+      }else{
+        result.push(arr[i])
+      }
+    }
+    return(result)
+  }
+  console.log(judwaa([1, 2, 5 , 2, 7, 9, 1, 3]))
