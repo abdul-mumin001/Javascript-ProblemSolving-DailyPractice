06e1d0b267cc888216ee5a52965c2685c5d873aa
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-10 21:06:51 +0530

    Debugging-1

--- DSA-by-Ritik-sir/Problem-solving/Debugging-1.js
+++ DSA-by-Ritik-sir/Problem-solving/Debugging-1.js
@@ -1,7 +1,7 @@
-// Q Amit was trying to solve the Unique Array problem, but for some reason his code is not working. 
+// Q Amit was trying to solve the Unique Array problem, but for some reason his code is not working.
 // Can you help him ? Fix the given code. to produce correct output.
 
-// You are given a sorted array in ascending order, remove the duplicate elements and return the new 
+// You are given a sorted array in ascending order, remove the duplicate elements and return the new
 // sorted array.
 
 // Input:
@@ -21,13 +21,13 @@
 // [2, 5, 7, 10, 12, 15]
 // ```
 
-function deleteDuplicate(arr){
-    for(let i = 0; i <= arr.length; ++i){
-      if(arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])){
-        arr.slice(arr.lastIndexOf(arr[i]), 0)
-       }
+function deleteDuplicate(arr) {
+  for (let i = 0; i <= arr.length; ++i) {
+    if (arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])) {
+      arr.slice(arr.lastIndexOf(arr[i]), 0);
     }
-    return [...new Set(arr)]
   }
-  console.log(deleteDuplicate([1, 2, 2, 3, 4, 4, 5]))
-  console.log(deleteDuplicate([2, 5, 5, 7, 10, 12, 12, 15]))
+  return [...new Set(arr)];
+}
+console.log(deleteDuplicate([1, 2, 2, 3, 4, 4, 5]));
+console.log(deleteDuplicate([2, 5, 5, 7, 10, 12, 12, 15]));
