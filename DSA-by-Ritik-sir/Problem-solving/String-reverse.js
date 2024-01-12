fda5d9fcbe866c1f6d8630b54b70277874c3e8bc
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-12 23:01:11 +0530

    String reverse

---
+++ DSA-by-Ritik-sir/Problem-solving/String-reverse.js
@@ -0,0 +1,25 @@
+// Q Given a string, return the string in reverse
+
+// Input:
+// ```
+// aircampus
+// ```
+// Output:
+// ```
+// supmacria
+// ```
+// Input:
+// ```
+// javascript
+// ```
+// Output:
+// ```
+// tpircsavaj
+// ```
+
+function reverse(str){
+  
+  
+    return str.split("").reverse().join("")
+  }
+  console.log(reverse('aircampus'))
