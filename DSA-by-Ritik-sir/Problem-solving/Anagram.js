b5bbfc810d28f7c638662f62ace901cd0ed6b7b5
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-19 23:19:27 +0530

    anagram

--- DSA-by-Ritik-sir/Problem-solving/Anagram.js
+++ DSA-by-Ritik-sir/Problem-solving/Anagram.js
@@ -19,14 +19,14 @@
 // false
 // ```
 
-function anagram(str1, str2){
-    let x=str1.split("").sort().join("");
-    let y=str2.split("").sort().join("");
-    if(x==y){
-      return true;
-    }else{
-      return false;
-    }
+function anagram(str1, str2) {
+  let x = str1.split("").sort().join("");
+  let y = str2.split("").sort().join("");
+  if (x == y) {
+    return true;
+  } else {
+    return false;
   }
-  console.log(anagram('elbow', 'below'))
-  console.log(anagram('night', 'thin'))
+}
+console.log(anagram("elbow", "below"));
+console.log(anagram("night", "thin"));
