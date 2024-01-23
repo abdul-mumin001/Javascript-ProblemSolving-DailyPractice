40f12961ed272cd3d2dc56d4c1a4b2f61d02b8f2
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-23 22:46:09 +0530

    Alphanumeric

---
+++ DSA-by-Ritik-sir/Problem-solving/Alphanumeric.js
@@ -0,0 +1,32 @@
+// Q Given a string, if the string is alphanumeric return true else return false.
+
+// Input:
+// ```
+// aircampus
+// ```
+// Output:
+// ```
+// false
+// ```
+// Input:
+// ```
+// javascr1pt
+// ```
+// Output:
+// ```
+// true
+// ```
+
+function alphanumeric(str){
+  
+    for(let i=0;i<str.length;i++){
+      let code=str.split("").sort().join()
+      if((code.charCodeAt(i)>47 && code.charCodeAt(i)<58)){
+        return true
+      }else{
+        return false
+      }
+    }
+  
+  }
+  console.log(alphanumeric('DownTown'))
