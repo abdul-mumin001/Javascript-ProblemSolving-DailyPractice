cac6649814044919fc196b8a9620458ecc958644
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-23 22:47:35 +0530

    Alphanumeric

--- DSA-by-Ritik-sir/Problem-solving/Alphanumeric.js
+++ DSA-by-Ritik-sir/Problem-solving/Alphanumeric.js
@@ -17,17 +17,15 @@
 // true
 // ```
 
-function alphanumeric(str){
-  
-    for(let i=0;i<str.length;i++){
-      let code=str.split("").sort().join()
-      if((code.charCodeAt(i)>47 && code.charCodeAt(i)<58)){
-        return true
-      }else{
-        return false
-      }
+function alphanumeric(str) {
+  for (let i = 0; i < str.length; i++) {
+    let code = str.split("").sort().join();
+    if (code.charCodeAt(i) > 47 && code.charCodeAt(i) < 58) {
+      return true;
+    } else {
+      return false;
     }
-  
   }
-  console.log(alphanumeric('DownTown'))
-  console.log(alphanumeric('javascr1pt'))
+}
+console.log(alphanumeric("DownTown"));
+console.log(alphanumeric("javascr1pt"));
