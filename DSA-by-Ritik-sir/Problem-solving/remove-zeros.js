c228c649725963b060d39afd5278bacd14411201
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-24 23:20:59 +0530

    remove zeros

---
+++ DSA-by-Ritik-sir/Problem-solving/remove-zeros.js
@@ -0,0 +1,25 @@
+// Q Given a string remove leading and trailing zeros from it and return the string
+
+// Input:
+// ```
+// 230.00
+// ```
+// Output:
+// ```
+// 230
+// ```
+// Input:
+// ```
+// 00420
+// ```
+// Output:
+// ```
+// 420
+// ```
+
+function removeZeroes(str){
+    let value=(str*1).toString();
+    return value;
+  }
+  console.log(removeZeroes(230.00))
+  
