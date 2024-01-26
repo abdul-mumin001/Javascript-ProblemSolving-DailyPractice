6b5cfdca41f95144c263a7674053818b2adeae6a
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-26 21:55:22 +0530

    String reverse

---
+++ DSA-by-Ritik-sir/Problem-solving/String-reverse-I.js
@@ -0,0 +1,24 @@
+// Q Given a string sentence, reverse the string word by word
+
+// Input:
+// ```
+// the sky is blue
+// ```
+// Output:
+// ```
+// blue is sky the
+// ```
+// Input:
+// ```
+// hello   world!
+// ```
+// Output:
+// ```
+// world! hello
+// ```
+
+function reverse(str){
+    let x=str.split(" ").reverse().join(" ");
+    return x;
+  }
+  console.log(reverse('the sky is blue'))
