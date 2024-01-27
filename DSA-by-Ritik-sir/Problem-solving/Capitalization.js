bb1b9272b12e0a0abca4de268922c1b30b9165f0
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-27 22:20:53 +0530

    Capitalization

--- DSA-by-Ritik-sir/Problem-solving/Capitalization.js
+++ DSA-by-Ritik-sir/Problem-solving/Capitalization.js
@@ -17,15 +17,16 @@
 // Capitalize Every Word
 // ```
 
-function capitalization(str){
-    let x=str.split(" ");
-    let capitalizeTheFirstLetter=[]
-    x.forEach(letter=>{
-      capitalizeTheFirstLetter.push(letter[0].toUpperCase()+letter.slice(1,letter.length))
-    })
-    
-      return capitalizeTheFirstLetter.join(" ")
-    
-  }
-  console.log(capitalization('This is a title'))
-  console.log(capitalization('capitalize every word'))
+function capitalization(str) {
+  let x = str.split(" ");
+  let capitalizeTheFirstLetter = [];
+  x.forEach((letter) => {
+    capitalizeTheFirstLetter.push(
+      letter[0].toUpperCase() + letter.slice(1, letter.length)
+    );
+  });
+
+  return capitalizeTheFirstLetter.join(" ");
+}
+console.log(capitalization("This is a title"));
+console.log(capitalization("capitalize every word"));
