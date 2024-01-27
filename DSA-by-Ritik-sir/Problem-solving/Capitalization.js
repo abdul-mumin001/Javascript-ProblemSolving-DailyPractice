6029a37ccc5182387aa9dfa8432dda3b648d8329
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-01-27 22:19:03 +0530

    Capitalization

---
+++ DSA-by-Ritik-sir/Problem-solving/Capitalization.js
@@ -0,0 +1,31 @@
+// Q Given a string, capitalize the first letter of each word.
+
+// Input:
+// ```
+// This is a title
+// ```
+// Output:
+// ```
+// This Is A Title
+// ```
+// Input:
+// ```
+// capitalize every word
+// ```
+// Output:
+// ```
+// Capitalize Every Word
+// ```
+
+function capitalization(str){
+    let x=str.split(" ");
+    let capitalizeTheFirstLetter=[]
+    x.forEach(letter=>{
+      capitalizeTheFirstLetter.push(letter[0].toUpperCase()+letter.slice(1,letter.length))
+    })
+    
+      return capitalizeTheFirstLetter.join(" ")
+    
+  }
+  console.log(capitalization('This is a title'))
+  
