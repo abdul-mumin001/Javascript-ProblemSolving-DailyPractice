695906c9f812e8aed2bb55fb2304f22733750977
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-02-07 21:00:35 +0530

    Midddle character

--- DSA-by-Ritik-sir/Problem-solving/Middle-character.js
+++ DSA-by-Ritik-sir/Problem-solving/Middle-character.js
@@ -1,4 +1,4 @@
-// Q. Given a string, return the middle character, if the string length is odd, else return the 
+// Q. Given a string, return the middle character, if the string length is odd, else return the
 // middle two characters.
 
 // Input:
@@ -16,15 +16,15 @@
 // Output:
 // ```
 // t
-// ``` 
+// ```
 
-function middleChar(str){
-    let middleCharacter=Math.floor(str.length/2);
-    let ans=str[middleCharacter]
-    if(str.length %2===0){
-      ans=str[middleCharacter-1]+ans
-    }
-    return ans
+function middleChar(str) {
+  let middleCharacter = Math.floor(str.length / 2);
+  let ans = str[middleCharacter];
+  if (str.length % 2 === 0) {
+    ans = str[middleCharacter - 1] + ans;
   }
-  console.log(middleChar('test'))
-  console.log(middleChar('testing'))
+  return ans;
+}
+console.log(middleChar("test"));
+console.log(middleChar("testing"));
