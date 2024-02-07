f6f7a2b41f922a658c2daa6295c2e4e27d1a05ea
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-02-07 20:59:04 +0530

    Midddle character

---
+++ DSA-by-Ritik-sir/Problem-solving/Middle-character.js
@@ -0,0 +1,29 @@
+// Q. Given a string, return the middle character, if the string length is odd, else return the 
+// middle two characters.
+
+// Input:
+// ```
+// test
+// ```
+// Output:
+// ```
+// es
+// ```
+// Input:
+// ```
+// testing
+// ```
+// Output:
+// ```
+// t
+// ``` 
+
+function middleChar(str){
+    let middleCharacter=Math.floor(str.length/2);
+    let ans=str[middleCharacter]
+    if(str.length %2===0){
+      ans=str[middleCharacter-1]+ans
+    }
+    return ans
+  }
+  console.log(middleChar('test'))
