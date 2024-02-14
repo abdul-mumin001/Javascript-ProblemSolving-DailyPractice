43153809812527e3a959d9492284ba665c61df9e
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-02-14 23:13:46 +0530

    Valid email

--- DSA-by-Ritik-sir/Problem-solving/Valid-email.js
+++ DSA-by-Ritik-sir/Problem-solving/Valid-email.js
@@ -1,7 +1,6 @@
 // Q Given a string which contains an email address, return true if the given email is valid,
 // else return false.
 
-
 // Input:
 // ```
 // js@aircampus.in
@@ -19,12 +18,12 @@
 // false
 // ```
 
-function validEmail(str){
-    if(str.endsWith('@aircampus.in')){
-      return true;
-    }else{
-      return false;
-    }
+function validEmail(str) {
+  if (str.endsWith("@aircampus.in")) {
+    return true;
+  } else {
+    return false;
   }
-  console.log(validEmail('js@aircampus.in'))
-  console.log(validEmail('admin@aircampus'))
+}
+console.log(validEmail("js@aircampus.in"));
+console.log(validEmail("admin@aircampus"));
