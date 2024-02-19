c060e0976bfbb7741d29098f06d14f13850041e6
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-02-19 19:07:49 +0530

    Like dislike

---
+++ DSA-by-Ritik-sir/Problem-solving/Like-dislike.js
@@ -0,0 +1,25 @@
+// Q Given an object with counts of likes and dislikes, return what vote count should be displayed.
+
+// Input:
+// ```
+// { upvotes: 13, downvotes: 0 }
+// ```
+// Output:
+// ```
+// 13
+// ```
+// Input:
+// ```
+// { upvotes: 2, downvotes: 33 }
+// ```
+// Output:
+// ```
+// -31
+// ```
+
+function likesDislikes(obj){
+  
+    return (obj['upvotes']-obj['downvotes'])
+    
+  }
+  console.log(likesDislikes({ upvotes: 13, downvotes: 0 }))
