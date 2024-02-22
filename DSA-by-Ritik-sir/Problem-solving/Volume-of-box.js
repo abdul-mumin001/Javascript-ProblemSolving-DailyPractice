564285e7730fefee40e7e705991d234163c6a7b0
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-02-22 22:57:30 +0530

    Volume of Box

---
+++ DSA-by-Ritik-sir/Problem-solving/Volume-of-box.js
@@ -0,0 +1,23 @@
+// Q Given an object containing the width length and height of a box. Return the volume of the box
+
+// Input:
+// ```
+// { width: 7, length: 2, height: 1 }
+// ```
+// Output:
+// ```
+// 14
+// ```
+// Input:
+// ```
+// { width: 4, length: 2, height: 2 }
+// ```
+// Output:
+// ```
+// 16
+// ```
+
+function volume(obj){
+    return obj["width"]*obj["length"]*obj["height"]
+  }
+  console.log(volume({ width: 7, length: 2, height: 1 }))
