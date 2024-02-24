dacbf4b72b289dbea46eb7ca1565e33c2106eb82
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-02-24 22:34:57 +0530

    Ingridients

---
+++ DSA-by-Ritik-sir/Problem-solving/Ingridients.js
@@ -0,0 +1,44 @@
+// Q Given an object containing the quantity of ingridients the chef has. For preparing a food item, 
+// the chef requires one quantity of each ingredient. Return how many food items the chef can prepare 
+// before he runs out of one of the ingridients
+
+// Input:
+// ```
+// {
+//   "salt": 23,
+//   "oil": 12,
+//   "ketchup": 10
+// }
+// ```
+// Output:
+// ```
+// 10
+// ```
+// Input:
+// ```
+// {
+//   "salt": 432,
+//   "oil": 543,
+//   "ketchup": 777
+// }
+// ```
+// Output:
+// ```
+// 432
+// ```
+
+function ingridients(obj){
+    if(obj["salt"]<obj["oil"] && obj["salt"]<obj["ketchup"]){
+      return obj["salt"]
+    }else if(obj["oil"]<obj["salt"] && obj["oil"]<obj["ketchup"]){
+      return obj["oil"]
+    }
+    else{
+      return obj["ketchup"]
+    }
+  }
+  console.log(ingridients({
+    "salt": 23,
+    "oil": 12,
+    "ketchup": 10
+  }))
