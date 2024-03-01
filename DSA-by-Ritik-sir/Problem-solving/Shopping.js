34fc18d24c14dba522552c79447de874f2514e72
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-03-01 23:33:43 +0530

    Shopping

---
+++ DSA-by-Ritik-sir/Problem-solving/Shopping.js
@@ -0,0 +1,24 @@
+// Q Given an object containing the list of shopping items and their respective prices. Returnt
+//  the all the items that has been purchased
+
+// Input:
+// ```
+// { "Shampoo": 5.99, "Rubber Ducks": 15.99 }
+// ```
+// Output:
+// ```
+// ['Shampoo', 'Rubber Ducks']
+// ```
+// Input:
+// ```
+// { "Monopoly": 11.99, "Secret Hitler": 35.99, "Bananagrams": 13.99 }
+// ```
+// Output:
+// ```
+// ['Monopoly', 'Secret Hitler', 'Bananagrams']
+// ```
+
+function shopping(obj){
+    return Object.keys(obj)
+  }
+  console.log(shopping({ "Shampoo": 5.99, "Rubber Ducks": 15.99 }))
