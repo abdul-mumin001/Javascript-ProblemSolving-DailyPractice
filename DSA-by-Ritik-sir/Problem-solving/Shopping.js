c7e8f8f1fd2277d438fc58f14c2419a38a11dae1
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-03-01 23:34:57 +0530

    Shopping

--- DSA-by-Ritik-sir/Problem-solving/Shopping.js
+++ DSA-by-Ritik-sir/Problem-solving/Shopping.js
@@ -18,8 +18,10 @@
 // ['Monopoly', 'Secret Hitler', 'Bananagrams']
 // ```
 
-function shopping(obj){
-    return Object.keys(obj)
-  }
-  console.log(shopping({ "Shampoo": 5.99, "Rubber Ducks": 15.99 }))
-  console.log(shopping({ "Monopoly": 11.99, "Secret Hitler": 35.99, "Bananagrams": 13.99 }))
+function shopping(obj) {
+  return Object.keys(obj);
+}
+console.log(shopping({ Shampoo: 5.99, "Rubber Ducks": 15.99 }));
+console.log(
+  shopping({ Monopoly: 11.99, "Secret Hitler": 35.99, Bananagrams: 13.99 })
+);
