5e45397f7268f5b525874ff325402b6fb5570ec2
abdul-mumin001 <abdulmuminbar@gmail.com>
2024-03-03 22:27:38 +0530

    Object to array

---
+++ DSA-by-Ritik-sir/Problem-solving/Object-to-array.js
@@ -0,0 +1,24 @@
+// Q Given an object, return the object as an array where each array element represent the corresponding
+//  key value.
+
+// Input:
+// ```
+// { a: 1, b: 2 }
+// ```
+// Output:
+// ```
+// [["a", 1], ["b", 2]]
+// ```
+// Input:
+// ```
+// {}
+// ```
+// Output:
+// ```
+// []
+// ```
+
+function array(obj){
+    return Object.entries(obj)
+   }
+   console.log(array({ a: 1, b: 2 }))
